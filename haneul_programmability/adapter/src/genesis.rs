966b19a71ab2efbd2ff9e6f6719d06b178f05505
GeunhwaJeong <jeonggh892@gmail.com>
2022-02-22 10:07:29 -0800

    [authority] Refactor AuthorityState creation to have a single creation point
    * Refactor AuthorityState creation to have a single creation point
    
    * Removed unnecessary already commented out code
    
    * Removed a somewhat redundant AuthorityState constructor used for testing

--- haneul_programmability/adapter/src/genesis.rs
+++ haneul_programmability/adapter/src/genesis.rs
@@ -1,7 +1,6 @@
 // Copyright (c) 2022, Haneul Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use move_vm_runtime::native_functions::NativeFunctionTable;
 use once_cell::sync::Lazy;
 use std::path::{Path, PathBuf};
 use std::sync::Mutex;
@@ -10,7 +9,6 @@ use haneul_types::error::HaneulResult;
 use haneul_types::{
     base_types::{HaneulAddress, TransactionDigest},
     object::Object,
-    MOVE_STDLIB_ADDRESS, HANEUL_FRAMEWORK_ADDRESS,
 };
 
 static GENESIS: Lazy<Mutex<Genesis>> = Lazy::new(|| {
@@ -19,12 +17,11 @@ static GENESIS: Lazy<Mutex<Genesis>> = Lazy::new(|| {
 
 struct Genesis {
     pub objects: Vec<Object>,
-    pub native_functions: NativeFunctionTable,
 }
 
-pub fn clone_genesis_data() -> (Vec<Object>, NativeFunctionTable) {
+pub fn clone_genesis_modules() -> Vec<Object> {
     let genesis = GENESIS.lock().unwrap();
-    (genesis.objects.clone(), genesis.native_functions.clone())
+    genesis.objects.clone()
 }
 
 /// Create and return objects wrapping the genesis modules for fastX
@@ -32,15 +29,10 @@ fn create_genesis_module_objects(lib_dir: &Path) -> HaneulResult<Genesis> {
     let haneul_modules = haneul_framework::get_haneul_framework_modules(lib_dir)?;
     let std_modules =
         haneul_framework::get_move_stdlib_modules(&lib_dir.join("deps").join("move-stdlib"))?;
-    let native_functions =
-        haneul_framework::natives::all_natives(MOVE_STDLIB_ADDRESS, HANEUL_FRAMEWORK_ADDRESS);
     let owner = HaneulAddress::default();
     let objects = vec![
         Object::new_package(haneul_modules, owner, TransactionDigest::genesis()),
         Object::new_package(std_modules, owner, TransactionDigest::genesis()),
     ];
-    Ok(Genesis {
-        objects,
-        native_functions,
-    })
+    Ok(Genesis { objects })
 }
