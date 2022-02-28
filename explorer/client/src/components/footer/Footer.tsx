95a268a9d93569b94086e59fd58f4ff3b77869ba
GeunhwaJeong <jeonggh892@gmail.com>
2022-02-28 11:05:54 +0000

    Adds Tailwind CSS, Mock Transaction Data, Search Function and Transaction Results Page
    * Implements a Tailwind CSS approach that compartmentalizes styling
    * Adds a mock transaction JSON dataset for testing and to facilitate work on the UI while API is in development (see `./explorer/client/src/utils/transaction_mock.json`)
    * Adds the Search Function
    * Adds the Transaction Results page
    * Implements 'Japanese Streetwear' look with Yoshimichi Ohira fonts from Google Fonts
    * Updates the tests to reflect the new functionality
    * Implements a logical structure to the `src` code with `pages` for complete pages and  `components` for things that are part of a page

---
+++ explorer/client/src/components/footer/Footer.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import ExternalLink from '../external-link/ExternalLink';
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <nav className={styles.links}>
+                <Link to="/" aria-label="home button">
+                    Home
+                </Link>
+                <ExternalLink
+                    href="https://haneul-labs.com/"
+                    label="Haneul Labs"
+                />
+                <ExternalLink
+                    href="https://devportal-30dd0.web.app/"
+                    label="Developer Hub"
+                />
+            </nav>
+        </footer>
+    );
+}
+
+export default Footer;
