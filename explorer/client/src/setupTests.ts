7eb79b0dc014c43a76156ae7c3e6e6fa2c2766e2
GeunhwaJeong <jeonggh892@gmail.com>
2022-03-22 12:05:16 -0700

    Add license headers for all typescript files

--- explorer/client/src/setupTests.ts
+++ explorer/client/src/setupTests.ts
@@ -1,1 +1,4 @@
+// Copyright (c) 2022, Haneul Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import '@testing-library/jest-dom';
--- nft_mirror/oracle_server/src/sdk/gatewayServiceAPI.ts
+++ nft_mirror/oracle_server/src/sdk/gatewayServiceAPI.ts
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Haneul Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 import {
     paths as GatewayServicePaths,
     components,
