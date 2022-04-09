8fc5baa05d6d7f6b07d3b5c2989dfa33f87ba32a
GeunhwaJeong <jeonggh892@gmail.com>
2022-04-09 16:21:00 -0700

    Add ordering for transactions on Gateway

--- haneul/src/gateway.rs
+++ haneul/src/gateway.rs
@@ -16,6 +16,7 @@ use haneul_network::network::NetworkClient;
 use haneul_network::transport;
 use haneul_types::base_types::AuthorityName;
 use haneul_types::committee::Committee;
+use haneul_types::error::HaneulResult;
 
 use crate::config::{AuthorityInfo, Config};
 use crate::rest_gateway::RestGatewayClient;
@@ -59,16 +60,16 @@ impl Display for GatewayType {
 }
 
 impl GatewayType {
-    pub fn init(&self) -> GatewayClient {
-        match self {
+    pub fn init(&self) -> HaneulResult<GatewayClient> {
+        Ok(match self {
             GatewayType::Embedded(config) => {
                 let path = config.db_folder_path.clone();
                 let committee = config.make_committee();
                 let authority_clients = config.make_authority_clients();
-                Box::new(GatewayState::new(path, committee, authority_clients))
+                Box::new(GatewayState::new(path, committee, authority_clients)?)
             }
             GatewayType::Rest(url) => Box::new(RestGatewayClient { url: url.clone() }),
-        }
+        })
     }
 }
 
