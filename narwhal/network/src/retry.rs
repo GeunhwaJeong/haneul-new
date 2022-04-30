d95b7f21ed9d7069f69e73a6f9baf57fbb2e1719
GeunhwaJeong <jeonggh892@gmail.com>
2022-04-29 21:34:43 -0400

    chore: fix the licensing

--- narwhal/network/src/retry.rs
+++ narwhal/network/src/retry.rs
@@ -1,3 +1,6 @@
+// Copyright (c) 2022, Haneul Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
 use std::{future::Future, time::Duration};
 
 /// Retry configurations for establishing connections and sending messages.
