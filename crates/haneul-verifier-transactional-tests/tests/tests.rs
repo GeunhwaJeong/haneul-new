09d7dec67594796ae72e25a4f4320166a03f8532
GeunhwaJeong <jeonggh892@gmail.com>
2022-05-17 10:38:54 -0700

    [verifier] Migrate struct_with_key tests
    - Migrated to expected output tests

---
+++ crates/haneul-verifier-transactional-tests/tests/tests.rs
@@ -0,0 +1,7 @@
+// Copyright (c) 2022, Haneul Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+pub const TEST_DIR: &str = "tests";
+use haneul_transactional_test_runner::run_test;
+
+datatest_stable::harness!(run_test, TEST_DIR, r".*\.(mvir|move)$");
