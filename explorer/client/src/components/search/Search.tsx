00717f65851cdff01c17796367e9c7067f538d71
GeunhwaJeong <jeonggh892@gmail.com>
2022-05-19 04:39:09 -0500

    [explorer + ts-sdk] Search input string type checks
    * add string type checks to typescript sdk
    
    * naming changes for transactiondigest type check
    
    * add search format error page
    
    * remove extra TODO about type guard
    
    * use else if to skip extra steps in search util
    
    * Update sdk/typescript/src/types/common.ts
    
    
    * Update sdk/typescript/src/types/common.ts
    
    
    * Update sdk/typescript/src/types/common.ts
    
    
    * fix import in common.ts
    
    * add unit tests for string type checks
    
    * unite error pages and add details to format errors
    
    * lint changes
    
    * fix move package nav shortcut
    
    * lint changes
    
    * use isGenesisLibAddress
    
    * add static env override for search type checker
    
    * lint changes
    
    * adjust missing error routing
    
    * improve genesis library regex

--- explorer/client/src/components/search/Search.tsx
+++ explorer/client/src/components/search/Search.tsx
@@ -1,10 +1,15 @@
 // Copyright (c) 2022, Haneul Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { isValidTransactionDigest, isValidHaneulAddress } from '@haneullabs/haneul.js';
 import React, { useState, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { navigateWithUnknown } from '../../utils/searchUtil';
+import { isGenesisLibAddress } from '../../utils/api/searchUtil';
+import {
+    navigateWithUnknown,
+    overrideTypeChecks,
+} from '../../utils/searchUtil';
 
 import styles from './Search.module.css';
 
@@ -18,6 +23,24 @@ function getPlaceholderText(category: SearchCategory) {
         case 'objects':
         case 'all':
             return 'Search by ID';
+    }
+}
+
+function isInputValid(category: SearchCategory, input: string): boolean {
+    if (overrideTypeChecks) return true;
+
+    switch (category) {
+        case 'objects':
+        case 'addresses':
+            return isValidHaneulAddress(input) || isGenesisLibAddress(input);
+        case 'transactions':
+            return isValidTransactionDigest(input);
+        case 'all':
+            return (
+                isValidHaneulAddress(input) ||
+                isValidTransactionDigest(input) ||
+                isGenesisLibAddress(input)
+            );
     }
 }
 
@@ -35,14 +58,22 @@ function Search() {
             if (!input.length) return;
             setPleaseWaitMode(true);
 
+            // remove empty char from input
+            let query = input.trim();
+            if (!isInputValid(category, query)) {
+                navigate(`../error/${category}/${query}`);
+                setInput('');
+                setPleaseWaitMode(false);
+                return;
+            }
+
             if (category === 'all') {
-                // remove empty char from input
-                navigateWithUnknown(input.trim(), navigate).then(() => {
+                navigateWithUnknown(query, navigate).then(() => {
                     setInput('');
                     setPleaseWaitMode(false);
                 });
             } else {
-                navigate(`../${category}/${input.trim()}`);
+                navigate(`../${category}/${query}`);
                 setInput('');
                 setPleaseWaitMode(false);
                 setCategory('all');
