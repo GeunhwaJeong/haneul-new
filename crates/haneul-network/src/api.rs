5e018b6d495dd47dd5f762946a0f10484f8cc24b
GeunhwaJeong <jeonggh892@gmail.com>
2022-05-23 13:54:30 -0700

    grpc: use build.rs file for generating grpc bindings

--- crates/haneul-network/src/api.rs
+++ crates/haneul-network/src/api.rs
@@ -1,9 +1,9 @@
 // Copyright (c) 2022, Haneul Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-#[path = "generated/haneul.validator.Validator.rs"]
-#[rustfmt::skip]
-mod validator;
+mod validator {
+    include!(concat!(env!("OUT_DIR"), "/haneul.validator.Validator.rs"));
+}
 
 pub use validator::{
     validator_client::ValidatorClient,
