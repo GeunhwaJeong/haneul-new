8931c7b1da9ec44181d9b7ec2c80b221d698569a
GeunhwaJeong <jeonggh892@gmail.com>
2022-05-26 12:02:12 -0500

    updated sad path test resilience to errors

--- crates/haneul-core/src/authority_active/gossip/configurable_batch_action_client.rs
+++ crates/haneul-core/src/authority_active/gossip/configurable_batch_action_client.rs
@@ -10,6 +10,7 @@ use async_trait::async_trait;
 use std::borrow::Borrow;
 use std::collections::BTreeMap;
 use std::sync::Arc;
+use std::sync::Once;
 use std::{env, fs};
 use haneul_adapter::genesis;
 use haneul_types::base_types::*;
@@ -24,6 +25,15 @@ use haneul_types::messages::{
 };
 use haneul_types::messages_checkpoint::{CheckpointRequest, CheckpointResponse};
 use haneul_types::object::Object;
+
+static mut SHOULD_FAIL: bool = true;
+static FIXER: Once = Once::new();
+
+fn fix() {
+    FIXER.call_once(|| unsafe {
+        SHOULD_FAIL = false;
+    })
+}
 
 #[derive(Clone)]
 pub struct TestBatch {
@@ -149,34 +159,36 @@ impl AuthorityAPI for ConfigurableBatchActionClient {
         let name = self.state.name;
         let mut items: Vec<Result<BatchInfoResponseItem, HaneulError>> = Vec::new();
         let mut seq = 0;
+        let zero_batch = SignedBatch::new(AuthorityBatch::initial(), &*secret, name);
+        items.push(Ok(BatchInfoResponseItem(UpdateItem::Batch(zero_batch))));
         let _ = actions.iter().for_each(|action| {
             match action {
                 BatchActionInternal::EmitUpdateItem(test_batch) => {
-                    let mut temp_items: Vec<Result<BatchInfoResponseItem, HaneulError>> = Vec::new();
                     let mut transactions = Vec::new();
                     for digest in test_batch.digests.clone() {
                         transactions.push((seq, digest));
                         // Safe client requires batches arrive first
-                        temp_items.push(Ok(BatchInfoResponseItem(UpdateItem::Transaction((
+                        items.push(Ok(BatchInfoResponseItem(UpdateItem::Transaction((
                             seq, digest,
                         )))));
                         seq += 1;
                     }
+                    // batch size of 1
                     let new_batch = AuthorityBatch::make_next(&last_batch, &transactions).unwrap();
                     last_batch = new_batch;
                     items.push({
                         let item = SignedBatch::new(last_batch.clone(), &*secret, name);
                         Ok(BatchInfoResponseItem(UpdateItem::Batch(item)))
                     });
-                    for temp_item in temp_items {
-                        items.push(temp_item)
+                }
+                BatchActionInternal::EmitError() => unsafe {
+                    if SHOULD_FAIL {
+                        fix();
+                        items.push(Err(HaneulError::GenericAuthorityError {
+                            error: "Synthetic authority error".to_string(),
+                        }))
                     }
-                }
-                BatchActionInternal::EmitError() => {
-                    items.push(Err(HaneulError::GenericAuthorityError {
-                        error: "Synthetic authority error".to_string(),
-                    }))
-                }
+                },
             };
         });
 
