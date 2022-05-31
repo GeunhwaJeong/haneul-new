9a0230e96ebff974be281851b790856141268d10
GeunhwaJeong <jeonggh892@gmail.com>
2022-05-31 14:55:14 -0700

    [TypeScript SDK] Implement execute move call

--- sdk/typescript/src/signers/txn-data-serializers/txn-data-serializer.ts
+++ sdk/typescript/src/signers/txn-data-serializers/txn-data-serializer.ts
@@ -2,16 +2,25 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Base64DataBuffer } from '../../serialization/base64';
-import { ObjectId, HaneulAddress } from '../../types';
+import { ObjectId, HaneulAddress, HaneulJsonValue } from '../../types';
 
 ///////////////////////////////
 // Exported Types
 export interface TransferCoinTransaction {
-  signer: HaneulAddress;
   objectId: ObjectId;
-  gasPayment: ObjectId;
+  gasPayment?: ObjectId;
   gasBudget: number;
   recipient: HaneulAddress;
+}
+
+export interface MoveCallTransaction {
+  packageObjectId: ObjectId;
+  module: string;
+  function: string;
+  typeArguments: string[];
+  arguments: HaneulJsonValue[];
+  gasPayment?: ObjectId;
+  gasBudget: number;
 }
 
 ///////////////////////////////
@@ -20,7 +29,15 @@ export interface TransferCoinTransaction {
  * Serializes a transaction to a string that can be signed by a `Signer`.
  */
 export interface TxnDataSerializer {
-  newTransferCoin(txn: TransferCoinTransaction): Promise<Base64DataBuffer>;
+  newTransferCoin(
+    signerAddress: HaneulAddress,
+    txn: TransferCoinTransaction
+  ): Promise<Base64DataBuffer>;
 
-  // TODO: add more interface methods
+  newMoveCall(
+    signerAddress: HaneulAddress,
+    txn: MoveCallTransaction
+  ): Promise<Base64DataBuffer>;
+
+  // TODO: add `newSplitCoin` and `newMergeCoin`
 }
--- wallet/src/ui/app/redux/slices/haneul-objects/Coin.ts
+++ wallet/src/ui/app/redux/slices/haneul-objects/Coin.ts
@@ -3,7 +3,14 @@
 
 import { isHaneulMoveObject } from '@haneullabs/haneul.js';
 
-import type { HaneulObject, HaneulMoveObject } from '@haneullabs/haneul.js';
+import type {
+    ObjectId,
+    HaneulObject,
+    HaneulMoveObject,
+    TransactionResponse,
+    RawSigner,
+    HaneulAddress,
+} from '@haneullabs/haneul.js';
 
 const COIN_TYPE = '0x2::Coin::Coin';
 const COIN_TYPE_ARG_REGEX = /^0x2::Coin::Coin<(.+)>$/;
@@ -25,5 +32,61 @@ export class Coin {
 
     public static getBalance(obj: HaneulMoveObject) {
         return BigInt(obj.fields.balance);
+    }
+
+    public static getID(obj: HaneulMoveObject): ObjectId {
+        return obj.fields.id.id;
+    }
+
+    /**
+     * Transfer `amount` of Coin<T> to `recipient`.
+     *
+     * @param signer A signer with connection to the gateway:e.g., new RawSigner(keypair, new JsonRpcProvider(endpoint))
+     * @param coins A list of Coins owned by the signer with the same generic type(e.g., 0x2::Haneul::Haneul)
+     * @param amount The amount to be transfer
+     * @param recipient The haneul address of the recipient
+     */
+    public static async transferCoin(
+        signer: RawSigner,
+        coins: HaneulMoveObject[],
+        amount: BigInt,
+        recipient: HaneulAddress
+    ): Promise<TransactionResponse> {
+        if (coins.length < 2) {
+            throw new Error(`Not enough coins to transfer`);
+        }
+        const coin = await Coin.selectCoin(coins, amount);
+        return await signer.transferCoin({
+            objectId: coin,
+            gasBudget: 1000,
+            recipient: recipient,
+        });
+    }
+
+    private static async selectCoin(
+        coins: HaneulMoveObject[],
+        amount: BigInt
+    ): Promise<ObjectId> {
+        const coin = await Coin.selectCoinForSplit(coins, amount);
+        // TODO: Split coin not implemented yet
+        return Coin.getID(coin);
+    }
+
+    private static async selectCoinForSplit(
+        coins: HaneulMoveObject[],
+        amount: BigInt
+    ): Promise<HaneulMoveObject> {
+        // Sort coins by balance in an ascending order
+        coins.sort();
+
+        const coinWithSufficientBalance = coins.find(
+            (c) => Coin.getBalance(c) >= amount
+        );
+        if (coinWithSufficientBalance) {
+            return coinWithSufficientBalance;
+        }
+
+        // merge coins to have a coin with sufficient balance
+        throw new Error(`Merge coin Not implemented`);
     }
 }
