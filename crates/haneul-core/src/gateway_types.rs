9d735ff41babb563d8c686cec227c2f622eab858
GeunhwaJeong <jeonggh892@gmail.com>
2022-06-13 20:48:24 -0700

    add subjective timestamps on haneul-nodes
    * add timestamps
    
    * move to index store

--- crates/haneul-core/src/gateway_types.rs
+++ crates/haneul-core/src/gateway_types.rs
@@ -48,6 +48,7 @@ mod gateway_types_tests;
 pub struct TransactionEffectsResponse {
     pub certificate: HaneulCertifiedTransaction,
     pub effects: HaneulTransactionEffects,
+    pub timestamp_ms: Option<u64>,
 }
 
 #[derive(Serialize, Deserialize, Debug, JsonSchema)]
