c35e97842cc4e591a0ba29085faa5f7bd7543745
GeunhwaJeong <jeonggh892@gmail.com>
2022-06-28 16:51:44 +0100

    wallet-ext: use fullnode for objects

--- wallet/src/ui/app/redux/slices/haneul-objects/index.ts
+++ wallet/src/ui/app/redux/slices/haneul-objects/index.ts
@@ -29,9 +29,9 @@ export const fetchAllOwnedObjects = createAsyncThunk<
     const allHaneulObjects: HaneulObject[] = [];
     if (address) {
         const allObjectRefs =
-            await api.instance.gateway.getObjectsOwnedByAddress(`${address}`);
+            await api.instance.fullNode.getObjectsOwnedByAddress(`${address}`);
         const objectIDs = allObjectRefs.map((anObj) => anObj.objectId);
-        const allObjRes = await api.instance.gateway.getObjectBatch(objectIDs);
+        const allObjRes = await api.instance.fullNode.getObjectBatch(objectIDs);
         for (const objRes of allObjRes) {
             const haneulObj = getObjectExistsResponse(objRes);
             if (haneulObj) {
