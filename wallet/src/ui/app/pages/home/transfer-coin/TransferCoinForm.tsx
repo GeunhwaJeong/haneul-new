b91195061384d4244533fc5f100b300cb7139f07
GeunhwaJeong <jeonggh892@gmail.com>
2022-07-06 18:03:30 +0100

    wallet-ext: home/main view design update
    * use new design for main view and make content container scroll instead of the whole view
    * move network-switch to settings page

--- wallet/src/ui/app/pages/home/transfer-coin/TransferCoinForm.tsx
+++ wallet/src/ui/app/pages/home/transfer-coin/TransferCoinForm.tsx
@@ -15,7 +15,7 @@ import {
 } from '_redux/slices/haneul-objects/Coin';
 import { balanceFormatOptions } from '_shared/formatting';
 
-import type { FormValues } from './';
+import type { FormValues } from '.';
 
 import st from './TransferCoinForm.module.scss';
 
