87eefe452b6a3b1a97e0d6f5d7594cc90c8133b9
GeunhwaJeong <jeonggh892@gmail.com>
2022-07-12 14:00:39 -0400

    add stake feature

--- wallet/src/ui/app/redux/slices/haneul-objects/Coin.ts
+++ wallet/src/ui/app/redux/slices/haneul-objects/Coin.ts
@@ -14,6 +14,7 @@ import type {
     TransactionResponse,
     RawSigner,
     HaneulAddress,
+    JsonRpcProvider,
 } from '@haneullabs/haneul.js';
 
 const COIN_TYPE = '0x2::coin::Coin';
@@ -21,9 +22,12 @@ const COIN_TYPE_ARG_REGEX = /^0x2::coin::Coin<(.+)>$/;
 export const DEFAULT_GAS_BUDGET_FOR_SPLIT = 1000;
 export const DEFAULT_GAS_BUDGET_FOR_MERGE = 500;
 export const DEFAULT_GAS_BUDGET_FOR_TRANSFER = 100;
+export const DEFAULT_GAS_BUDGET_FOR_STAKE = 1000;
 export const GAS_TYPE_ARG = '0x2::haneul::HANEUL';
 export const GAS_SYMBOL = 'HANEUL';
 export const DEFAULT_NFT_TRANSFER_GAS_FEE = 450;
+export const HANEUL_SYSTEM_STATE_OBJECT_ID =
+    '0x0000000000000000000000000000000000000005';
 
 // TODO use sdk
 export class Coin {
@@ -76,6 +80,32 @@ export class Coin {
             objectId: coin,
             gasBudget: DEFAULT_GAS_BUDGET_FOR_TRANSFER,
             recipient: recipient,
+        });
+    }
+
+    /**
+     * Stake `amount` of Coin<T> to `validator`. Technically it means user delegates `amount` of Coin<T> to `validator`,
+     * such that `validator` will stake the `amount` of Coin<T> for the user.
+     *
+     * @param signer A signer with connection to the gateway:e.g., new RawSigner(keypair, new JsonRpcProvider(endpoint))
+     * @param coins A list of Coins owned by the signer with the same generic type(e.g., 0x2::Haneul::Haneul)
+     * @param amount The amount to be staked
+     * @param validator The haneul address of the chosen validator
+     */
+    public static async stakeCoin(
+        signer: RawSigner,
+        coins: HaneulMoveObject[],
+        amount: bigint,
+        validator: HaneulAddress
+    ): Promise<TransactionResponse> {
+        const coin = await Coin.selectCoin(signer, coins, amount);
+        return await signer.executeMoveCall({
+            packageObjectId: '0x2',
+            module: 'haneul_system',
+            function: 'request_add_delegation',
+            typeArguments: [],
+            arguments: [HANEUL_SYSTEM_STATE_OBJECT_ID, coin, validator],
+            gasBudget: DEFAULT_GAS_BUDGET_FOR_STAKE,
         });
     }
 
@@ -141,5 +171,16 @@ export class Coin {
         }
         // primary coin might have a balance smaller than the `amount`
         return primaryCoin;
+    }
+
+    public static async getActiveValidators(
+        provider: JsonRpcProvider
+    ): Promise<Array<HaneulMoveObject>> {
+        const contents = await provider.getObject(HANEUL_SYSTEM_STATE_OBJECT_ID);
+        const data = (contents.details as HaneulObject).data;
+        const validators = (data as HaneulMoveObject).fields.validators;
+        const active_validators = (validators as HaneulMoveObject).fields
+            .active_validators;
+        return active_validators as Array<HaneulMoveObject>;
     }
 }
