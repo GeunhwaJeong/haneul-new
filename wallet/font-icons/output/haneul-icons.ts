d7a044456a21491335b095123c659628e1b16a93
GeunhwaJeong <jeonggh892@gmail.com>
2022-07-14 20:19:44 +0300

    wallet-ext: updated font with new icons
    * new icons:
      * arrow-left
      * clipboard
      * globe
      * logout
      * person
      * haneul-chevron-right
    * used icomoon to optimize all the svg iamges

--- wallet/font-icons/output/haneul-icons.ts
+++ wallet/font-icons/output/haneul-icons.ts
@@ -3,13 +3,19 @@
 
 export enum HaneulIcons {
   Apps = "haneul-icons-apps",
+  ArrowLeft = "haneul-icons-arrow-left",
+  Clipboard = "haneul-icons-clipboard",
+  Globe = "haneul-icons-globe",
   History = "haneul-icons-history",
+  Logout = "haneul-icons-logout",
   Nfts = "haneul-icons-nfts",
+  Person = "haneul-icons-person",
+  HaneulChevronRight = "haneul-icons-haneul-chevron-right",
   HaneulLogoIcon = "haneul-icons-haneul-logo-icon",
   HaneulLogoTxt = "haneul-icons-haneul-logo-txt",
   Tokens = "haneul-icons-tokens"
 }
 
-export type HaneulIconsClassname = "haneul-icons-apps" | "haneul-icons-history" | "haneul-icons-nfts" | "haneul-icons-haneul-logo-icon" | "haneul-icons-haneul-logo-txt" | "haneul-icons-tokens"
-export type HaneulIconsIcon = "apps" | "history" | "nfts" | "haneul-logo-icon" | "haneul-logo-txt" | "tokens"
+export type HaneulIconsClassname = "haneul-icons-apps" | "haneul-icons-arrow-left" | "haneul-icons-clipboard" | "haneul-icons-globe" | "haneul-icons-history" | "haneul-icons-logout" | "haneul-icons-nfts" | "haneul-icons-person" | "haneul-icons-haneul-chevron-right" | "haneul-icons-haneul-logo-icon" | "haneul-icons-haneul-logo-txt" | "haneul-icons-tokens"
+export type HaneulIconsIcon = "apps" | "arrow-left" | "clipboard" | "globe" | "history" | "logout" | "nfts" | "person" | "haneul-chevron-right" | "haneul-logo-icon" | "haneul-logo-txt" | "tokens"
 export const HaneulIconsPrefix = "haneul-icons-"
