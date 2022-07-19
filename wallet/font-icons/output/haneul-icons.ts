9278c05f8dcc67964eb113366261a99b53219393
GeunhwaJeong <jeonggh892@gmail.com>
2022-07-19 13:32:10 +0300

    wallet-ext: add font icons for home screen
    * coins
    * hand-coins
    * percentage-polygon

--- wallet/font-icons/output/haneul-icons.ts
+++ wallet/font-icons/output/haneul-icons.ts
@@ -5,10 +5,13 @@ export enum HaneulIcons {
   Apps = "haneul-icons-apps",
   ArrowLeft = "haneul-icons-arrow-left",
   Clipboard = "haneul-icons-clipboard",
+  Coins = "haneul-icons-coins",
   Globe = "haneul-icons-globe",
+  HandCoins = "haneul-icons-hand-coins",
   History = "haneul-icons-history",
   Logout = "haneul-icons-logout",
   Nfts = "haneul-icons-nfts",
+  PercentagePolygon = "haneul-icons-percentage-polygon",
   Person = "haneul-icons-person",
   HaneulChevronRight = "haneul-icons-haneul-chevron-right",
   HaneulLogoIcon = "haneul-icons-haneul-logo-icon",
@@ -16,6 +19,6 @@ export enum HaneulIcons {
   Tokens = "haneul-icons-tokens"
 }
 
-export type HaneulIconsClassname = "haneul-icons-apps" | "haneul-icons-arrow-left" | "haneul-icons-clipboard" | "haneul-icons-globe" | "haneul-icons-history" | "haneul-icons-logout" | "haneul-icons-nfts" | "haneul-icons-person" | "haneul-icons-haneul-chevron-right" | "haneul-icons-haneul-logo-icon" | "haneul-icons-haneul-logo-txt" | "haneul-icons-tokens"
-export type HaneulIconsIcon = "apps" | "arrow-left" | "clipboard" | "globe" | "history" | "logout" | "nfts" | "person" | "haneul-chevron-right" | "haneul-logo-icon" | "haneul-logo-txt" | "tokens"
+export type HaneulIconsClassname = "haneul-icons-apps" | "haneul-icons-arrow-left" | "haneul-icons-clipboard" | "haneul-icons-coins" | "haneul-icons-globe" | "haneul-icons-hand-coins" | "haneul-icons-history" | "haneul-icons-logout" | "haneul-icons-nfts" | "haneul-icons-percentage-polygon" | "haneul-icons-person" | "haneul-icons-haneul-chevron-right" | "haneul-icons-haneul-logo-icon" | "haneul-icons-haneul-logo-txt" | "haneul-icons-tokens"
+export type HaneulIconsIcon = "apps" | "arrow-left" | "clipboard" | "coins" | "globe" | "hand-coins" | "history" | "logout" | "nfts" | "percentage-polygon" | "person" | "haneul-chevron-right" | "haneul-logo-icon" | "haneul-logo-txt" | "tokens"
 export const HaneulIconsPrefix = "haneul-icons-"
