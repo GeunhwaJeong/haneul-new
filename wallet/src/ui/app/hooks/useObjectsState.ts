1a13103a0fb2ffee151f27eae08fbb6bd7657401
GeunhwaJeong <jeonggh892@gmail.com>
2022-07-19 13:41:24 +0300

    wallet-ext: home/tokens view ui update
    * main buttons are disabled until we implement the required functionality
    * left send and stake buttons until we implement the new required views
    * show stake button only for HANEUL (not available for other coins)

---
+++ wallet/src/ui/app/hooks/useObjectsState.ts
@@ -0,0 +1,27 @@
+// Copyright (c) 2022, Haneul Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useMemo } from 'react';
+
+import useAppSelector from './useAppSelector';
+
+export function useObjectsState() {
+    const objectsLoading = useAppSelector(
+        ({ haneulObjects }) => haneulObjects.loading
+    );
+    const lastSync = useAppSelector(({ haneulObjects }) => haneulObjects.lastSync);
+    const error = useAppSelector(({ haneulObjects }) => haneulObjects.error);
+    const showError =
+        !!error && (!lastSync || Date.now() - lastSync > 30 * 1000);
+    const syncedOnce = !!lastSync;
+    const loading = objectsLoading && !syncedOnce;
+    return useMemo(
+        () => ({
+            loading,
+            syncedOnce,
+            error,
+            showError,
+        }),
+        [loading, syncedOnce, error, showError]
+    );
+}
