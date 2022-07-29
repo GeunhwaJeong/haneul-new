0db386f53acd3e5fa565720fefc96bffe66c4937
GeunhwaJeong <jeonggh892@gmail.com>
2022-07-28 17:49:16 -0700

    bridge cluster tests into cargo test

---
+++ crates/haneul-cluster-test/tests/local_cluster_test.rs
@@ -0,0 +1,11 @@
+// Copyright (c) 2022, Haneul Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use haneul_cluster_test::{config::ClusterTestOpt, ClusterTest};
+
+#[tokio::test]
+async fn cluster_test() {
+    telemetry_subscribers::init_for_testing();
+
+    ClusterTest::run(ClusterTestOpt::new_local()).await;
+}
