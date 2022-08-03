852bfee2f6dccfed019a70f5f007d2c45e40076a
GeunhwaJeong <jeonggh892@gmail.com>
2022-08-02 22:20:25 -0400

    Use dbmap utils derive dump and open. Cleanup repetitions
    * derive dbmap features

--- crates/haneul-storage/src/node_sync_store.rs
+++ crates/haneul-storage/src/node_sync_store.rs
@@ -1,42 +1,26 @@
 // Copyright (c) 2022, Haneul Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::default_db_options;
-use std::path::Path;
-
 use haneul_types::{
     base_types::TransactionDigest,
-    error::{HaneulError, HaneulResult},
+    error::HaneulResult,
     messages::{CertifiedTransaction, SignedTransactionEffects},
 };
 
 use typed_store::rocks::DBMap;
-use typed_store::{reopen, traits::Map};
+use typed_store::traits::DBMapTableUtil;
+use typed_store::traits::Map;
+use typed_store_macros::DBMapUtils;
 
 /// NodeSyncStore store is used by nodes to store downloaded objects (certs, etc) that have
 /// not yet been applied to the node's HaneulDataStore.
+#[derive(DBMapUtils)]
 pub struct NodeSyncStore {
     /// Certificates/Effects that have been fetched from remote validators, but not sequenced.
     certs_and_fx: DBMap<TransactionDigest, (CertifiedTransaction, SignedTransactionEffects)>,
 }
 
 impl NodeSyncStore {
-    pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, HaneulError> {
-        let (options, _) = default_db_options(None, None);
-
-        let db = {
-            let path = &path;
-            let db_options = Some(options.clone());
-            let opt_cfs: &[(&str, &rocksdb::Options)] = &[("certs_and_fx", &options)];
-            typed_store::rocks::open_cf_opts(path, db_options, opt_cfs)
-        }
-        .map_err(HaneulError::StorageError)?;
-
-        let certs_and_fx = reopen!(&db, "certs_and_fx";<TransactionDigest, (CertifiedTransaction, SignedTransactionEffects)>);
-
-        Ok(Self { certs_and_fx })
-    }
-
     pub fn has_cert_and_effects(&self, tx: &TransactionDigest) -> HaneulResult<bool> {
         Ok(self.certs_and_fx.contains_key(tx)?)
     }
