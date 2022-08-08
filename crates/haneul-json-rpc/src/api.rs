166c4700f2bd15e082630c1b46362009508321e5
GeunhwaJeong <jeonggh892@gmail.com>
2022-08-08 12:56:58 -0400

    normalized modules and txn arguments tagging

--- crates/haneul-json-rpc/src/api.rs
+++ crates/haneul-json-rpc/src/api.rs
@@ -5,7 +5,7 @@ use jsonrpsee::core::RpcResult;
 use jsonrpsee_proc_macros::rpc;
 use haneul_json::HaneulJsonValue;
 use haneul_json_rpc_types::{
-    GatewayTxSeqNumber, GetObjectDataResponse, GetRawObjectDataResponse,
+    GatewayTxSeqNumber, GetObjectDataResponse, GetRawObjectDataResponse, MoveFunctionArgType,
     RPCTransactionRequestParams, HaneulEventEnvelope, HaneulEventFilter, HaneulObjectInfo, HaneulTypeTag,
     TransactionBytes, TransactionEffectsResponse, TransactionResponse,
 };
@@ -105,6 +105,16 @@ pub trait RpcReadApi {
 #[open_rpc(namespace = "haneul", tag = "Full Node API")]
 #[rpc(server, client, namespace = "haneul")]
 pub trait RpcFullNodeReadApi {
+    /// Return the argument types of a Move function,
+    /// based on normalized Type.
+    #[method(name = "getMoveFunctionArgTypes")]
+    async fn get_move_function_arg_types(
+        &self,
+        object_id: ObjectID,
+        module: String,
+        function: String,
+    ) -> RpcResult<Vec<MoveFunctionArgType>>;
+
     /// Return list of transactions for a specified input object.
     #[method(name = "getTransactionsByInputObject")]
     async fn get_transactions_by_input_object(
