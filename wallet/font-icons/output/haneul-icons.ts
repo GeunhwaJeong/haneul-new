0d1e211ead23464682421c142f8be8080c8f7ad4
GeunhwaJeong <jeonggh892@gmail.com>
2022-08-15 13:12:03 +0300

    wallet-ext: wait for inter font or icon-font to load
    * wait for the fonts to load before showing text/icons that use them (FOIT). The resources are already packed inside the extension so it should load them fast. Solves content moving or displaying wrong icon for a second

--- wallet/font-icons/output/haneul-icons.ts
+++ wallet/font-icons/output/haneul-icons.ts
@@ -2,12 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export enum HaneulIcons {
+  Buy = "haneul-icons-Buy",
   Info = "haneul-icons-Info",
   Preview = "haneul-icons-Preview",
   Apps = "haneul-icons-apps",
   ArrowLeft = "haneul-icons-arrow-left",
   ArrowRight = "haneul-icons-arrow-right",
-  Buy = "haneul-icons-buy",
   Checkmark = "haneul-icons-checkmark",
   Clipboard = "haneul-icons-clipboard",
   Close = "haneul-icons-close",
@@ -26,6 +26,6 @@ export enum HaneulIcons {
   VersionIcon = "haneul-icons-version-icon"
 }
 
-export type HaneulIconsClassname = "haneul-icons-Info" | "haneul-icons-Preview" | "haneul-icons-apps" | "haneul-icons-arrow-left" | "haneul-icons-arrow-right" | "haneul-icons-buy" | "haneul-icons-checkmark" | "haneul-icons-clipboard" | "haneul-icons-close" | "haneul-icons-coins" | "haneul-icons-globe" | "haneul-icons-hand-coins" | "haneul-icons-history" | "haneul-icons-logout" | "haneul-icons-nfts" | "haneul-icons-percentage-polygon" | "haneul-icons-person" | "haneul-icons-haneul-chevron-right" | "haneul-icons-haneul-logo-icon" | "haneul-icons-haneul-logo-txt" | "haneul-icons-tokens" | "haneul-icons-version-icon"
-export type HaneulIconsIcon = "Info" | "Preview" | "apps" | "arrow-left" | "arrow-right" | "buy" | "checkmark" | "clipboard" | "close" | "coins" | "globe" | "hand-coins" | "history" | "logout" | "nfts" | "percentage-polygon" | "person" | "haneul-chevron-right" | "haneul-logo-icon" | "haneul-logo-txt" | "tokens" | "version-icon"
+export type HaneulIconsClassname = "haneul-icons-Buy" | "haneul-icons-Info" | "haneul-icons-Preview" | "haneul-icons-apps" | "haneul-icons-arrow-left" | "haneul-icons-arrow-right" | "haneul-icons-checkmark" | "haneul-icons-clipboard" | "haneul-icons-close" | "haneul-icons-coins" | "haneul-icons-globe" | "haneul-icons-hand-coins" | "haneul-icons-history" | "haneul-icons-logout" | "haneul-icons-nfts" | "haneul-icons-percentage-polygon" | "haneul-icons-person" | "haneul-icons-haneul-chevron-right" | "haneul-icons-haneul-logo-icon" | "haneul-icons-haneul-logo-txt" | "haneul-icons-tokens" | "haneul-icons-version-icon"
+export type HaneulIconsIcon = "Buy" | "Info" | "Preview" | "apps" | "arrow-left" | "arrow-right" | "checkmark" | "clipboard" | "close" | "coins" | "globe" | "hand-coins" | "history" | "logout" | "nfts" | "percentage-polygon" | "person" | "haneul-chevron-right" | "haneul-logo-icon" | "haneul-logo-txt" | "tokens" | "version-icon"
 export const HaneulIconsPrefix = "haneul-icons-"
