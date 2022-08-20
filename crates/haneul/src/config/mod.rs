65c725834d7fa649766d1d54cad8cb80191cd6e4
GeunhwaJeong <jeonggh892@gmail.com>
2022-08-19 21:38:29 -0700

    enable fullnode config in haneul client & wallet context

--- crates/haneul/src/config/mod.rs
+++ crates/haneul/src/config/mod.rs
@@ -20,6 +20,8 @@ pub struct HaneulClientConfig {
     pub keystore: KeystoreType,
     pub gateway: ClientType,
     pub active_address: Option<HaneulAddress>,
+    // Temporarily make this optional, until we fully deprecate gateway
+    pub fullnode: Option<ClientType>,
 }
 
 impl Config for HaneulClientConfig {}
@@ -40,6 +42,10 @@ impl Display for HaneulClientConfig {
         };
         writeln!(writer, "{}", self.keystore)?;
         write!(writer, "{}", self.gateway)?;
+
+        if let Some(fullnode_type) = &self.fullnode {
+            write!(writer, "{}", fullnode_type)?;
+        }
 
         write!(f, "{}", writer)
     }
