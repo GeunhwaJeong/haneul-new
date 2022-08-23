0a24f244217dd0a4a715d7accb34b198a0d8bde7
GeunhwaJeong <jeonggh892@gmail.com>
2022-08-22 21:47:38 -0700

    [wallet-adapter] Migrate wallet adapter demo to Vite
    * Migrate wallet adapter demo to Vite
    
    * Resolve rebase issues

--- wallet-adapter/packages/adapters/integrations/all-wallets/src/index.ts
+++ wallet-adapter/packages/adapters/integrations/all-wallets/src/index.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2022, Haneul Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-export * from '@haneul-wallet-adapter/haneul-wallet';
-export * from '@haneul-wallet-adapter/mock-wallet';
+export * from '@haneullabs/wallet-adapter-haneul-wallet';
+export * from '@haneullabs/wallet-adapter-mock-wallet';
--- wallet-adapter/packages/adapters/integrations/haneul-wallet/src/adapter.ts
+++ wallet-adapter/packages/adapters/integrations/haneul-wallet/src/adapter.ts
@@ -1,75 +1,84 @@
 // Copyright (c) 2022, Haneul Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { MoveCallTransaction, HaneulAddress, HaneulTransactionResponse } from "@haneullabs/haneul.js";
+import {
+  MoveCallTransaction,
+  HaneulAddress,
+  HaneulTransactionResponse,
+} from "@haneullabs/haneul.js";
 import { WalletCapabilities } from "@haneullabs/wallet-adapter-base";
 
-const ALL_PERMISSION_TYPES = [
-    'viewAccount',
-    'suggestTransactions',
-] as const;
+const ALL_PERMISSION_TYPES = ["viewAccount", "suggestTransactions"] as const;
 type AllPermissionsType = typeof ALL_PERMISSION_TYPES;
 type PermissionType = AllPermissionsType[number];
 
 interface HaneulWallet {
-    hasPermissions(permissions: readonly PermissionType[]): Promise<boolean>;
-    requestPermissions(): Promise<boolean>;
-    getAccounts(): Promise<HaneulAddress[]>;
-    executeMoveCall: (transaction: MoveCallTransaction) => Promise<HaneulTransactionResponse>;
-    executeSerializedMoveCall: (transactionBytes: Uint8Array) => Promise<HaneulTransactionResponse>;
+  hasPermissions(permissions: readonly PermissionType[]): Promise<boolean>;
+  requestPermissions(): Promise<boolean>;
+  getAccounts(): Promise<HaneulAddress[]>;
+  executeMoveCall: (
+    transaction: MoveCallTransaction
+  ) => Promise<HaneulTransactionResponse>;
+  executeSerializedMoveCall: (
+    transactionBytes: Uint8Array
+  ) => Promise<HaneulTransactionResponse>;
 }
 interface HaneulWalletWindow {
-    haneulWallet: HaneulWallet
+  haneulWallet: HaneulWallet;
 }
 
 declare const window: HaneulWalletWindow;
 
 // Stored as state somewhere (Probably in a place with generics )
-export class HaneulWalletAdapter implements WalletCapabilities{
-    connecting: boolean;
-    connected: boolean;
+export class HaneulWalletAdapter implements WalletCapabilities {
+  connecting: boolean;
+  connected: boolean;
 
-    getAccounts(): Promise<string[]> {
-        return window.haneulWallet.getAccounts();
-    }
-    executeMoveCall(transaction: MoveCallTransaction): Promise<HaneulTransactionResponse> {
-        return window.haneulWallet.executeMoveCall(transaction);
-    }
-    executeSerializedMoveCall(transactionBytes: Uint8Array): Promise<HaneulTransactionResponse> {
-        return window.haneulWallet.executeSerializedMoveCall(transactionBytes);
-    }
+  getAccounts(): Promise<string[]> {
+    return window.haneulWallet.getAccounts();
+  }
+  executeMoveCall(
+    transaction: MoveCallTransaction
+  ): Promise<HaneulTransactionResponse> {
+    return window.haneulWallet.executeMoveCall(transaction);
+  }
+  executeSerializedMoveCall(
+    transactionBytes: Uint8Array
+  ): Promise<HaneulTransactionResponse> {
+    return window.haneulWallet.executeSerializedMoveCall(transactionBytes);
+  }
 
-    name = "Haneul Wallet";
+  name = "Haneul Wallet";
 
-    async connect(): Promise<void> {
-        this.connecting = true;
-        if (window.haneulWallet) {
-            const wallet = window.haneulWallet;
-            try {
-                let given = await wallet.requestPermissions();
-                const newLocal: readonly PermissionType[] = ['viewAccount']
-                let perms = await wallet.hasPermissions(newLocal);
-                console.log(perms);
-                console.log(given);
-                this.connected = true;
-            } catch (err) {
-                console.error(err);
-            } finally {
-                this.connecting = false;
-            }
-        }
-    }
-
-    // Come back to this later
-    async disconnect(): Promise<void> {
-        if (this.connected == true) {
-            this.connected = false;
-        }
-        console.log("disconnected");
-    }
-
-    constructor() {
-        this.connected = false;
+  async connect(): Promise<void> {
+    this.connecting = true;
+    if (window.haneulWallet) {
+      const wallet = window.haneulWallet;
+      try {
+        let given = await wallet.requestPermissions();
+        const newLocal: readonly PermissionType[] = ["viewAccount"];
+        let perms = await wallet.hasPermissions(newLocal);
+        console.log(perms);
+        console.log(given);
+        this.connected = true;
+      } catch (err) {
+        console.error(err);
+      } finally {
         this.connecting = false;
+      }
     }
+  }
+
+  // Come back to this later
+  async disconnect(): Promise<void> {
+    if (this.connected == true) {
+      this.connected = false;
+    }
+    console.log("disconnected");
+  }
+
+  constructor() {
+    this.connected = false;
+    this.connecting = false;
+  }
 }
