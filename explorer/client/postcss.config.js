3053c152d40ddf3bfa667034a7d33a688661e8ec
GeunhwaJeong <jeonggh892@gmail.com>
2022-08-26 13:59:14 -0700

    [explorer] Migrate explorer to vite
    * Migrate explorer to vite
    
    * Fix format issues, remove unused package
    
    * Fix builds and format
    
    * Resolve PR feedback
    
    * Update packages
    
    * Remove react-scripts
    
    * Update packages

---
+++ explorer/client/postcss.config.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Haneul Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
