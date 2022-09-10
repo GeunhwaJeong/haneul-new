75bb7fa65cd0aa8c6d9bf12ceba69268d207b8fd
GeunhwaJeong <jeonggh892@gmail.com>
2022-09-10 16:02:10 -0700

    [doc] fix Move package tutorial
    The package tutorial is currently broken, which is confusing some folks on Discord and in builder's groups. Let's take this opportunity to fix and simplify the tutorial:
    
    * Use `haneul move new` to create the package skeleton
    * Fix `haneul move new` to create a Move.toml that depends on the devnet branch + has a named alias for `haneul` by default
    * Fix unused imports in the example code
    * Expand the example code and explain the different parts of the module

--- crates/haneul/src/haneul_move/new.rs
+++ crates/haneul/src/haneul_move/new.rs
@@ -4,9 +4,12 @@
 use clap::Parser;
 use move_cli::base::new;
 use std::path::PathBuf;
+use haneul_types::HANEUL_FRAMEWORK_ADDRESS;
 
 const HANEUL_PKG_NAME: &str = "Haneul";
-const HANEUL_PKG_PATH: &str = "{ git = \"https://github.com/GeunhwaJeong/haneul.git\", subdir = \"crates/haneul-framework\", rev = \"main\" }";
+
+// Use devnet by default. Probably want to add options to make this configurable later
+const HANEUL_PKG_PATH: &str = "{ git = \"https://github.com/GeunhwaJeong/haneul.git\", subdir = \"crates/haneul-framework\", rev = \"devnet\" }";
 
 #[derive(Parser)]
 pub struct New {
@@ -21,7 +24,13 @@ impl New {
             path,
             "0.0.1",
             [(HANEUL_PKG_NAME, HANEUL_PKG_PATH)],
-            [(name, "0x0")],
+            [
+                (name, "0x0"),
+                (
+                    &HANEUL_PKG_NAME.to_lowercase(),
+                    &HANEUL_FRAMEWORK_ADDRESS.to_string(),
+                ),
+            ],
             "",
         )?;
         Ok(())
