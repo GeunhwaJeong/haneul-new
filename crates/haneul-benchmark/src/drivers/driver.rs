eae06d4dc41bd61dee4eb11e6a3da3dfaadc85fa
GeunhwaJeong <jeonggh892@gmail.com>
2022-09-12 18:07:12 -0700

    Cleanup old benchmark code
    * Fix metric collection for network client in stress test
    
    * Cleanup old benchmark code

--- crates/haneul-benchmark/src/drivers/driver.rs
+++ crates/haneul-benchmark/src/drivers/driver.rs
@@ -1,18 +1,17 @@
 // Copyright (c) 2022, Haneul Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-
-use crate::workloads::workload::Payload;
-use crate::workloads::workload::Workload;
 use async_trait::async_trait;
 use prometheus::Registry;
 use haneul_core::authority_aggregator::AuthorityAggregator;
 use haneul_core::authority_client::NetworkAuthorityClient;
 
+use crate::workloads::workload::WorkloadInfo;
+
 #[async_trait]
 pub trait Driver<T> {
     async fn run(
         &self,
-        workload: Box<dyn Workload<dyn Payload>>,
+        workload: Vec<WorkloadInfo>,
         aggregator: AuthorityAggregator<NetworkAuthorityClient>,
         registry: &Registry,
     ) -> Result<T, anyhow::Error>;
