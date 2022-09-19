c827edac35f0216d233887924afef923f7542707
GeunhwaJeong <jeonggh892@gmail.com>
2022-09-19 15:33:21 -0700

    Remove experimental SDK

--- sdk/typescript/experimental/src/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2022, Haneul Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-export * from "./haneul-client";
-export * from "./module";
-export * from "./types";
-export * from "./rpc";
