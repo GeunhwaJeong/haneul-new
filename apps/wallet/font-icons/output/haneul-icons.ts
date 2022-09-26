fbd16e915f3f122777589b4e35632e522fa4523f
GeunhwaJeong <jeonggh892@gmail.com>
2022-09-26 17:57:43 +0100

    wallet-ext: update font-icons
    * add nft-type-image

--- apps/wallet/font-icons/output/haneul-icons.ts
+++ apps/wallet/font-icons/output/haneul-icons.ts
@@ -22,6 +22,7 @@ export enum HaneulIcons {
   HandCoins = "haneul-icons-hand-coins",
   History = "haneul-icons-history",
   Logout = "haneul-icons-logout",
+  NftTypeImage = "haneul-icons-nft-type-image",
   Nfts = "haneul-icons-nfts",
   PercentagePolygon = "haneul-icons-percentage-polygon",
   Person = "haneul-icons-person",
@@ -34,6 +35,6 @@ export enum HaneulIcons {
   VersionIcon = "haneul-icons-version-icon"
 }
 
-export type HaneulIconsClassname = "haneul-icons-Buy" | "haneul-icons-Download" | "haneul-icons-Info" | "haneul-icons-Plus" | "haneul-icons-Preview" | "haneul-icons-ThumbsUp" | "haneul-icons-Union" | "haneul-icons-activity" | "haneul-icons-apps" | "haneul-icons-arrow-left" | "haneul-icons-arrow-right" | "haneul-icons-checkmark" | "haneul-icons-chevron-down" | "haneul-icons-clipboard" | "haneul-icons-close" | "haneul-icons-coins" | "haneul-icons-globe" | "haneul-icons-hand-coins" | "haneul-icons-history" | "haneul-icons-logout" | "haneul-icons-nfts" | "haneul-icons-percentage-polygon" | "haneul-icons-person" | "haneul-icons-search" | "haneul-icons-haneul-chevron-right" | "haneul-icons-haneul-logo-icon" | "haneul-icons-haneul-logo-txt" | "haneul-icons-swap" | "haneul-icons-tokens" | "haneul-icons-version-icon"
-export type HaneulIconsIcon = "Buy" | "Download" | "Info" | "Plus" | "Preview" | "ThumbsUp" | "Union" | "activity" | "apps" | "arrow-left" | "arrow-right" | "checkmark" | "chevron-down" | "clipboard" | "close" | "coins" | "globe" | "hand-coins" | "history" | "logout" | "nfts" | "percentage-polygon" | "person" | "search" | "haneul-chevron-right" | "haneul-logo-icon" | "haneul-logo-txt" | "swap" | "tokens" | "version-icon"
+export type HaneulIconsClassname = "haneul-icons-Buy" | "haneul-icons-Download" | "haneul-icons-Info" | "haneul-icons-Plus" | "haneul-icons-Preview" | "haneul-icons-ThumbsUp" | "haneul-icons-Union" | "haneul-icons-activity" | "haneul-icons-apps" | "haneul-icons-arrow-left" | "haneul-icons-arrow-right" | "haneul-icons-checkmark" | "haneul-icons-chevron-down" | "haneul-icons-clipboard" | "haneul-icons-close" | "haneul-icons-coins" | "haneul-icons-globe" | "haneul-icons-hand-coins" | "haneul-icons-history" | "haneul-icons-logout" | "haneul-icons-nft-type-image" | "haneul-icons-nfts" | "haneul-icons-percentage-polygon" | "haneul-icons-person" | "haneul-icons-search" | "haneul-icons-haneul-chevron-right" | "haneul-icons-haneul-logo-icon" | "haneul-icons-haneul-logo-txt" | "haneul-icons-swap" | "haneul-icons-tokens" | "haneul-icons-version-icon"
+export type HaneulIconsIcon = "Buy" | "Download" | "Info" | "Plus" | "Preview" | "ThumbsUp" | "Union" | "activity" | "apps" | "arrow-left" | "arrow-right" | "checkmark" | "chevron-down" | "clipboard" | "close" | "coins" | "globe" | "hand-coins" | "history" | "logout" | "nft-type-image" | "nfts" | "percentage-polygon" | "person" | "search" | "haneul-chevron-right" | "haneul-logo-icon" | "haneul-logo-txt" | "swap" | "tokens" | "version-icon"
 export const HaneulIconsPrefix = "haneul-icons-"
