7f1c177f4d0ca1bb04f7fae868e28fbc08fd7676
GeunhwaJeong <jeonggh892@gmail.com>
2022-10-07 21:46:06 +0100

    wallet-ext: create wallet view
    * update ui
    * encrypt wallet when enabled (under env flag)

--- apps/wallet/src/ui/app/components/alert/index.tsx
+++ apps/wallet/src/ui/app/components/alert/index.tsx
@@ -4,7 +4,7 @@
 import cl from 'classnames';
 import { memo } from 'react';
 
-import Icon from '_components/icon';
+import Icon, { HaneulIcons } from '_components/icon';
 
 import type { ReactNode } from 'react';
 
@@ -18,7 +18,7 @@ export type AlertProps = {
 function Alert({ children, className }: AlertProps) {
     return (
         <div className={cl(st.container, st.error, className)}>
-            <Icon className={st.icon} icon="exclamation-circle" />
+            <Icon className={st.icon} icon={HaneulIcons.Info} />
             <div className={st.message}>{children}</div>
         </div>
     );
