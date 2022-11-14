1a31860b7946f4bc9a06e4bfb319fe21408dedbd
GeunhwaJeong <jeonggh892@gmail.com>
2022-11-14 14:09:13 -0800

    [checkpoint v2] Submit checkpoint signatures to consensus
    This PR signs and submit checkpoint to consensus so it can be later aggregated into a certified checkpoint.
    
    https://github.com/GeunhwaJeong/haneul/issues/5763

--- crates/haneul-core/src/checkpoints2/checkpoint_output.rs
+++ crates/haneul-core/src/checkpoints2/checkpoint_output.rs
@@ -1,16 +1,30 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use crate::authority::StableSyncAuthoritySigner;
+use crate::consensus_adapter::SubmitToConsensus;
+use async_trait::async_trait;
+use haneul_types::base_types::AuthorityName;
 use haneul_types::error::HaneulResult;
-use haneul_types::messages_checkpoint::{CheckpointContents, CheckpointSummary};
+use haneul_types::messages::ConsensusTransaction;
+use haneul_types::messages_checkpoint::{
+    CheckpointContents, CheckpointSignatureMessage, CheckpointSummary, SignedCheckpointSummary,
+};
 use tracing::{debug, info};
 
+#[async_trait]
 pub trait CheckpointOutput: Sync + Send + 'static {
-    fn checkpoint_created(
+    async fn checkpoint_created(
         &self,
         summary: &CheckpointSummary,
         contents: &CheckpointContents,
     ) -> HaneulResult;
+}
+
+pub struct SubmitCheckpointToConsensus<T> {
+    pub sender: T,
+    pub signer: StableSyncAuthoritySigner,
+    pub authority: AuthorityName,
 }
 
 pub struct LogCheckpointOutput;
@@ -21,8 +35,30 @@ impl LogCheckpointOutput {
     }
 }
 
+#[async_trait]
+impl<T: SubmitToConsensus> CheckpointOutput for SubmitCheckpointToConsensus<T> {
+    async fn checkpoint_created(
+        &self,
+        summary: &CheckpointSummary,
+        contents: &CheckpointContents,
+    ) -> HaneulResult {
+        LogCheckpointOutput
+            .checkpoint_created(summary, contents)
+            .await?;
+        let summary = SignedCheckpointSummary::new_from_summary(
+            summary.clone(),
+            self.authority,
+            &*self.signer,
+        );
+        let message = CheckpointSignatureMessage { summary };
+        let transaction = ConsensusTransaction::new_checkpoint_signature_message(message);
+        self.sender.submit_to_consensus(&transaction).await
+    }
+}
+
+#[async_trait]
 impl CheckpointOutput for LogCheckpointOutput {
-    fn checkpoint_created(
+    async fn checkpoint_created(
         &self,
         summary: &CheckpointSummary,
         contents: &CheckpointContents,
