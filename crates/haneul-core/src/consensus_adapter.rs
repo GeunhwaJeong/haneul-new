4b0a3c4027900b22753a1397911ef5b9e7c7f215
GeunhwaJeong <jeonggh892@gmail.com>
2022-11-16 06:11:37 -0500

    feat: build basic TxValidator scaffolding

--- crates/haneul-core/src/consensus_adapter.rs
+++ crates/haneul-core/src/consensus_adapter.rs
@@ -345,6 +345,9 @@ impl<'a> Drop for InflightDropGuard<'a> {
     }
 }
 
+/// An implementation of the Narwhal TxValidator trait that validates transactions coming from Haneul, and those coming from the network.
+pub use narwhal_worker::TrivialTxValidator as HaneulTxValidator;
+
 #[cfg(test)]
 mod adapter_tests {
     use super::ConsensusAdapter;
