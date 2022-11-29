083ca0d3ab494ec0ff9c3f084a56e058722bcb35
GeunhwaJeong <jeonggh892@gmail.com>
2022-11-29 01:17:47 +0000

    explorer: moveCall execution
    * explorer: moveCall execution
    
    * execute a moveCall transaction using wallet-adapter
    
    * explorer: addresses pr comments
    
    * Label ui component
    * Input ui component
    * fixes useZodForm
    * removes useFieldArray
    * use 4 spacing instead of 3.75
    * add input shadow color to config
    * use useMutation
    * add burner wallet adapter in dev

---
+++ apps/explorer/src/components/module/module-functions-interaction/useFunctionParamsDetails.ts
@@ -0,0 +1,26 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useMemo } from 'react';
+
+import { getNormalizedFunctionParameterTypeDetails } from '../utils';
+
+import type { HaneulMoveNormalizedType } from '@haneullabs/haneul.js';
+
+export function useFunctionParamsDetails(
+    params: HaneulMoveNormalizedType[],
+    functionTypeArgNames?: string[]
+) {
+    return useMemo(
+        () =>
+            params
+                .map((aParam) =>
+                    getNormalizedFunctionParameterTypeDetails(
+                        aParam,
+                        functionTypeArgNames
+                    )
+                )
+                .filter(({ isTxContext }) => !isTxContext),
+        [params, functionTypeArgNames]
+    );
+}
