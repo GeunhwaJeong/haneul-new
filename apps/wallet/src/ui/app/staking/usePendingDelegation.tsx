0b0994bd2fa1d1fe572c715613d239c2fd678a94
GeunhwaJeong <jeonggh892@gmail.com>
2022-12-05 18:10:21 -0800

    [wallet-ext] Add support for fetching pending delegations

---
+++ apps/wallet/src/ui/app/staking/usePendingDelegation.tsx
@@ -0,0 +1,110 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { isHaneulMoveObject, isHaneulObject, type HaneulAddress } from '@haneullabs/haneul.js';
+import { useQuery, type UseQueryResult } from '@tanstack/react-query';
+import { useMemo } from 'react';
+
+import { notEmpty } from '../helpers';
+import { useAppSelector } from '../hooks';
+import { api } from '../redux/store/thunk-extras';
+
+const STATE_OBJECT = '0x5';
+
+// TODO: Generalize into SDK:
+interface SystemStateObject {
+    validators: {
+        fields: {
+            active_validators: {
+                fields: {
+                    metadata: {
+                        fields: {
+                            name: string;
+                        };
+                    };
+                    delegation_staking_pool: {
+                        fields: {
+                            // TODO: Figure out why this is an empty string sometimes:
+                            pending_delegations:
+                                | string
+                                | {
+                                      fields: {
+                                          delegator: HaneulAddress;
+                                          haneul_amount: number;
+                                      };
+                                  }[];
+                        };
+                    };
+                };
+            }[];
+        };
+    };
+}
+
+interface PendingDelegation {
+    name: string;
+    staked: bigint;
+}
+
+/**
+ * Fetches the pending delegations from the system object. This is currently pretty hacky and expensive.
+ */
+export function usePendingDelegation(): [PendingDelegation[], UseQueryResult] {
+    const address = useAppSelector(({ account: { address } }) => address);
+
+    // TODO: Use generlized `useGetObject` hook when it lands:
+    const objectQuery = useQuery(['object', STATE_OBJECT], async () => {
+        return api.instance.fullNode.getObject(STATE_OBJECT);
+    });
+
+    const { data } = objectQuery;
+
+    const pendingDelegation = useMemo(() => {
+        if (
+            !address ||
+            !data ||
+            !isHaneulObject(data.details) ||
+            !isHaneulMoveObject(data.details.data)
+        ) {
+            return [];
+        }
+
+        const systemState = data.details.data.fields as SystemStateObject;
+
+        const pendingDelegationsPerValidator =
+            systemState.validators.fields.active_validators
+                .map((validator) => {
+                    const pendingDelegations =
+                        validator.fields.delegation_staking_pool.fields
+                            .pending_delegations;
+
+                    if (!Array.isArray(pendingDelegations)) return null;
+
+                    const filteredDelegations = pendingDelegations.filter(
+                        (delegation) => delegation.fields.delegator === address
+                    );
+
+                    if (!filteredDelegations.length) return null;
+
+                    // TODO: Follow-up about why this is base64 encoded:
+                    const name = Buffer.from(
+                        validator.fields.metadata.fields.name,
+                        'base64'
+                    ).toString();
+
+                    return {
+                        name,
+                        staked: filteredDelegations.reduce(
+                            (acc, delegation) =>
+                                acc + BigInt(delegation.fields.haneul_amount),
+                            0n
+                        ),
+                    } as PendingDelegation;
+                })
+                .filter(notEmpty);
+
+        return pendingDelegationsPerValidator;
+    }, [data, address]);
+
+    return [pendingDelegation, objectQuery];
+}
