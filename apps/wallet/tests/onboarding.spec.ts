479119c644ae03b203758da1d52229f633fdb026
GeunhwaJeong <jeonggh892@gmail.com>
2023-01-09 09:05:16 -0800

    [wallet-ext] End-to-end tests

---
+++ apps/wallet/tests/onboarding.spec.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { test, expect } from './fixtures';
+import { createWallet } from './utils/auth';
+import { generateKeypair } from './utils/localnet';
+
+test('create new wallet', async ({ page, extensionUrl }) => {
+    await createWallet(page, extensionUrl);
+    await expect(page.getByTestId('coin-page')).toBeVisible();
+});
+
+test('import wallet', async ({ page, extensionUrl }) => {
+    const { mnemonic, keypair } = await generateKeypair();
+
+    await page.goto(extensionUrl);
+    await page.getByRole('link', { name: /Get Started/ }).click();
+    await page.getByRole('link', { name: /Import an Existing Wallet/ }).click();
+    await page.getByLabel('Enter Recovery Phrase').fill(mnemonic);
+    await page.getByRole('button', { name: /Continue/ }).click();
+    await page.getByLabel('Create Password').fill('haneullabs');
+    await page.getByLabel('Confirm Password').fill('haneullabs');
+    await page.getByRole('button', { name: /Import/ }).click();
+    await page.getByRole('button', { name: /Open Haneul Wallet/ }).click();
+    await expect(page.getByTestId('coin-page')).toBeVisible();
+    await expect(
+        page.getByText(keypair.getPublicKey().toHaneulAddress().slice(0, 4))
+    ).toBeVisible();
+});
