63da3c15b3b6447400063011b0d7249d5aadce6c
GeunhwaJeong <jeonggh892@gmail.com>
2023-01-11 10:33:34 -0800

    [frenemeies] Scaffold dapp

---
+++ dapps/frenemies/src/routes/Connect.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { ConnectButton } from '@haneullabs/wallet-kit';
+
+export function Connect() {
+	return <ConnectButton />
+}
