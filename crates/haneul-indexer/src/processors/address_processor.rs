4e4fe26118bd1da2700f49a1d32f4371d67d341b
GeunhwaJeong <jeonggh892@gmail.com>
2023-01-11 15:01:27 -0700

    indexer: add basic prometheus metrics
    besides title, this PR also reverts changes in  which was a hack
    and we do not need it any more with Brad's instructions on
    haneul-operations.
    
    Tested locally
    1. run local validator
    2. run indexer with local validator and clean local DB
    ```
    cargo run --bin haneul-indexer -- --db-url "postgres://postgres:postgres@localhost/gegao" --rpc-client-url "http://127.0.0.1:9000"
    ```
    
    3. saw metrics on http://localhost:8081/metrics (the default address and
    port)
    ```
    # HELP total_address_batch_processed Total number of address batches processed
    # TYPE total_address_batch_processed counter
    total_address_batch_processed 210948
    # HELP total_address_processor_error Total number of address processor error
    # TYPE total_address_processor_error counter
    total_address_processor_error 0
    # HELP total_event_handler_error Total number of event handler error
    # TYPE total_event_handler_error counter
    total_event_handler_error 0
    # HELP total_event_page_committed Total number of event page committed
    # TYPE total_event_page_committed counter
    total_event_page_committed 544
    # HELP total_event_page_fetch_attempt Total number of event page fetch attempt
    # TYPE total_event_page_fetch_attempt counter
    total_event_page_fetch_attempt 545
    # HELP total_event_page_received Total number of event page received
    # TYPE total_event_page_received counter
    total_event_page_received 545
    # HELP total_events_processed Total number of events processed
    # TYPE total_events_processed counter
    total_events_processed 8853
    # HELP total_events_received Total number of events received
    # TYPE total_events_received counter
    total_events_received 8910
    # HELP total_object_batch_processed Total number of object batches processed
    # TYPE total_object_batch_processed counter
    total_object_batch_processed 574
    # HELP total_object_processor_error Total number of object processor error
    # TYPE total_object_processor_error counter
    total_object_processor_error 0
    # HELP total_package_batch_processed Total number of package batches processed
    # TYPE total_package_batch_processed counter
    total_package_batch_processed 574
    # HELP total_package_processor_error Total number of package processor error
    # TYPE total_package_processor_error counter
    total_package_processor_error 0
    # HELP total_transaction_handler_error Total number of transaction handler error
    # TYPE total_transaction_handler_error counter
    total_transaction_handler_error 0
    # HELP total_transaction_page_committed Total number of transaction page committed
    # TYPE total_transaction_page_committed counter
    total_transaction_page_committed 542
    # HELP total_transaction_page_fetch_attempt Total number of transaction page fetch attempt
    # TYPE total_transaction_page_fetch_attempt counter
    total_transaction_page_fetch_attempt 542
    # HELP total_transaction_page_received Total number of transaction page received
    # TYPE total_transaction_page_received counter
    total_transaction_page_received 542
    # HELP total_transactions_processed Total number of transactions processed
    # TYPE total_transactions_processed counter
    total_transactions_processed 2920
    # HELP total_transactions_received Total number of transactions received
    # TYPE total_transactions_received counter
    total_transactions_received 2920
    # HELP uptime uptime of the node service in seconds
    # TYPE uptime counter
    uptime{version="0.21.0-18dab1f42"} 58
    ```

--- crates/haneul-indexer/src/processors/address_processor.rs
+++ crates/haneul-indexer/src/processors/address_processor.rs
@@ -1,10 +1,12 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use prometheus::Registry;
 use std::sync::Arc;
-use haneul_indexer::errors::IndexerError;
 use tracing::info;
 
+use haneul_indexer::errors::IndexerError;
+use haneul_indexer::metrics::IndexerAddressProcessorMetrics;
 use haneul_indexer::models::address_logs::{commit_address_log, read_address_log};
 use haneul_indexer::models::addresses::{commit_addresses, transaction_to_address, NewAddress};
 use haneul_indexer::models::transactions::read_transactions;
@@ -14,11 +16,19 @@ const TRANSACTION_BATCH_SIZE: usize = 100;
 
 pub struct AddressProcessor {
     pg_connection_pool: Arc<PgConnectionPool>,
+    pub address_processor_metrics: IndexerAddressProcessorMetrics,
 }
 
 impl AddressProcessor {
-    pub fn new(pg_connection_pool: Arc<PgConnectionPool>) -> AddressProcessor {
-        Self { pg_connection_pool }
+    pub fn new(
+        pg_connection_pool: Arc<PgConnectionPool>,
+        prometheus_registry: &Registry,
+    ) -> AddressProcessor {
+        let address_processor_metrics = IndexerAddressProcessorMetrics::new(prometheus_registry);
+        Self {
+            pg_connection_pool,
+            address_processor_metrics,
+        }
     }
 
     pub async fn start(&self) -> Result<(), IndexerError> {
@@ -38,6 +48,9 @@ impl AddressProcessor {
 
             commit_addresses(&mut pg_pool_conn, addr_vec)?;
             commit_address_log(&mut pg_pool_conn, last_processed_id)?;
+            self.address_processor_metrics
+                .total_address_batch_processed
+                .inc();
         }
     }
 }
