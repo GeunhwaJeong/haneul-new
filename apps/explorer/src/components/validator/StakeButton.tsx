a94775f582a93c1d4a6dbc5b7f2e39aa648f99ac
GeunhwaJeong <jeonggh892@gmail.com>
2023-01-25 10:47:37 -0800

    [explorer] Stake button on validator page

---
+++ apps/explorer/src/components/validator/StakeButton.tsx
@@ -0,0 +1,98 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useFeature } from '@growthbook/growthbook-react';
+import {
+    ConnectButton,
+    useWalletKit,
+    type StandardWalletAdapter,
+    type WalletWithFeatures,
+} from '@haneullabs/wallet-kit';
+import { useParams } from 'react-router-dom';
+
+import { Button } from '~/ui/Button';
+import { GROWTHBOOK_FEATURES } from '~/utils/growthbook';
+
+// This is a custom feature supported by the Haneul Wallet:
+type StakeInput = { validatorAddress: string };
+type HaneulWalletStakeFeature = {
+    'haneulWallet:stake': {
+        version: '0.0.1';
+        stake: (input: StakeInput) => Promise<void>;
+    };
+};
+
+type StakeWallet = WalletWithFeatures<Partial<HaneulWalletStakeFeature>>;
+
+export function StakeButton() {
+    const stakeButtonEnabled = useFeature(
+        GROWTHBOOK_FEATURES.VALIDATOR_PAGE_STAKING
+    ).on;
+    const { id } = useParams();
+    const { wallets, currentWallet, connect } = useWalletKit();
+
+    if (!stakeButtonEnabled) return null;
+
+    const stakeSupportedWallets = wallets.filter((wallet) => {
+        if (!('wallet' in wallet)) {
+            return false;
+        }
+
+        const standardWallet = wallet.wallet as StakeWallet;
+        return 'haneulWallet:stake' in standardWallet.features;
+    });
+
+    const currentWalletSupportsStake =
+        currentWallet &&
+        !!stakeSupportedWallets.find(({ name }) => currentWallet.name === name);
+
+    if (!stakeSupportedWallets.length) {
+        return (
+            <Button
+                size="lg"
+                variant="outline"
+                href="https://chrome.google.com/webstore/detail/haneul-wallet/opcgpfmipidbgpenhmajoajpbobppdil"
+            >
+                Install Haneul Wallet to stake HANEUL
+            </Button>
+        );
+    }
+
+    if (!currentWallet) {
+        return (
+            <ConnectButton
+                className="!border !border-solid !border-steel-dark !bg-transparent !px-4 !py-3 !text-body !font-semibold !text-steel-dark !shadow-none"
+                connectText="Stake HANEUL"
+            />
+        );
+    }
+
+    if (!currentWalletSupportsStake) {
+        return (
+            <Button
+                size="lg"
+                variant="outline"
+                onClick={() => {
+                    // Always just assume we should connect to the first stake supported wallet for now:
+                    connect(stakeSupportedWallets[0].name);
+                }}
+            >
+                Stake HANEUL on a supported wallet
+            </Button>
+        );
+    }
+
+    return (
+        <Button
+            size="lg"
+            onClick={() => {
+                (
+                    (currentWallet as StandardWalletAdapter)
+                        .wallet as StakeWallet
+                ).features['haneulWallet:stake']?.stake({ validatorAddress: id! });
+            }}
+        >
+            Stake HANEUL
+        </Button>
+    );
+}
--- sdk/wallet-adapter/wallet-kit/src/index.tsx
+++ sdk/wallet-adapter/wallet-kit/src/index.tsx
@@ -1,6 +1,9 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+export { StandardWalletAdapter } from "@haneullabs/wallet-adapter-wallet-standard";
+export type { StandardWalletAdapterWallet, WalletWithFeatures } from '@haneullabs/wallet-standard';
+
 export * from "./ConnectButton";
 export * from "./ConnectModal";
 export * from "./WalletKitContext";
