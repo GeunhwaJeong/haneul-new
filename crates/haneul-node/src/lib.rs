a300864f9179f032c8acb48f34eab8967a31aed2
GeunhwaJeong <jeonggh892@gmail.com>
2023-02-08 11:49:49 +0100

    [objects pruner] live pruner support
    First iteration of live pruner.
    **Implementation**: `AuthorityStorePruner` is subscribed to updates from
    `CheckpointExecutor`
     
    Notes:
    * for now the same deletion method is used as in original pruner:
    `delete_range`. Eventually we may want to experiment with point-deletes.
    Starting with delete_range to address potential tombstone concern
    separately

--- crates/haneul-node/src/lib.rs
+++ crates/haneul-node/src/lib.rs
@@ -56,7 +56,7 @@ use haneul_storage::{
 use haneul_types::committee::Committee;
 use haneul_types::crypto::KeypairTraits;
 use haneul_types::quorum_driver_types::QuorumDriverEffectsQueueResult;
-use tokio::sync::broadcast;
+use tokio::sync::{broadcast, mpsc};
 use tokio::sync::{watch, Mutex};
 use tokio::task::JoinHandle;
 use tower::ServiceBuilder;
@@ -69,6 +69,7 @@ pub use handle::HaneulNodeHandle;
 use narwhal_config::SharedWorkerCache;
 use narwhal_types::TransactionsClient;
 use haneul_core::authority::authority_per_epoch_store::AuthorityPerEpochStore;
+use haneul_core::checkpoints::checkpoint_executor::CheckpointExecutionMessage;
 use haneul_core::checkpoints::{
     CheckpointMetrics, CheckpointService, CheckpointStore, SendCheckpointToStateSync,
     SubmitCheckpointToConsensus,
@@ -145,6 +146,7 @@ impl HaneulNode {
             &genesis_committee,
             None,
         ));
+        let (checkpoint_sender, checkpoint_receiver) = mpsc::channel(10);
         let store = Arc::new(
             AuthorityStore::open(
                 &config.db_path().join("store"),
@@ -152,6 +154,7 @@ impl HaneulNode {
                 genesis,
                 &committee_store,
                 &config.authority_store_pruning_config,
+                checkpoint_receiver,
             )
             .await?,
         );
@@ -307,7 +310,9 @@ impl HaneulNode {
         info!("HaneulNode started!");
         let node = Arc::new(node);
         let node_copy = node.clone();
-        spawn_monitored_task!(async move { Self::monitor_reconfiguration(node_copy).await });
+        spawn_monitored_task!(async move {
+            Self::monitor_reconfiguration(node_copy, checkpoint_sender).await
+        });
 
         Ok(node)
     }
@@ -721,7 +726,10 @@ impl HaneulNode {
 
     /// This function waits for a signal from the checkpoint executor to indicate that on-chain
     /// epoch has changed. Upon receiving such signal, we reconfigure the entire system.
-    pub async fn monitor_reconfiguration(self: Arc<Self>) -> Result<()> {
+    pub async fn monitor_reconfiguration(
+        self: Arc<Self>,
+        checkpoint_sender: mpsc::Sender<CheckpointExecutionMessage>,
+    ) -> Result<()> {
         let mut checkpoint_executor = CheckpointExecutor::new(
             self.state_sync.subscribe_to_synced_checkpoints(),
             self.checkpoint_store.clone(),
@@ -729,6 +737,7 @@ impl HaneulNode {
             self.state.transaction_manager().clone(),
             self.config.checkpoint_executor_config.clone(),
             &self.registry_service.default_registry(),
+            checkpoint_sender,
         );
 
         loop {
