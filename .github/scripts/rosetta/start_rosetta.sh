68792695a1ee228142a974a0d9b62867cf3a71d9
GeunhwaJeong <jeonggh892@gmail.com>
2023-02-08 14:01:37 -0800

    [haneul-rosetta] - default data directory to /data, and override fullnode's db dir
    CB is having issue restoring from snapshots, possibly due to data
    directory configuration issues, this PR defaults the data directory to
    /data and also override embedded haneul node's database directory to
    /data/haneul_db

--- .github/scripts/rosetta/start_rosetta.sh
+++ .github/scripts/rosetta/start_rosetta.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 echo "Start Rosetta online server"
-haneul-rosetta start-online-server &
+haneul-rosetta start-online-server --data-path ./data &
 
 echo "Start Rosetta offline server"
 haneul-rosetta start-offline-server &
