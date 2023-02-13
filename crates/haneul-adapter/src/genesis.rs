eea6153300c7bb4b64f815c92d79a0e9c7bffc7a
GeunhwaJeong <jeonggh892@gmail.com>
2023-02-13 11:36:11 -0800

    [2/x][adapter] Transactional tests include genesis objects
    Add genesis objects to the transactional test runner so that we can test
    functions involving them in isolation. Start by adding the `Clock`.
    
    This change bumps the Fake IDs of every object in existing tests by one,
    so the bulk of this change is updating those.
    
    ## Test Plan
    
    ```
    haneul-adapter-transactional-tests$ cargo nextest run
    ```
    
    ## Stack:
    
    -

--- crates/haneul-adapter/src/genesis.rs
+++ crates/haneul-adapter/src/genesis.rs
@@ -4,13 +4,17 @@
 use move_binary_format::CompiledModule;
 use once_cell::sync::Lazy;
 use haneul_types::base_types::{ObjectRef, HaneulAddress, TxContext};
-use haneul_types::HANEUL_FRAMEWORK_ADDRESS;
+use haneul_types::clock::Clock;
+use haneul_types::id::UID;
+use haneul_types::object::{MoveObject, Owner};
 use haneul_types::{base_types::TransactionDigest, object::Object};
+use haneul_types::{HANEUL_CLOCK_OBJECT_ID, HANEUL_CLOCK_OBJECT_SHARED_VERSION, HANEUL_FRAMEWORK_ADDRESS};
 
 static GENESIS: Lazy<Genesis> = Lazy::new(create_genesis_module_objects);
 
 struct Genesis {
     pub objects: Vec<Object>,
+    pub packages: Vec<Object>,
     pub modules: Vec<Vec<CompiledModule>>,
 }
 
@@ -19,12 +23,16 @@ pub fn clone_genesis_compiled_modules() -> Vec<Vec<CompiledModule>> {
 }
 
 pub fn clone_genesis_packages() -> Vec<Object> {
+    GENESIS.packages.clone()
+}
+
+pub fn clone_genesis_objects() -> Vec<Object> {
     GENESIS.objects.clone()
 }
 
 pub fn get_framework_object_ref() -> ObjectRef {
     GENESIS
-        .objects
+        .packages
         .iter()
         .find(|o| o.id() == HANEUL_FRAMEWORK_ADDRESS.into())
         .unwrap()
@@ -39,11 +47,46 @@ pub fn get_genesis_context() -> TxContext {
 fn create_genesis_module_objects() -> Genesis {
     let haneul_modules = haneul_framework::get_haneul_framework();
     let std_modules = haneul_framework::get_move_stdlib();
-    // unwraps safe because genesis packages should never exceed max size
-    let objects = vec![
+    let objects = vec![create_clock()];
+    // SAFETY: unwraps safe because genesis packages should never exceed max size
+    let packages = vec![
         Object::new_package(std_modules.clone(), TransactionDigest::genesis()).unwrap(),
         Object::new_package(haneul_modules.clone(), TransactionDigest::genesis()).unwrap(),
     ];
     let modules = vec![std_modules, haneul_modules];
-    Genesis { objects, modules }
+    Genesis {
+        objects,
+        packages,
+        modules,
+    }
+}
+
+fn create_clock() -> Object {
+    // SAFETY: unwrap safe because genesis objects should be serializable
+    let contents = bcs::to_bytes(&Clock {
+        id: UID::new(HANEUL_CLOCK_OBJECT_ID),
+        timestamp_ms: 0,
+    })
+    .unwrap();
+
+    // SAFETY: Whether `Clock` has public transfer or not is statically known, and unwrap safe
+    // because genesis objects should never exceed max size
+    let move_object = unsafe {
+        let has_public_transfer = false;
+        MoveObject::new_from_execution(
+            Clock::type_(),
+            has_public_transfer,
+            HANEUL_CLOCK_OBJECT_SHARED_VERSION,
+            contents,
+        )
+        .unwrap()
+    };
+
+    Object::new_move(
+        move_object,
+        Owner::Shared {
+            initial_shared_version: HANEUL_CLOCK_OBJECT_SHARED_VERSION,
+        },
+        TransactionDigest::genesis(),
+    )
 }
