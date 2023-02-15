9eba2ebc2a6800297ef92e6609cda5df1e7b1359
GeunhwaJeong <jeonggh892@gmail.com>
2023-02-15 21:01:40 +0000

    wallet-ext: accounts settings view create new account from mnemonic
    https://user-images.githubusercontent.com/10210143/218893872-378b65d4-18cd-49bd-92a0-f5fdb1a57fc6.mov
    
    closes APPS-287

--- apps/wallet/src/shared/messaging/messages/payloads/keyring/index.ts
+++ apps/wallet/src/shared/messaging/messages/payloads/keyring/index.ts
@@ -57,6 +57,10 @@ type MethodToPayloads = {
         args: { address: HaneulAddress };
         return: void;
     };
+    deriveNextAccount: {
+        args: void;
+        return: void;
+    };
 };
 
 export interface KeyringPayload<Method extends keyof MethodToPayloads>
