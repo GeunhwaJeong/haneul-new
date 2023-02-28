2beef013e85302c935f59c9d071f5e6a6ff80ede
GeunhwaJeong <jeonggh892@gmail.com>
2023-02-28 12:39:01 -0500

    [apps][part 1] add common utilities to haneullabs/core
    ## Description 
    
    This PR consolidates some duplicated frontend utilities like
    `SentryRpcClient` and `useFormatCoin` by moving them into
    `haneullabs/core`. I also added all the typical stuff like linting,
    formatting, testing, etc. to the library which is by no means perfect
    but a good starting point for reducing duplication across our
    applications. Specifically, I think we could look into sharing basic
    ESLint, Prettier, and TypeScript settings to provide more consistency on
    the developer experience front but that's for another day 😛
    
    This is just part 1 where we're adding the utilities to `haneullabs/core`;
    we'll actually use the utilities in
    https://github.com/GeunhwaJeong/haneul/pull/8632 since there were a lot of
    files to change.
    
    ## Test Plan 
    - Unit tests for `useFormatCoin` in the wallet and explorer were
    identical albeit not fully comprehensive
    - Did some spot-checking across the wallet and explorer to make price
    formatting works as intended
    - Tested all the npm scripts work for linting and testing

---
+++ apps/core/README.md
@@ -0,0 +1,3 @@
+# `@haneullabs/core`
+
+This JavaScript library contains helper utilities meant to be used across Haneullabs Lab's frontend applications.
---
+++ apps/core/src/hooks/__tests__/useFormatCoin.test.ts
@@ -0,0 +1,74 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import BigNumber from 'bignumber.js';
+import { describe, it, expect } from 'vitest';
+
+import { formatBalance, CoinFormat } from '../useFormatCoin';
+
+const HANEUL_DECIMALS = 9;
+
+function toMist(haneul: string) {
+    return new BigNumber(haneul).shiftedBy(HANEUL_DECIMALS).toString();
+}
+
+describe('formatBalance', () => {
+    it('formats zero amounts correctly', () => {
+        expect(formatBalance('0', 0)).toEqual('0');
+        expect(formatBalance('0', HANEUL_DECIMALS)).toEqual('0');
+    });
+
+    it('formats decimal amounts correctly', () => {
+        expect(formatBalance('0', HANEUL_DECIMALS)).toEqual('0');
+        expect(formatBalance('0.000', HANEUL_DECIMALS)).toEqual('0');
+    });
+
+    it('formats integer amounts correctly', () => {
+        expect(formatBalance(toMist('1'), HANEUL_DECIMALS)).toEqual('1');
+        expect(formatBalance(toMist('1.0001'), HANEUL_DECIMALS)).toEqual('1');
+        expect(formatBalance(toMist('1.1201'), HANEUL_DECIMALS)).toEqual('1.12');
+        expect(formatBalance(toMist('1.1234'), HANEUL_DECIMALS)).toEqual('1.123');
+        expect(formatBalance(toMist('1.1239'), HANEUL_DECIMALS)).toEqual('1.123');
+
+        expect(formatBalance(toMist('9999.9999'), HANEUL_DECIMALS)).toEqual(
+            '9,999.999'
+        );
+        // 10k + handling:
+        expect(formatBalance(toMist('10000'), HANEUL_DECIMALS)).toEqual('10 K');
+        expect(formatBalance(toMist('12345'), HANEUL_DECIMALS)).toEqual(
+            '12.345 K'
+        );
+        // Millions:
+        expect(formatBalance(toMist('1234000'), HANEUL_DECIMALS)).toEqual(
+            '1.234 M'
+        );
+        // Billions:
+        expect(formatBalance(toMist('1234000000'), HANEUL_DECIMALS)).toEqual(
+            '1.234 B'
+        );
+    });
+
+    it('formats integer amounts with full CoinFormat', () => {
+        expect(
+            formatBalance(toMist('1'), HANEUL_DECIMALS, CoinFormat.FULL)
+        ).toEqual('1');
+        expect(
+            formatBalance(toMist('1.123456789'), HANEUL_DECIMALS, CoinFormat.FULL)
+        ).toEqual('1.123456789');
+        expect(
+            formatBalance(toMist('9999.9999'), HANEUL_DECIMALS, CoinFormat.FULL)
+        ).toEqual('9,999.9999');
+        expect(
+            formatBalance(toMist('10000'), HANEUL_DECIMALS, CoinFormat.FULL)
+        ).toEqual('10,000');
+        expect(
+            formatBalance(toMist('12345'), HANEUL_DECIMALS, CoinFormat.FULL)
+        ).toEqual('12,345');
+        expect(
+            formatBalance(toMist('1234000'), HANEUL_DECIMALS, CoinFormat.FULL)
+        ).toEqual('1,234,000');
+        expect(
+            formatBalance(toMist('1234000000'), HANEUL_DECIMALS, CoinFormat.FULL)
+        ).toEqual('1,234,000,000');
+    });
+});
