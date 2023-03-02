9c85102302f66b887ffbac78ca75479e5c96981e
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-02 10:51:02 -0700

    [transactional tests] Fixed missing input object checks
    ## Description 
    
    Input checker was not run before execution in transactional tests. The
    programmable transaction runtime relies on these checks
    
    ## Test Plan 
    
    Just test fixes 
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-transactional-test-runner/src/test_adapter.rs
+++ crates/haneul-transactional-test-runner/src/test_adapter.rs
@@ -36,6 +36,7 @@ use std::{
 };
 use haneul_adapter::execution_engine;
 use haneul_adapter::{adapter::new_move_vm, execution_mode};
+use haneul_core::transaction_input_checker::check_objects;
 use haneul_framework::DEFAULT_FRAMEWORK_PATH;
 use haneul_protocol_config::ProtocolConfig;
 use haneul_types::clock::Clock;
@@ -49,9 +50,7 @@ use haneul_types::{
     crypto::{get_key_pair_from_rng, AccountKeyPair},
     event::Event,
     gas,
-    messages::{
-        ExecutionStatus, InputObjects, TransactionData, TransactionEffects, VerifiedTransaction,
-    },
+    messages::{ExecutionStatus, TransactionData, TransactionEffects, VerifiedTransaction},
     object::{self, Object, ObjectFormatOptions, GAS_VALUE_FOR_TESTING},
     object::{MoveObject, Owner},
     MOVE_STDLIB_ADDRESS, HANEUL_CLOCK_OBJECT_ID, HANEUL_CLOCK_OBJECT_SHARED_VERSION,
@@ -563,9 +562,12 @@ impl<'a> HaneulTestAdapter<'a> {
             gas::start_gas_metering(gas_budget, 1, 1, HaneulCostTable::new(&PROTOCOL_CONSTANTS))
                 .unwrap()
         };
-
+        transaction
+            .data()
+            .transaction_data()
+            .validity_check(&PROTOCOL_CONSTANTS)?;
         let transaction_digest = TransactionDigest::new(self.rng.gen());
-        let objects_by_kind = transaction
+        let (input_objects, objects) = transaction
             .data()
             .intent_message
             .value
@@ -577,8 +579,12 @@ impl<'a> HaneulTestAdapter<'a> {
                 let obj = self.storage.get_object(&id)?.clone();
                 Some((kind, obj))
             })
-            .collect::<Vec<_>>();
-        let input_objects = InputObjects::new(objects_by_kind);
+            .unzip();
+        let input_objects = check_objects(
+            transaction.data().transaction_data(),
+            input_objects,
+            objects,
+        )?;
         let transaction_dependencies = input_objects.transaction_dependencies();
         let shared_object_refs: Vec<_> = input_objects.filter_shared_objects();
         let temporary_store = TemporaryStore::new(
