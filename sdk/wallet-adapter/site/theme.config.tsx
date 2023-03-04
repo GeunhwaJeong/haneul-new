5d69b62a3462c06f3bb38781d34524a018deeb4a
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-03 16:45:48 -0800

    [wallet-kit] Add wallet kit docs site

---
+++ sdk/wallet-adapter/site/theme.config.tsx
@@ -0,0 +1,21 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>Haneul Wallet Kit</span>,
+  project: {
+    link: "https://github.com/GeunhwaJeong/haneul/tree/main/sdk/wallet-adapter",
+  },
+  chat: {
+    link: "https://discord.com/invite/Haneul",
+  },
+  docsRepositoryBase:
+    "https://github.com/GeunhwaJeong/haneul/tree/main/sdk/wallet-adapter",
+  footer: {
+    text: "Copyright © 2023, Haneul Labs, Inc.",
+  },
+};
+
+export default config;
