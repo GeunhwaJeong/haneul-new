6461d7233ff3deffbc8618731f53d1d4d8e51727
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-06 14:19:02 -0800

    [move][haneul-verifier] Flip ID Leak rules
    - Instead of checking leaks at unpack, we check them at pack.
    - The rule for this is that the `id` field of an object must come from a
    set of approved functions, ostensibly just `object::new`
    - This is quite the breaking change, but opens up the door for more
    complicated dynamic field behavior, since you can keep the UID around
    after the object is unpacked.

--- crates/haneul-verifier/src/private_generics.rs
+++ crates/haneul-verifier/src/private_generics.rs
@@ -13,9 +13,8 @@ use move_binary_format::{
 use move_core_types::{account_address::AccountAddress, ident_str, identifier::IdentStr};
 use haneul_types::{error::ExecutionError, HANEUL_FRAMEWORK_ADDRESS};
 
-use crate::{format_signature_token, verification_failure};
+use crate::{format_signature_token, verification_failure, TEST_SCENARIO_MODULE_NAME};
 
-const TEST_SCENARIO_MODULE_NAME: &str = "test_scenario";
 pub const TRANSFER_MODULE: &IdentStr = ident_str!("transfer");
 pub const EVENT_MODULE: &IdentStr = ident_str!("event");
 pub const TRANSFER_FUNCTIONS: &[&IdentStr] = &[
