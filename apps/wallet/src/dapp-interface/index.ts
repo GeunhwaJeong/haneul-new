649da7f24022e5c4254ac8cab2c03b25854c700c
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-07 10:52:42 -0600

    [wallet-ext] Remove legacy dapp interface
    ## Description 
    
    This removes the `window.haneulWallet` interface. This will force
    integrations through the wallet standard, but will simplify things for
    us internally with the transaction rework.
    
    ## Test Plan 
    
    Wallet continues to build.

--- apps/wallet/src/dapp-interface/index.ts
+++ apps/wallet/src/dapp-interface/index.ts
@@ -3,26 +3,6 @@
 
 import { registerWallet } from '@haneullabs/wallet-standard';
 
-import { DAppInterface } from './DAppInterface';
 import { HaneulWallet } from './WalletStandardInterface';
 
 registerWallet(new HaneulWallet());
-
-try {
-    let lazyDappInterface: DAppInterface | null = null;
-    Object.defineProperty(window, 'haneulWallet', {
-        enumerable: false,
-        configurable: false,
-        get() {
-            if (!lazyDappInterface) {
-                lazyDappInterface = new DAppInterface();
-            }
-            return lazyDappInterface;
-        },
-    });
-} catch (e) {
-    // eslint-disable-next-line no-console
-    console.warn(
-        '[haneul-wallet] Unable to attach to window.haneulWallet. There are likely multiple copies of the Haneul Wallet installed.'
-    );
-}
