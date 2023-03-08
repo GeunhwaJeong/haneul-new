0e4b1501c38951ea71e672d337905c15dcddda0d
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-08 16:06:38 +0000

    [indexer] - store bcs bytes for object and packages instead of raw json
    ## Description 
    
    store bcs bytes for object and packages instead of raw json
    
    ## Test Plan 
    
    Manual tested locally confirming object bytes is written into the
    database
    <img width="1100" alt="image"
    src="https://user-images.githubusercontent.com/1888654/223546975-0a141213-2317-49a7-94ee-f32e5b8e0fda.png">

--- crates/haneul-indexer/src/models/packages.rs
+++ crates/haneul-indexer/src/models/packages.rs
@@ -6,37 +6,31 @@ use crate::schema::packages;
 
 use diesel::prelude::*;
 
-use haneul_json_rpc_types::HaneulMovePackage;
-use haneul_types::base_types::{ObjectID, HaneulAddress};
+use crate::models::objects::NamedBcsBytes;
+use haneul_json_rpc_types::HaneulRawMovePackage;
+use haneul_types::base_types::HaneulAddress;
 
 #[derive(Queryable, Insertable, Debug, Identifiable)]
-#[diesel(table_name = packages)]
+#[diesel(table_name = packages, primary_key(package_id, version))]
 pub struct Package {
-    pub id: Option<i64>,
     pub package_id: String,
+    pub version: i64,
     pub author: String,
-    pub module_names: Vec<String>,
-    pub package_content: String,
+    pub data: Vec<NamedBcsBytes>,
 }
 
 impl Package {
-    pub fn try_from(
-        id: ObjectID,
-        sender: HaneulAddress,
-        package: &HaneulMovePackage,
-    ) -> Result<Self, IndexerError> {
+    pub fn try_from(sender: HaneulAddress, package: &HaneulRawMovePackage) -> Result<Self, IndexerError> {
         Ok(Self {
-            id: None,
-            package_id: id.to_hex(),
+            package_id: package.id.to_string(),
+            version: package.version.value() as i64,
             author: sender.to_string(),
-            module_names: package.disassembled.keys().cloned().collect(),
-            // TODO: store raw package bytes instead when object refactoring is done.
-            package_content: serde_json::to_string(&package.disassembled).map_err(|err| {
-                IndexerError::InsertableParsingError(format!(
-                    "Failed converting package module map to JSON with error: {:?}",
-                    err
-                ))
-            })?,
+            data: package
+                .module_map
+                .clone()
+                .into_iter()
+                .map(|(k, v)| NamedBcsBytes(k, v))
+                .collect(),
         })
     }
 }
