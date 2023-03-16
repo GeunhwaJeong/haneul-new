4d1bd060180e4ad7d19b0a8c8acffc3f466a37ca
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-15 20:06:08 -0700

    add make committee info

--- crates/haneul/src/validator_commands.rs
+++ crates/haneul/src/validator_commands.rs
@@ -1,40 +1,56 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+use move_core_types::ident_str;
+use multiaddr::Multiaddr;
 use std::{
-    fs,
     fmt::{Debug, Display, Formatter, Write},
+    fs,
     path::PathBuf,
 };
-use move_core_types::ident_str;
 use haneul_config::genesis::GenesisValidatorInfo;
 
-use crate::client_commands::{WalletContext, write_transaction_response};
+use crate::client_commands::{write_transaction_response, WalletContext};
 use crate::fire_drill::get_gas_obj_ref;
 use clap::*;
 use colored::Colorize;
 use fastcrypto::traits::KeyPair;
-use fastcrypto::{
-    traits::ToFromBytes,
-};
+use fastcrypto::traits::ToFromBytes;
 use serde::Serialize;
-use haneul_types::{HANEUL_FRAMEWORK_OBJECT_ID, HANEUL_SYSTEM_OBJ_CALL_ARG};
-
 use shared_crypto::intent::Intent;
-use haneul_json_rpc_types::{
-    HaneulTransactionResponse, HaneulTransactionResponseOptions
-};
+use haneul_json_rpc_types::{HaneulTransactionResponse, HaneulTransactionResponseOptions};
 use haneul_keys::keystore::AccountKeystore;
+use haneul_keys::{
+    key_derive::generate_new_key,
+    keypair_file::{
+        read_authority_keypair_from_file, read_keypair_from_file, read_network_keypair_from_file,
+        write_authority_keypair_to_file, write_keypair_to_file,
+    },
+};
 use haneul_sdk::HaneulClient;
-use haneul_types::crypto::{generate_proof_of_possession, get_authority_key_pair, AuthorityPublicKeyBytes};
-use haneul_types::messages::{CallArg, TransactionData};
+use haneul_types::crypto::{
+    generate_proof_of_possession, get_authority_key_pair, AuthorityPublicKeyBytes,
+};
 use haneul_types::messages::Transaction;
+use haneul_types::messages::{CallArg, TransactionData};
+use haneul_types::{
+    crypto::{AuthorityKeyPair, NetworkKeyPair, SignatureScheme, HaneulKeyPair},
+    HANEUL_FRAMEWORK_OBJECT_ID, HANEUL_SYSTEM_OBJ_CALL_ARG,
+};
 use tracing::info;
-
 
 #[derive(Parser)]
 #[clap(rename_all = "kebab-case")]
 pub enum HaneulValidatorCommand {
+    #[clap(name = "make-validator-info")]
+    MakeValidatorInfo {
+        name: String,
+        description: String,
+        image_url: String,
+        project_url: String,
+        host_name: String,
+        gas_price: u64,
+    },
     #[clap(name = "become-candidate")]
     BecomeCandidate {
         #[clap(name = "validator-info-path")]
@@ -49,9 +65,43 @@ pub enum HaneulValidatorCommand {
 #[derive(Serialize)]
 #[serde(untagged)]
 pub enum HaneulValidatorCommandResponse {
+    MakeValidatorInfo,
     BecomeCandidate(HaneulTransactionResponse),
     JoinCommittee(HaneulTransactionResponse),
     LeaveCommittee(HaneulTransactionResponse),
+}
+
+fn make_key_files(
+    file_name: PathBuf,
+    is_protocol_key: bool,
+    key: Option<HaneulKeyPair>,
+) -> anyhow::Result<()> {
+    if file_name.exists() {
+        println!("Use existing {:?} key file.", file_name);
+        return Ok(());
+    } else if is_protocol_key {
+        let (_, keypair) = get_authority_key_pair();
+        write_authority_keypair_to_file(&keypair, file_name.clone())?;
+        println!("Generated new key file: {:?}.", file_name);
+    } else {
+        let kp = match key {
+            Some(key) => {
+                println!(
+                    "Generated new key file {:?} based on haneul.keystore file.",
+                    file_name
+                );
+                key
+            }
+            None => {
+                let (_, kp, _, _) = generate_new_key(SignatureScheme::ED25519, None)?;
+                println!("Generated new key file: {:?}.", file_name);
+                kp
+            }
+        };
+        write_keypair_to_file(&kp, &file_name)?;
+    }
+    println!("Generated new key file: {:?}.", file_name);
+    Ok(())
 }
 
 impl HaneulValidatorCommand {
@@ -60,37 +110,128 @@ impl HaneulValidatorCommand {
         context: &mut WalletContext,
     ) -> Result<HaneulValidatorCommandResponse, anyhow::Error> {
         let client = context.get_client().await?;
+        let haneul_address = context.active_address()?;
         let ret = Ok(match self {
-            HaneulValidatorCommand::BecomeCandidate {
-                file
+            HaneulValidatorCommand::MakeValidatorInfo {
+                name,
+                description,
+                image_url,
+                project_url,
+                host_name,
+                gas_price,
             } => {
+                let dir = std::env::current_dir()?;
+                let protocol_key_file_name = dir.join("protocol.key");
+                let account_key = match context.config.keystore.get_key(&haneul_address)? {
+                    HaneulKeyPair::Ed25519(account_key) => HaneulKeyPair::Ed25519(account_key.copy()),
+                    _ => panic!(
+                        "Other account key types supported yet, please use Ed25519 keys for now."
+                    ),
+                };
+                let account_key_file_name = dir.join("account.key");
+                let network_key_file_name = dir.join("network.key");
+                let worker_key_file_name = dir.join("worker.key");
+                make_key_files(protocol_key_file_name.clone(), true, None)?;
+                make_key_files(account_key_file_name.clone(), false, Some(account_key))?;
+                make_key_files(network_key_file_name.clone(), false, None)?;
+                make_key_files(worker_key_file_name.clone(), false, None)?;
+
+                let keypair: AuthorityKeyPair =
+                    read_authority_keypair_from_file(protocol_key_file_name)?;
+                let account_keypair: HaneulKeyPair = read_keypair_from_file(account_key_file_name)?;
+                let worker_keypair: NetworkKeyPair =
+                    read_network_keypair_from_file(network_key_file_name)?;
+                let network_keypair: NetworkKeyPair =
+                    read_network_keypair_from_file(worker_key_file_name)?;
+                let pop =
+                    generate_proof_of_possession(&keypair, (&account_keypair.public()).into());
+                let validator_info = GenesisValidatorInfo {
+                    info: haneul_config::ValidatorInfo {
+                        name,
+                        protocol_key: keypair.public().into(),
+                        worker_key: worker_keypair.public().clone(),
+                        account_key: account_keypair.public(),
+                        network_key: network_keypair.public().clone(),
+                        gas_price,
+                        commission_rate: 0,
+                        network_address: Multiaddr::try_from(format!(
+                            "/dns/{}/tcp/8080/http",
+                            host_name
+                        ))?,
+                        p2p_address: Multiaddr::try_from(format!("/dns/{}/udp/8084", host_name))?,
+                        narwhal_primary_address: Multiaddr::try_from(format!(
+                            "/dns/{}/udp/8081",
+                            host_name
+                        ))?,
+                        narwhal_worker_address: Multiaddr::try_from(format!(
+                            "/dns/{}/udp/8082",
+                            host_name
+                        ))?,
+                        description,
+                        image_url,
+                        project_url,
+                    },
+                    proof_of_possession: pop,
+                };
+                // TODO set key files permisssion
+                let validator_info_file_name = dir.join("validator.info");
+                let validator_info_bytes = serde_yaml::to_vec(&validator_info)?;
+                fs::write(validator_info_file_name.clone(), validator_info_bytes)?;
+                println!(
+                    "Generated validator info file: {:?}.",
+                    validator_info_file_name
+                );
+                HaneulValidatorCommandResponse::MakeValidatorInfo
+            }
+            HaneulValidatorCommand::BecomeCandidate { file } => {
                 let validator_info_bytes = fs::read(file)?;
                 // Note: we should probably rename the struct or evolve it accordingly.
                 let validator_info: GenesisValidatorInfo =
                     serde_yaml::from_slice(&validator_info_bytes)?;
                 let validator = validator_info.info;
 
-                // FIXME remove these
-                let new_protocol_key_pair = get_authority_key_pair().1;
-                let pop = generate_proof_of_possession(&new_protocol_key_pair, context.active_address()?);
-
                 let args = vec![
-                    CallArg::Pure(bcs::to_bytes(&AuthorityPublicKeyBytes::from_bytes(new_protocol_key_pair.public().as_bytes())?).unwrap()),
-                    CallArg::Pure(bcs::to_bytes(&validator.network_key().as_bytes().to_vec()).unwrap()),
-                    CallArg::Pure(bcs::to_bytes(&validator.worker_key().as_bytes().to_vec()).unwrap()),
-                    CallArg::Pure(bcs::to_bytes(&pop.as_ref().to_vec()).unwrap()),
-                    CallArg::Pure(bcs::to_bytes(&validator.name().to_owned().into_bytes()).unwrap()),
-                    CallArg::Pure(bcs::to_bytes(&validator.description.clone().into_bytes()).unwrap()),
-                    CallArg::Pure(bcs::to_bytes(&validator.image_url.clone().into_bytes()).unwrap()),
-                    CallArg::Pure(bcs::to_bytes(&validator.project_url.clone().into_bytes()).unwrap()),
+                    CallArg::Pure(
+                        bcs::to_bytes(&AuthorityPublicKeyBytes::from_bytes(
+                            validator.protocol_key().as_bytes(),
+                        )?)
+                        .unwrap(),
+                    ),
+                    CallArg::Pure(
+                        bcs::to_bytes(&validator.network_key().as_bytes().to_vec()).unwrap(),
+                    ),
+                    CallArg::Pure(
+                        bcs::to_bytes(&validator.worker_key().as_bytes().to_vec()).unwrap(),
+                    ),
+                    CallArg::Pure(
+                        bcs::to_bytes(&validator_info.proof_of_possession.as_ref().to_vec())
+                            .unwrap(),
+                    ),
+                    CallArg::Pure(
+                        bcs::to_bytes(&validator.name().to_owned().into_bytes()).unwrap(),
+                    ),
+                    CallArg::Pure(
+                        bcs::to_bytes(&validator.description.clone().into_bytes()).unwrap(),
+                    ),
+                    CallArg::Pure(
+                        bcs::to_bytes(&validator.image_url.clone().into_bytes()).unwrap(),
+                    ),
+                    CallArg::Pure(
+                        bcs::to_bytes(&validator.project_url.clone().into_bytes()).unwrap(),
+                    ),
                     CallArg::Pure(bcs::to_bytes(&validator.network_address().to_vec()).unwrap()),
                     CallArg::Pure(bcs::to_bytes(&validator.p2p_address().to_vec()).unwrap()),
-                    CallArg::Pure(bcs::to_bytes(&validator.narwhal_primary_address().to_vec()).unwrap()),
-                    CallArg::Pure(bcs::to_bytes(&validator.narwhal_worker_address().to_vec()).unwrap()),
+                    CallArg::Pure(
+                        bcs::to_bytes(&validator.narwhal_primary_address().to_vec()).unwrap(),
+                    ),
+                    CallArg::Pure(
+                        bcs::to_bytes(&validator.narwhal_worker_address().to_vec()).unwrap(),
+                    ),
                     CallArg::Pure(bcs::to_bytes(&validator.gas_price()).unwrap()),
                     CallArg::Pure(bcs::to_bytes(&validator.commission_rate()).unwrap()),
                 ];
-                let response = call_0x5(context, "request_add_validator_candidate", args, &client).await?;
+                let response =
+                    call_0x5(context, "request_add_validator_candidate", args, &client).await?;
                 HaneulValidatorCommandResponse::BecomeCandidate(response)
             }
 
@@ -100,13 +241,13 @@ impl HaneulValidatorCommand {
             }
 
             HaneulValidatorCommand::LeaveCommittee => {
-                let response = call_0x5(context, "request_remove_validator", vec![], &client).await?;
+                let response =
+                    call_0x5(context, "request_remove_validator", vec![], &client).await?;
                 HaneulValidatorCommandResponse::LeaveCommittee(response)
             }
         });
         ret
     }
-
 }
 
 async fn call_0x5(
@@ -119,8 +260,12 @@ async fn call_0x5(
     let gas_obj_ref = get_gas_obj_ref(sender, haneul_client).await?;
     let mut args = vec![HANEUL_SYSTEM_OBJ_CALL_ARG];
     args.extend(call_args);
-    let rgp = haneul_client.governance_api().get_reference_gas_price().await?;
-    let gas_budget = 2000 * rgp;
+    let rgp = haneul_client
+        .governance_api()
+        .get_reference_gas_price()
+        .await?;
+    // 10k is a herustic number for gas unit
+    let gas_budget = 10_000 * rgp;
     let tx_data = TransactionData::new_move_call(
         sender,
         HANEUL_FRAMEWORK_OBJECT_ID,
@@ -137,7 +282,8 @@ async fn call_0x5(
         .config
         .keystore
         .sign_secure(&sender, &tx_data, Intent::default())?;
-    let transaction = Transaction::from_data(tx_data, Intent::default(), vec![signature]).verify()?;
+    let transaction =
+        Transaction::from_data(tx_data, Intent::default(), vec![signature]).verify()?;
     haneul_client
         .quorum_driver()
         .execute_transaction(
@@ -153,6 +299,7 @@ impl Display for HaneulValidatorCommandResponse {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let mut writer = String::new();
         match self {
+            HaneulValidatorCommandResponse::MakeValidatorInfo => {}
             HaneulValidatorCommandResponse::BecomeCandidate(response) => {
                 write!(writer, "{}", write_transaction_response(response)?)?;
             }
@@ -171,7 +318,7 @@ impl Debug for HaneulValidatorCommandResponse {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let string = serde_json::to_string_pretty(self);
         let s = match string {
-            Ok(s) => format!("{s}"),
+            Ok(s) => s,
             Err(err) => format!("{err}").red().to_string(),
         };
         write!(f, "{}", s)
