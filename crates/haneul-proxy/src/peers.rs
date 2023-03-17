3c4bc8dd22b797a887619628afc4087f84f26990
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-17 14:00:30 -0500

    [haneul-proxy/ requested comments]
    Summary:
    
    * addressing comments from https://github.com/GeunhwaJeong/haneul/pull/9123
    
    Test Plan:
    
    cargo build/test/clippy
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-proxy/src/peers.rs
+++ crates/haneul-proxy/src/peers.rs
@@ -135,6 +135,9 @@ impl HaneulNodeProvider {
     }
 }
 
+/// extract will get the network pubkey bytes from a HaneulValidatorSummary type.  This type comes from a
+/// full node rpc result.  See get_validators for details.  The key here, if extracted successfully, will
+/// ultimately be stored in the allow list and let us communicate with those actual peers via tls.
 fn extract(summary: HaneulSystemStateSummary) -> impl Iterator<Item = (Ed25519PublicKey, HaneulPeer)> {
     summary.active_validators.into_iter().filter_map(|vm| {
         match Ed25519PublicKey::from_bytes(&vm.network_pubkey_bytes) {
