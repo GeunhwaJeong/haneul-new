1c939cdcbc8a0391058afa0a38623c5030db2311
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-17 14:59:34 -0700

    [Explorer]: update main table
    ## Description 
    
    - Update main table
    - Add a story for all icons
    
    https://haneullabs.atlassian.net/browse/APPS-641
    
    ### Success
    
    <img width="766" alt="Screenshot 2023-03-16 at 12 46 30 PM"
    src="https://user-images.githubusercontent.com/127577476/225738190-ea91c1da-11f1-43cd-a002-e6682c6e9a60.png">
    
    ### Failure
    
    ![Screenshot 2023-03-16 at 12 55 09
    AM](https://user-images.githubusercontent.com/127577476/225551660-ea3e5220-13f0-4278-85cb-db7d5cddea67.png)
    
    ### Loading
    
    ![Screenshot 2023-03-16 at 1 08 27
    AM](https://user-images.githubusercontent.com/127577476/225553967-0bbb4224-56bd-4f8a-a4c6-efcf5b37d813.png)
    
    
    ## Icons story
    
    ![Screenshot 2023-03-16 at 12 56 17
    AM](https://user-images.githubusercontent.com/127577476/225551688-96b9a2e2-6286-4c51-bf6a-c2832a3f01c1.png)
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    
    ---------

--- apps/explorer/src/components/transaction-card/TxCardUtils.tsx
+++ apps/explorer/src/components/transaction-card/TxCardUtils.tsx
@@ -2,30 +2,29 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { useFormatCoin } from '@haneullabs/core';
+import { X12 } from '@haneullabs/icons';
 import {
+    type ExecutionStatusType,
     getExecutionStatusType,
     getTotalGasUsed,
-    getTransactionKind,
     getTransactionDigest,
+    getTransactionKind,
     getTransactionKindName,
-    HANEUL_TYPE_ARG,
-    type GetTxnDigestsResponse,
-    type ExecutionStatusType,
-    type TransactionKindName,
-    type JsonRpcProvider,
     getTransactionSender,
-    formatDigest,
-    formatAddress,
+    type GetTxnDigestsResponse,
+    type JsonRpcProvider,
+    HANEUL_TYPE_ARG,
+    type TransactionKindName,
 } from '@haneullabs/haneul.js';
-import { Fragment } from 'react';
+import clsx from 'clsx';
+import { type ReactNode } from 'react';
 
-import { ReactComponent as ContentArrowRight } from '../../assets/SVGIcons/16px/ArrowRight.svg';
 import { getAmount } from '../../utils/getAmount';
 import { TxTimeType } from '../tx-time/TxTimeType';
 
 import styles from './RecentTxCard.module.css';
 
-import { AddressLink, ObjectLink, TransactionLink } from '~/ui/InternalLink';
+import { AddressLink, TransactionLink } from '~/ui/InternalLink';
 import { TransactionType } from '~/ui/TransactionType';
 
 export type TxnData = {
@@ -38,14 +37,6 @@ export type TxnData = {
     kind: TransactionKindName | undefined;
     From: string;
     timestamp_ms?: number;
-};
-
-type Category = 'object' | 'transaction' | 'address';
-
-export type LinkObj = {
-    url: string;
-    name?: string;
-    category?: Category;
 };
 
 export function HaneulAmount({
@@ -71,22 +62,26 @@ export function HaneulAmount({
     return <span className={styles.haneulamount}>--</span>;
 }
 
-export function TxAddresses({ content }: { content: LinkObj[] }) {
+function TxTableHeader({ label }: { label: string }) {
+    return <div className="pl-3">{label}</div>;
+}
+
+function TxTableCol({
+    isHighlightedOnHover,
+    children,
+}: {
+    isHighlightedOnHover?: boolean;
+    children: ReactNode;
+}) {
     return (
-        <section className={styles.addresses}>
-            {content.map((itm, idx) => (
-                <Fragment key={idx + itm.url}>
-                    {itm.category === 'address' ? (
-                        <AddressLink address={itm.url} />
-                    ) : itm.category === 'object' ? (
-                        <ObjectLink objectId={itm.url} />
-                    ) : (
-                        <TransactionLink digest={itm.url} />
-                    )}
-                    {idx !== content.length - 1 && <ContentArrowRight />}
-                </Fragment>
-            ))}
-        </section>
+        <div
+            className={clsx(
+                'flex h-full items-center rounded px-3',
+                isHighlightedOnHover && 'hover:bg-haneul-light'
+            )}
+        >
+            {children}
+        </div>
     );
 }
 
@@ -95,35 +90,18 @@ export const genTableDataFromTxData = (results: TxnData[]) => ({
     data: results.map((txn) => ({
         date: <TxTimeType timestamp={txn.timestamp_ms} />,
         transactionId: (
-            <TxAddresses
-                content={[
-                    {
-                        url: txn.txId,
-                        name: formatDigest(txn.txId),
-                        category: 'transaction',
-                    },
-                ]}
-            />
-        ),
-        addresses: (
-            <TxAddresses
-                content={[
-                    {
-                        url: txn.From,
-                        name: formatAddress(txn.From),
-                        category: 'address',
-                    },
-                    ...(txn.To
-                        ? [
-                              {
-                                  url: txn.To,
-                                  name: formatAddress(txn.To),
-                                  category: 'address',
-                              } as const,
-                          ]
-                        : []),
-                ]}
-            />
+            <TxTableCol isHighlightedOnHover>
+                <TransactionLink
+                    digest={txn.txId}
+                    before={
+                        txn.status === 'success' ? (
+                            <div className="h-2 w-2 rounded-full bg-success" />
+                        ) : (
+                            <X12 className="text-issue-dark" />
+                        )
+                    }
+                />
+            </TxTableCol>
         ),
         txTypes: (
             <TransactionType
@@ -131,34 +109,41 @@ export const genTableDataFromTxData = (results: TxnData[]) => ({
                 type={txn.kind}
             />
         ),
-        amounts: <HaneulAmount amount={txn.haneulAmount} />,
-        gas: <HaneulAmount amount={txn.txGas} />,
+        amounts: (
+            <TxTableCol>
+                <HaneulAmount amount={txn.haneulAmount} />
+            </TxTableCol>
+        ),
+        gas: (
+            <TxTableCol>
+                <HaneulAmount amount={txn.txGas} />
+            </TxTableCol>
+        ),
+        sender: (
+            <TxTableCol isHighlightedOnHover>
+                <AddressLink address={txn.From} />
+            </TxTableCol>
+        ),
     })),
     columns: [
         {
-            header: 'Type',
-            accessorKey: 'txTypes',
-        },
-        {
-            header: () => (
-                <div className={styles.addresses}>Transaction ID</div>
-            ),
+            header: () => <TxTableHeader label="Transaction ID" />,
             accessorKey: 'transactionId',
         },
         {
-            header: () => <div className={styles.addresses}>Addresses</div>,
-            accessorKey: 'addresses',
+            header: () => <TxTableHeader label="Sender" />,
+            accessorKey: 'sender',
         },
         {
-            header: 'Amount',
+            header: () => <TxTableHeader label="Amount" />,
             accessorKey: 'amounts',
         },
         {
-            header: 'Gas',
+            header: () => <TxTableHeader label="Gas" />,
             accessorKey: 'gas',
         },
         {
-            header: 'Time',
+            header: () => <TxTableHeader label="Time" />,
             accessorKey: 'date',
         },
     ],
