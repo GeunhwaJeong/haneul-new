8c5a42fecd2dc5bb34a52e5f4f48da8134d1fe4f
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-22 15:39:39 -0700

    [wallet-kit] Add site
    ## Description 
    
    Adding content of the wallet kit site.
    
    ## Test Plan 
    
    Ran locally

--- sdk/wallet-adapter/site/theme.config.tsx
+++ sdk/wallet-adapter/site/theme.config.tsx
@@ -1,21 +1,26 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>Haneul Wallet Kit</span>,
-  project: {
-    link: "https://github.com/GeunhwaJeong/haneul/tree/main/sdk/wallet-adapter",
-  },
-  chat: {
-    link: "https://discord.com/invite/Haneul",
-  },
-  docsRepositoryBase:
-    "https://github.com/GeunhwaJeong/haneul/tree/main/sdk/wallet-adapter",
-  footer: {
-    text: "Copyright © 2023, Haneul Labs, Inc.",
-  },
+    logo: <span>Haneul Wallet Kit</span>,
+    project: {
+        link: 'https://github.com/GeunhwaJeong/haneul/tree/main/sdk/wallet-adapter',
+    },
+    chat: {
+        link: 'https://discord.com/invite/Haneul',
+    },
+    docsRepositoryBase:
+        'https://github.com/GeunhwaJeong/haneul/tree/main/sdk/wallet-adapter',
+    footer: {
+        text: 'Copyright © 2023, Haneul Labs, Inc.',
+    },
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – Haneul Wallet Kit',
+        };
+    },
 };
 
 export default config;
