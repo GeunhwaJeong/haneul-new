39285949ca9a4a2d2eded8a3888d704cb235ce58
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-23 03:50:35 +0000

    do not return error when get package with display option
    ## Description 
    
    do not return error when get package with display option
    
    ## Test Plan 
    
    Changes coved by unit test

--- crates/haneul-json-rpc/src/unit_tests/rpc_server_tests.rs
+++ crates/haneul-json-rpc/src/unit_tests/rpc_server_tests.rs
@@ -66,6 +66,22 @@ async fn test_get_objects() -> Result<(), anyhow::Error> {
     Ok(())
 }
 
+#[tokio::test]
+async fn test_get_package_with_display_should_not_fail() -> Result<(), anyhow::Error> {
+    let cluster = TestClusterBuilder::new().build().await?;
+    let http_client = cluster.rpc_client();
+    let response = http_client
+        .get_object_with_options(
+            ObjectID::from(HANEUL_FRAMEWORK_ADDRESS),
+            Some(HaneulObjectDataOptions::new().with_display()),
+        )
+        .await;
+    assert!(response.is_ok());
+    let response: HaneulObjectResponse = response?;
+    assert!(response.into_object().unwrap().display.is_none());
+    Ok(())
+}
+
 #[sim_test]
 async fn test_public_transfer_object() -> Result<(), anyhow::Error> {
     let cluster = TestClusterBuilder::new().build().await?;
