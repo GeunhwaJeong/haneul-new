927622c00f1cc97e10fb02df85b0b116a8c5863a
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-30 19:58:15 -0400

    indexer: tx -> tx block leftovers
    ## Description 
    
    1. some tx -> tx block rename leftovers
    2. _with_options renaming leftovers
    3. add `query_events` to implemented method

--- crates/haneul-indexer/src/apis/read_api.rs
+++ crates/haneul-indexer/src/apis/read_api.rs
@@ -37,13 +37,13 @@ impl<S: IndexerStore> ReadApi<S> {
         }
     }
 
-    fn get_total_transaction_number_internal(&self) -> Result<u64, IndexerError> {
+    fn get_total_transaction_blocks_internal(&self) -> Result<u64, IndexerError> {
         self.state
             .get_total_transaction_number_from_checkpoints()
             .map(|n| n as u64)
     }
 
-    async fn get_transaction_with_options_internal(
+    async fn get_transaction_block_internal(
         &self,
         digest: &TransactionDigest,
         options: Option<HaneulTransactionBlockResponseOptions>,
@@ -63,7 +63,7 @@ impl<S: IndexerStore> ReadApi<S> {
         Ok(haneul_transaction_response)
     }
 
-    async fn multi_get_transactions_with_options_internal(
+    async fn multi_get_transaction_blocks_internal(
         &self,
         digests: &[TransactionDigest],
         options: Option<HaneulTransactionBlockResponseOptions>,
@@ -98,7 +98,7 @@ impl<S: IndexerStore> ReadApi<S> {
         Ok(haneul_tx_resp_vec)
     }
 
-    fn get_object_with_options_internal(
+    fn get_object_internal(
         &self,
         object_id: ObjectID,
         options: Option<HaneulObjectDataOptions>,
@@ -124,14 +124,11 @@ where
         object_id: ObjectID,
         options: Option<HaneulObjectDataOptions>,
     ) -> RpcResult<HaneulObjectResponse> {
-        if !self
-            .migrated_methods
-            .contains(&"get_object_with_options".into())
-        {
+        if !self.migrated_methods.contains(&"get_object".into()) {
             return self.fullnode.get_object(object_id, options).await;
         }
 
-        Ok(self.get_object_with_options_internal(object_id, options)?)
+        Ok(self.get_object_internal(object_id, options)?)
     }
 
     async fn multi_get_objects(
@@ -145,11 +142,11 @@ where
     async fn get_total_transaction_blocks(&self) -> RpcResult<BigInt> {
         if !self
             .migrated_methods
-            .contains(&"get_total_transaction_number".to_string())
+            .contains(&"get_total_transaction_blocks".to_string())
         {
             return self.fullnode.get_total_transaction_blocks().await;
         }
-        Ok(self.get_total_transaction_number_internal()?.into())
+        Ok(self.get_total_transaction_blocks_internal()?.into())
     }
 
     async fn get_transaction_block(
@@ -159,12 +156,12 @@ where
     ) -> RpcResult<HaneulTransactionBlockResponse> {
         if !self
             .migrated_methods
-            .contains(&"get_transaction".to_string())
+            .contains(&"get_transaction_block".to_string())
         {
             return self.fullnode.get_transaction_block(digest, options).await;
         }
         Ok(self
-            .get_transaction_with_options_internal(&digest, options)
+            .get_transaction_block_internal(&digest, options)
             .await?)
     }
 
@@ -175,7 +172,7 @@ where
     ) -> RpcResult<Vec<HaneulTransactionBlockResponse>> {
         if !self
             .migrated_methods
-            .contains(&"multi_get_transactions_with_options".to_string())
+            .contains(&"multi_get_transaction_blocks".to_string())
         {
             return self
                 .fullnode
@@ -183,7 +180,7 @@ where
                 .await;
         }
         Ok(self
-            .multi_get_transactions_with_options_internal(&digests, options)
+            .multi_get_transaction_blocks_internal(&digests, options)
             .await?)
     }
 
