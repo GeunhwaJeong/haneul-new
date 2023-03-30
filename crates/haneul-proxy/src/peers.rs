69d739e0d25c11e847e3b39a4001bdb01243f7b4
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-30 14:35:38 -0500

    [haneul-proxy/ cleanup handler/middleware]
    Summary:
    
    * cleanup the consumer.rs code
    * add a new middleware to decode the protobufs
    * improve error handling for rpc call to detect general rpc issues
    
    Test Plan:
    
    tested locally
    
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-proxy/src/peers.rs
+++ crates/haneul-proxy/src/peers.rs
@@ -88,17 +88,25 @@ impl HaneulNodeProvider {
             .body(request.to_string())
             .send()
             .await
-            .context("unable to perform rpc")?;
+            .context("unable to perform json rpc")?;
+
+        let raw = response
+            .bytes()
+            .await
+            .context("unable to extract body bytes from json rpc")?;
 
         #[derive(Debug, Deserialize)]
         struct ResponseBody {
             result: HaneulSystemStateSummary,
         }
 
-        let body = match response.json::<ResponseBody>().await {
+        let body: ResponseBody = match serde_json::from_slice(&raw) {
             Ok(b) => b,
             Err(error) => {
-                bail!("unable to decode json: {error}")
+                bail!(
+                    "unable to decode json: {error} response from json rpc: {:?}",
+                    raw
+                )
             }
         };
 
