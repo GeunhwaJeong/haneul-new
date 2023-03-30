842eed05f8f1889aebd13a9e50590760fb7e3822
GeunhwaJeong <jeonggh892@gmail.com>
2023-03-29 19:03:44 -0700

    Increase coin value and gas budget for large batch sizes
    ## Description 
    
    Increase gas coin budget and primary coin value to support large batch
    sizes
    ## Test Plan 
    
    Deployed in labnet

--- crates/haneul-core/src/test_utils.rs
+++ crates/haneul-core/src/test_utils.rs
@@ -285,6 +285,7 @@ pub fn make_pay_haneul_transaction(
     sender: HaneulAddress,
     keypair: &AccountKeyPair,
     gas_price: Option<u64>,
+    gas_budget: Option<u64>,
 ) -> VerifiedTransaction {
     let data = TransactionData::new_pay_haneul(
         sender,
@@ -292,7 +293,7 @@ pub fn make_pay_haneul_transaction(
         recipients,
         amounts,
         gas_object,
-        MAX_GAS,
+        gas_budget.unwrap_or(MAX_GAS),
         gas_price.unwrap_or(DUMMY_GAS_PRICE),
     )
     .unwrap();
