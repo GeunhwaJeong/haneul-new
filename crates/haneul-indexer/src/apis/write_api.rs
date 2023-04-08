5c62bebf6f805ac7e54a1081ac23d563cf53ad76
GeunhwaJeong <jeonggh892@gmail.com>
2023-04-08 12:34:42 -0400

    indexer: split FN sync worker and rpc server worker
    ## Description 
    
    Split the worker, as now it looks like indexer binary cannot do both FN
    sync & RPC serving very well in the same binary / K8
    
    Discussions are in inc 280.
    
    ## Test Plan 
    
    CI

--- crates/haneul-indexer/src/apis/write_api.rs
+++ crates/haneul-indexer/src/apis/write_api.rs
@@ -10,24 +10,33 @@ use jsonrpsee::RpcModule;
 use haneul_json_rpc::api::{WriteApiClient, WriteApiServer};
 use haneul_json_rpc::HaneulRpcModule;
 use haneul_json_rpc_types::{
-    DevInspectResults, DryRunTransactionBlockResponse, HaneulTransactionBlockEffectsAPI,
-    HaneulTransactionBlockResponse, HaneulTransactionBlockResponseOptions,
+    DevInspectResults,
+    DryRunTransactionBlockResponse,
+    // TODO(gegaowp): temp. disable fast-path
+    // HaneulTransactionBlockEffectsAPI,
+    HaneulTransactionBlockResponse,
+    HaneulTransactionBlockResponseOptions,
 };
 use haneul_open_rpc::Module;
 use haneul_types::base_types::HaneulAddress;
 use haneul_types::messages::ExecuteTransactionRequestType;
 use haneul_types::haneul_serde::BigInt;
 
-use crate::handlers::checkpoint_handler::{
-    fetch_changed_objects, get_deleted_db_objects, get_object_changes, to_changed_db_objects,
-};
-use crate::models::transactions::Transaction;
-use crate::store::{IndexerStore, TransactionObjectChanges};
-use crate::types::{
-    FastPathTransactionBlockResponse, HaneulTransactionBlockResponseWithOptions,
-    TemporaryTransactionBlockResponseStore,
-};
+// TODO(gegaowp): temp. disable fast-path
+// use crate::handlers::checkpoint_handler::{
+//     fetch_changed_objects, get_deleted_db_objects, get_object_changes, to_changed_db_objects,
+// };
+// use crate::models::transactions::Transaction;
+// use crate::store::{IndexerStore, TransactionObjectChanges};
+// use crate::types::{
+//         FastPathTransactionBlockResponse, HaneulTransactionBlockResponseWithOptions,
+//         TemporaryTransactionBlockResponseStore,
+//     };
+use crate::store::IndexerStore;
+use crate::types::HaneulTransactionBlockResponseWithOptions;
 
+// TODO(gegaowp): temp. disable fast-path and allow dead code.
+#[allow(dead_code)]
 pub(crate) struct WriteApi<S> {
     fullnode: HttpClient,
     state: S,
@@ -59,26 +68,28 @@ where
             .fullnode
             .execute_transaction_block(tx_bytes, signatures, Some(fast_path_options), request_type)
             .await?;
-        let fast_path_resp: FastPathTransactionBlockResponse =
-            haneul_transaction_response.clone().try_into()?;
-        let effects = &fast_path_resp.effects;
-        let epoch = effects.executed_epoch();
 
-        let object_changes = get_object_changes(effects);
-        let changed_objects = fetch_changed_objects(self.fullnode.clone(), object_changes).await?;
-        let changed_db_objects =
-            to_changed_db_objects(changed_objects, epoch, /* checkpoint */ None);
-        let deleted_db_objects = get_deleted_db_objects(effects, epoch, /* checkpoint */ None);
-        let tx_object_changes = TransactionObjectChanges {
-            changed_objects: changed_db_objects,
-            deleted_objects: deleted_db_objects,
-        };
+        // TODO(gegaowp): turn off DB commits on fast-path for now
+        // let fast_path_resp: FastPathTransactionBlockResponse =
+        //     haneul_transaction_response.clone().try_into()?;
+        // let effects = &fast_path_resp.effects;
+        // let epoch = effects.executed_epoch();
 
-        let transaction_store: TemporaryTransactionBlockResponseStore = fast_path_resp.into();
-        let transaction: Transaction = transaction_store.try_into()?;
-        self.state
-            .persist_fast_path(transaction, tx_object_changes)
-            .await?;
+        // let object_changes = get_object_changes(effects);
+        // let changed_objects = fetch_changed_objects(self.fullnode.clone(), object_changes).await?;
+        // let changed_db_objects =
+        //     to_changed_db_objects(changed_objects, epoch, /* checkpoint */ None);
+        // let deleted_db_objects = get_deleted_db_objects(effects, epoch, /* checkpoint */ None);
+        // let tx_object_changes = TransactionObjectChanges {
+        //     changed_objects: changed_db_objects,
+        //     deleted_objects: deleted_db_objects,
+        // };
+
+        // let transaction_store: TemporaryTransactionBlockResponseStore = fast_path_resp.into();
+        // let transaction: Transaction = transaction_store.try_into()?;
+        // self.state
+        //     .persist_fast_path(transaction, tx_object_changes)
+        //     .await?;
 
         Ok(HaneulTransactionBlockResponseWithOptions {
             response: haneul_transaction_response,
