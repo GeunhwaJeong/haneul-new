6fc814472ed6b3b8edb121febd169e386b0ba3d3
GeunhwaJeong <jeonggh892@gmail.com>
2023-04-12 07:50:31 -0700

    [explorer] paginated owned objects
    ## Description 
    
    Paginated owned objects for users.
    
    <img width="1028" alt="image"
    src="https://user-images.githubusercontent.com/128089541/230984702-1715b12d-a28c-4e5e-a496-7f361135d2dc.png">
    
    
    ## Test Plan 
    
    Tested against devnet address
    `0x1217d63b8c994e959943d35f7894edd462ee2b216c57f700873a3d80af0f86ef`
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    Pagination for owned objects.

--- apps/explorer/src/hooks/useGetOwnedObjects.ts
+++ apps/explorer/src/hooks/useGetOwnedObjects.ts
@@ -3,13 +3,15 @@
 
 import { useRpcClient } from '@haneullabs/core';
 import { type HaneulAddress } from '@haneullabs/haneul.js';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+const MAX_OBJECTS_PER_REQ = 6;
 
 export function useGetOwnedObjects(address?: HaneulAddress | null) {
     const rpc = useRpcClient();
-    return useQuery(
+    return useInfiniteQuery(
         ['get-owned-objects', address],
-        async () =>
+        async ({ pageParam }) =>
             await rpc.getOwnedObjects({
                 owner: address!,
                 options: {
@@ -17,7 +19,18 @@ export function useGetOwnedObjects(address?: HaneulAddress | null) {
                     showContent: true,
                     showDisplay: true,
                 },
+                limit: MAX_OBJECTS_PER_REQ,
+                cursor: pageParam ? pageParam.cursor : null,
             }),
-        { enabled: !!address }
+        {
+            staleTime: 10 * 60 * 1000,
+            enabled: !!address,
+            getNextPageParam: (lastPage) =>
+                lastPage?.hasNextPage
+                    ? {
+                          cursor: lastPage.nextCursor,
+                      }
+                    : false,
+        }
     );
 }
