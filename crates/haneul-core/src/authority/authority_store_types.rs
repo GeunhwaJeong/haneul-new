3835774e2e38fafd306c98e0865bbbeeefaefe39
GeunhwaJeong <jeonggh892@gmail.com>
2023-04-12 15:16:14 +0300

    [storage] store gas coin as a separate enum variant
    The supermajority of objects on the testnet are gas coins. 
    A typical gas coin consumes 128 bytes. 
    We can optimize this by removing the object_id and version fields from
    the gas coin values stored in the database, as those fields are also
    part of the key. Here is an example of two gas coins, one with the
    optimization and one without:
    ```
    V1(Value(StoreObjectValue { data: Coin(1), owner: AddressOwner(0xad4a51c549ea55befd70bce836e2128ca8d2f0cdfb7b6903fea68bbee0b94880), previous_transaction: TransactionDigest(6ahJ6gAGvVCRrKq23A1fjaUf4UPtrmgbToa6HXRtR62y), storage_rebate: 988000 }))
    
    number of bytes: 85
    
    V1(Value(StoreObjectValue { data: Move(MoveObject { type_: MoveObjectType(GasCoin), has_public_transfer: true, version: SequenceNumber(6), contents: [255, 234, 132, 30, 149, 70, 238, 175, 161, 161, 122, 199, 130, 103, 145, 104, 106, 84, 186, 29, 165, 32, 158, 79, 1, 107, 150, 24, 224, 57, 61, 114, 1, 0, 0, 0, 0, 0, 0, 0] }), owner: AddressOwner(0x90c677f11b65510bc05b5b27b510010904b1a1f51e91535191449d6ef7b183a7), previous_transaction: TransactionDigest(C1Mzco26AQPMwuLxT4jCFyP9XrA7ptUhxbzwf55o2inb), storage_rebate: 988000 }))
    
    number of bytes: 128
    ```
    We can save roughly 33% on the object table with this change. 
    
    The change is backward compatible, meaning all new gas coins will be
    written to the database as `StoreData::Coin`, while existing objects in
    the testnet will still be `Move(MoveObject)`
    
    ---------

--- crates/haneul-core/src/authority/authority_store_types.rs
+++ crates/haneul-core/src/authority/authority_store_types.rs
@@ -4,13 +4,14 @@
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
 use serde_with::Bytes;
-use std::convert::TryFrom;
 use haneul_types::base_types::MoveObjectType;
 use haneul_types::base_types::{ObjectDigest, SequenceNumber, TransactionDigest};
+use haneul_types::coin::Coin;
 use haneul_types::crypto::{default_hash, Signable};
 use haneul_types::error::HaneulError;
 use haneul_types::move_package::MovePackage;
 use haneul_types::object::{Data, MoveObject, Object, Owner};
+use haneul_types::storage::ObjectKey;
 
 pub type ObjectContentDigest = ObjectDigest;
 
@@ -105,6 +106,7 @@ pub enum StoreData {
     Move(MoveObject),
     Package(MovePackage),
     IndirectObject(IndirectObjectMetadata),
+    Coin(u64),
 }
 
 /// Metadata of stored moved object
@@ -215,6 +217,13 @@ pub(crate) fn get_store_object_pair(
                 let digest = move_object.digest();
                 indirect_object = Some(move_object);
                 StoreData::IndirectObject(IndirectObjectMetadata { version, digest })
+            } else if move_obj.type_().is_gas_coin() {
+                StoreData::Coin(
+                    Coin::from_bcs_bytes(move_obj.contents())
+                        .expect("failed to deserialize coin")
+                        .balance
+                        .value(),
+                )
             } else {
                 StoreData::Move(move_obj)
             }
@@ -232,38 +241,44 @@ pub(crate) fn get_store_object_pair(
     )
 }
 
-pub struct MigratedStoreObjectPair(pub StoreObjectValue, pub Option<StoreMoveObject>);
-impl TryFrom<MigratedStoreObjectPair> for Object {
-    type Error = HaneulError;
+pub(crate) fn try_construct_object(
+    object_key: &ObjectKey,
+    store_object: StoreObjectValue,
+    indirect_object: Option<StoreMoveObject>,
+) -> Result<Object, HaneulError> {
+    let data = match (store_object.data, indirect_object) {
+        (StoreData::Move(object), None) => Data::Move(object),
+        (StoreData::Package(package), None) => Data::Package(package),
+        (StoreData::IndirectObject(metadata), Some(indirect_obj)) => unsafe {
+            Data::Move(MoveObject::new_from_execution_with_limit(
+                indirect_obj.type_,
+                indirect_obj.has_public_transfer,
+                metadata.version,
+                indirect_obj.contents,
+                // verification is already done during initial execution
+                u64::MAX,
+            )?)
+        },
+        (StoreData::Coin(balance), None) => unsafe {
+            Data::Move(MoveObject::new_from_execution_with_limit(
+                MoveObjectType::gas_coin(),
+                true,
+                object_key.1,
+                bcs::to_bytes(&(object_key.0, balance)).expect("serialization failed"),
+                u64::MAX,
+            )?)
+        },
+        _ => {
+            return Err(HaneulError::StorageCorruptedFieldError(
+                "inconsistent object representation".to_string(),
+            ))
+        }
+    };
 
-    fn try_from(object: MigratedStoreObjectPair) -> Result<Self, Self::Error> {
-        let MigratedStoreObjectPair(store_object, indirect_object) = object;
-
-        let data = match (store_object.data, indirect_object) {
-            (StoreData::Move(object), None) => Data::Move(object),
-            (StoreData::Package(package), None) => Data::Package(package),
-            (StoreData::IndirectObject(metadata), Some(indirect_obj)) => unsafe {
-                Data::Move(MoveObject::new_from_execution_with_limit(
-                    indirect_obj.type_,
-                    indirect_obj.has_public_transfer,
-                    metadata.version,
-                    indirect_obj.contents,
-                    // verification is already done during initial execution
-                    u64::MAX,
-                )?)
-            },
-            _ => {
-                return Err(HaneulError::StorageCorruptedFieldError(
-                    "inconsistent object representation".to_string(),
-                ))
-            }
-        };
-
-        Ok(Self {
-            data,
-            owner: store_object.owner,
-            previous_transaction: store_object.previous_transaction,
-            storage_rebate: store_object.storage_rebate,
-        })
-    }
+    Ok(Object {
+        data,
+        owner: store_object.owner,
+        previous_transaction: store_object.previous_transaction,
+        storage_rebate: store_object.storage_rebate,
+    })
 }
