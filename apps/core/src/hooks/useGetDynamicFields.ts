eda9d488630249a67d4927d99ae22cc58be24378
GeunhwaJeong <jeonggh892@gmail.com>
2023-04-14 11:42:36 -0400

    Added DynamicField display
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    - Created Dynamic field card
    - Show DynamicFieldObject fields as the JSON blob
    - For Dynamic fields, scroll to load more data 
    - Added underlying object display view.
    - Created Prism code highlight component 
    - Reworked the TokenView component and removed the custom json view,
    removed the ObjectView component
    
    
    
    ## Test Plan 
    
    <img width="1302" alt="Screenshot 2023-04-12 at 2 07 43 PM"
    src="https://user-images.githubusercontent.com/126525197/231546212-386d0b8a-facb-44dc-a929-d928b993122e.png">
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [x] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ apps/core/src/hooks/useGetDynamicFields.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useRpcClient } from '../api/RpcClientContext';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { normalizeHaneulAddress } from '@haneullabs/haneul.js';
+
+const MAX_PAGE_SIZE = 10;
+
+export function useGetDynamicFields(
+    parentId: string,
+    maxPageSize = MAX_PAGE_SIZE
+) {
+    const rpc = useRpcClient();
+    return useInfiniteQuery(
+        ['dynamic-fields', parentId],
+        ({ pageParam = null }) =>
+            rpc.getDynamicFields({
+                parentId: normalizeHaneulAddress(parentId),
+                cursor: pageParam,
+                limit: maxPageSize,
+            }),
+        {
+            enabled: !!parentId,
+            getNextPageParam: ({ nextCursor, hasNextPage }) =>
+                hasNextPage ? nextCursor : null,
+        }
+    );
+}
