f0845ad24e8c13016614411ff45614933dad6bd8
GeunhwaJeong <jeonggh892@gmail.com>
2023-04-18 10:34:15 -0700

    [wallet]: update wallet welcome page
    ## Description 
    
    - Update value prop texts
    - https://haneullabs.atlassian.net/browse/APPS-746
    
    
    ![image](https://user-images.githubusercontent.com/127577476/232667278-e146e865-5cc1-4bd7-b572-af972e709c93.png)
    
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    
    ---------

--- apps/wallet/src/ui/app/pages/welcome/index.tsx
+++ apps/wallet/src/ui/app/pages/welcome/index.tsx
@@ -1,19 +1,24 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import cl from 'classnames';
-import { Link } from 'react-router-dom';
+import { ArrowRight16, CheckFill16 } from '@haneullabs/icons';
 
+import { Button } from '_app/shared/ButtonUI';
 import BottomMenuLayout, { Content } from '_app/shared/bottom-menu-layout';
 import { Heading } from '_app/shared/heading';
 import { Text } from '_app/shared/text';
-import Icon, { HaneulIcons } from '_components/icon';
 import Loading from '_components/loading';
 import Logo from '_components/logo';
 import { useInitializedGuard } from '_hooks';
 import PageLayout from '_pages/layout';
 
-import st from './Welcome.module.scss';
+const VALUE_PROP = [
+    'Send, receive tokens and NFTs',
+    'Stake HANEUL to earn rewards. Help the Haneul network remain decentralized.',
+    'Explore apps on Haneul blockchain',
+    'Quickly revoke access connection given to apps',
+    'Track your Haneul network activity',
+];
 
 const WelcomePage = () => {
     const checkingInitialized = useInitializedGuard(false);
@@ -21,71 +26,64 @@ const WelcomePage = () => {
         <PageLayout forceFullscreen={true}>
             <Loading loading={checkingInitialized}>
                 <div className="flex flex-col flex-nowrap items-center justify-center">
-                    <div className="pt-6 rounded-20 bg-haneul-lightest shadow-wallet-content flex flex-col flex-nowrap items-center justify-center w-popup-width h-popup-height">
+                    <div className="rounded-20 bg-haneul-lightest shadow-wallet-content flex flex-col flex-nowrap items-center justify-center w-popup-width h-popup-height">
                         <BottomMenuLayout>
                             <Content className="flex flex-col flex-nowrap items-center p-7.5 pb-0">
                                 <div className="mt-7.5 text-hero">
                                     <Logo />
                                 </div>
-                                <div className="mx-auto text-center mt-12">
-                                    <Heading
-                                        variant="heading2"
-                                        color="gray-90"
-                                        as="h1"
-                                        weight="bold"
-                                    >
-                                        Welcome to Haneul Wallet
-                                    </Heading>
-                                    <div className="mt-5">
-                                        <Text
-                                            variant="pBody"
-                                            color="steel-dark"
-                                            weight="medium"
-                                        >
-                                            Connecting you to the decentralized
-                                            web and Haneul network.
-                                        </Text>
-                                    </div>
-                                    <div className="flex gap-2 mt-10 items-center">
-                                        <Icon
-                                            icon={HaneulIcons.Checkmark}
-                                            className="text-success text-[8px]"
-                                        />
 
-                                        <Text
-                                            variant="body"
-                                            color="steel-dark"
-                                            weight="medium"
+                                <div className="mx-auto mt-7">
+                                    <div className="text-center">
+                                        <Heading
+                                            variant="heading2"
+                                            color="gray-90"
+                                            as="h1"
+                                            weight="bold"
                                         >
-                                            Buy, store, send and swap tokens
-                                        </Text>
+                                            Welcome to Haneul Wallet
+                                        </Heading>
+                                        <div className="mt-3">
+                                            <Text
+                                                variant="pBody"
+                                                color="steel-dark"
+                                                weight="medium"
+                                            >
+                                                Connecting you to the
+                                                decentralized web and Haneul
+                                                network.
+                                            </Text>
+                                        </div>
                                     </div>
-                                    <div className="flex gap-2 mt-3 items-center">
-                                        <Icon
-                                            icon={HaneulIcons.Checkmark}
-                                            className="text-success text-[8px]"
-                                        />
-                                        <Text
-                                            variant="body"
-                                            color="steel-dark"
-                                            weight="medium"
-                                        >
-                                            Explore blockchain apps
-                                        </Text>
+
+                                    <div className="mt-6 flex gap-2 flex-col">
+                                        {VALUE_PROP.map((value) => (
+                                            <div
+                                                key={value}
+                                                className="flex gap-2 items-center border bg-haneul-light/40 border-haneul/30 border-solid rounded-xl px-3 py-2"
+                                            >
+                                                <CheckFill16 className="text-steel flex-none w-4 h-4" />
+
+                                                <Text
+                                                    variant="pBody"
+                                                    color="steel-darker"
+                                                    weight="medium"
+                                                >
+                                                    {value}
+                                                </Text>
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
                             </Content>
-                            <div className={st.getStarted}>
-                                <Link
+
+                            <div className="flex sticky pb-10 m-auto w-[300px] -bottom-px bg-haneul-lightest">
+                                <Button
                                     to="/initialize/select"
-                                    className={cl(st.cta, 'btn', 'primary')}
-                                >
-                                    Get Started
-                                    <Icon
-                                        icon={HaneulIcons.ArrowLeft}
-                                        className={cl(st.arrowLeft)}
-                                    />
-                                </Link>
+                                    size="tall"
+                                    text="Get Started"
+                                    after={<ArrowRight16 />}
+                                />
                             </div>
                         </BottomMenuLayout>
                     </div>
