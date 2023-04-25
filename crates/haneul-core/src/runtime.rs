bcb99cc449113005bc6675e52cddf8016236f3db
GeunhwaJeong <jeonggh892@gmail.com>
2023-04-25 15:40:41 -0700

    [haneul-node] Stop and wait haneul node runtimes on SIGINT
    This change makes sure we stop all haneul node runtimes upon receiving
    SIGINT. This is needed to make sure rocks db databases are properly
    closed before existing haneul node

---
+++ crates/haneul-core/src/runtime.rs
@@ -0,0 +1,29 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use haneul_config::NodeConfig;
+use tokio::runtime::Runtime;
+
+pub struct HaneulRuntimes {
+    // Order in this struct is the order in which runtimes are stopped
+    pub haneul_node: Runtime,
+    pub metrics: Runtime,
+    // json_rpc: Runtime,
+}
+
+impl HaneulRuntimes {
+    pub fn new(_confg: &NodeConfig) -> Self {
+        let haneul_node = tokio::runtime::Builder::new_multi_thread()
+            .thread_name("haneul-node-runtime")
+            .enable_all()
+            .build()
+            .unwrap();
+        let metrics = tokio::runtime::Builder::new_multi_thread()
+            .thread_name("metrics-runtime")
+            .worker_threads(2)
+            .enable_all()
+            .build()
+            .unwrap();
+        Self { haneul_node, metrics }
+    }
+}
