ab8ec254b6cb713983d5fd77551052e657aa3dd6
GeunhwaJeong <jeonggh892@gmail.com>
2023-04-27 17:08:51 -0700

    Fix sponsored tx demo
    ## Description 
    
    Demo was broken with some recent changes, updates to fix it.
    
    ## Test Plan 
    
    Ran locally.

--- dapps/sponsored-transactions/src/utils/sponsorTransaction.ts
+++ dapps/sponsored-transactions/src/utils/sponsorTransaction.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { Ed25519Keypair, RawSigner, Transaction } from "@haneullabs/haneul.js";
+import { Ed25519Keypair, RawSigner, TransactionBlock } from "@haneullabs/haneul.js";
 import { provider } from "./rpc";
 
 // This simulates what a server would do to sponsor a transaction
@@ -16,8 +16,8 @@ export async function sponsorTransaction(
   await signer.requestHaneulFromFaucet();
   console.log(`Sponsor address: ${address}`);
 
-  const tx = Transaction.fromKind(transactionKindBytes);
+  const tx = TransactionBlock.fromKind(transactionKindBytes);
   tx.setSender(sender);
   tx.setGasOwner(address);
-  return await signer.signTransaction({ transaction: tx });
+  return await signer.signTransactionBlock({ transactionBlock: tx });
 }
