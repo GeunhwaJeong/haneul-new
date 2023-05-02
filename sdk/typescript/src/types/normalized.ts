a15ab1b35f71209c69b1154666bcb387764c04a8
GeunhwaJeong <jeonggh892@gmail.com>
2023-05-01 19:08:05 -0700

    [explorer] - top packages table
    ## Description 
    
    Add the Popular Packages to the home screen both for current / future
    layout
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- sdk/typescript/src/types/normalized.ts
+++ sdk/typescript/src/types/normalized.ts
@@ -13,6 +13,7 @@ import {
   literal,
   record,
   is,
+  tuple,
 } from 'superstruct';
 
 export type HaneulMoveFunctionArgTypesResponse = Infer<
@@ -67,6 +68,25 @@ export type HaneulMoveNormalizedType =
   | { MutableReference: HaneulMoveNormalizedType }
   | { Vector: HaneulMoveNormalizedType }
   | HaneulMoveNormalizedStructType;
+
+export const MoveCallMetric = tuple([
+  object({
+    module: string(),
+    package: string(),
+    function: string(),
+  }),
+  string(),
+]);
+
+export type MoveCallMetric = Infer<typeof MoveCallMetric>;
+
+export const MoveCallMetrics = object({
+  rank3Days: array(MoveCallMetric),
+  rank7Days: array(MoveCallMetric),
+  rank30Days: array(MoveCallMetric),
+});
+
+export type MoveCallMetrics = Infer<typeof MoveCallMetrics>;
 
 function isHaneulMoveNormalizedType(
   value: unknown,
