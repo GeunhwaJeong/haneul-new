69d27c358d39eac42197983b7d89a9a29c9ac90d
GeunhwaJeong <jeonggh892@gmail.com>
2023-05-02 14:35:37 -0700

    [explorer] - responsive footer update
    ## Description 
    
    - Add responsive styles for the updated Footer
    - Went back to using `footerLinks` to keep the markup a bit more tidy
    - Note: It was non-trivial to get the order of items to reverse between
    Desktop / Mobile, so I compromised for the sake of time
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/explorer/src/components/footer/Footer.tsx
+++ apps/explorer/src/components/footer/Footer.tsx
@@ -1,133 +1,61 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import {
-    SocialDiscord24,
-    SocialLinkedin24,
-    SocialTwitter24,
-} from '@haneullabs/icons';
-
 import { ReactComponent as HaneulWordmark } from '../../assets/HaneulWordmark.svg';
+import { LegalLinks, LegalText } from './Legal';
+import { footerLinks, socialLinks } from './footerLinks';
 
 import { Link } from '~/ui/Link';
 import { Text } from '~/ui/Text';
 
 function FooterLinks() {
     return (
-        <ul className="flex gap-8">
-            <li>
-                <Link variant="text" href="https://medium.com/haneullabs-labs">
-                    <Text variant="body/medium" color="steel-darker">
-                        Blog
-                    </Text>
-                </Link>
-            </li>
-            <li>
-                <Link
-                    variant="text"
-                    href="https://github.com/GeunhwaJeong/haneul/blob/main/doc/paper/haneul.pdf"
-                >
-                    <Text variant="body/medium" color="steel-darker">
-                        Whitepaper
-                    </Text>
-                </Link>
-            </li>
-            <li>
-                <Link variant="text" href="https://haneul-labs.com/#community">
-                    <Text variant="body/medium" color="steel-darker">
-                        Press
-                    </Text>
-                </Link>
-            </li>
-            <li>
-                <Link variant="text" href="https://docs.haneul.io/">
-                    <Text variant="body/medium" color="steel-darker">
-                        Docs
-                    </Text>
-                </Link>
-            </li>
-            <li>
-                <Link variant="text" href="https://github.com/GeunhwaJeong">
-                    <Text variant="body/medium" color="steel-darker">
-                        GitHub
-                    </Text>
-                </Link>
-            </li>
-            <li>
-                <Link variant="text" href="https://discord.gg/haneul">
-                    <SocialDiscord24 />
-                </Link>
-            </li>
-            <li>
-                <Link variant="text" href="https://twitter.com/HaneulNetwork">
-                    <SocialTwitter24 />
-                </Link>
-            </li>
-            <li>
-                <Link
-                    variant="text"
-                    href="https://www.linkedin.com/company/haneullabs-labs/"
-                >
-                    <SocialLinkedin24 />
-                </Link>
-            </li>
-        </ul>
+        <div className="flex flex-col items-center justify-center gap-6 md:flex-row md:justify-end">
+            <ul className="flex gap-6 md:flex-row">
+                {footerLinks.map(({ title, href }) => (
+                    <li key={href}>
+                        <Link variant="text" href={href}>
+                            <Text variant="body/medium" color="steel-darker">
+                                {title}
+                            </Text>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+
+            <ul className="flex justify-center gap-6">
+                {socialLinks.map(({ children, href }) => (
+                    <li key="href">
+                        <Link variant="text" color="steel-darker" href={href}>
+                            <div className="mt-2">{children}</div>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 }
 
 function Footer() {
     return (
         <footer className="bg-gray-40 px-5 py-10 md:px-10 md:py-14">
-            <nav className="flex flex-col gap-7.5">
-                <div className="flex w-full flex-col items-center justify-between gap-7.5 md:flex-row">
-                    <div className="flex gap-2 text-hero-dark">
+            <nav className="flex flex-col justify-center gap-4 divide-y divide-solid divide-gray-45 md:gap-7.5">
+                <div className="flex flex-col-reverse items-center gap-7.5 md:flex-row md:justify-between ">
+                    <div className="hidden self-center text-hero-dark md:flex md:self-start">
                         <HaneulWordmark />
                     </div>
-                    <FooterLinks />
-                </div>
-
-                <div className="h-[1px] w-full bg-gray-45" />
-                <div className="flex w-full items-center justify-between">
-                    <div className="h-full space-y-2">
-                        <Text
-                            color="steel-darker"
-                            variant="pSubtitleSmall/medium"
-                        >
-                            &copy;
-                            {`${new Date().getFullYear()} Haneul. All
-                                rights reserved.`}
-                        </Text>
+                    <div>
+                        <FooterLinks />
                     </div>
-                    <ul className="flex gap-2">
-                        <li>
-                            <Link
-                                variant="text"
-                                href="https://haneul-labs.com/legal?content=terms"
-                            >
-                                <Text
-                                    variant="pSubtitleSmall/medium"
-                                    color="steel-darker"
-                                >
-                                    Terms & Conditions
-                                </Text>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link
-                                variant="text"
-                                href="https://haneul-labs.com/legal?content=privacy"
-                            >
-                                <Text
-                                    variant="pSubtitleSmall/medium"
-                                    color="steel-darker"
-                                >
-                                    Privacy Policy
-                                </Text>
-                            </Link>
-                        </li>
-                    </ul>
+                </div>
+                <div className="flex flex-col-reverse justify-center gap-3 pt-3 md:flex-row md:justify-between">
+                    <LegalText />
+                    <LegalLinks />
                 </div>
             </nav>
+            <div className="mt-4 flex justify-center border-t border-solid border-gray-45 pt-5 text-hero-dark md:hidden md:self-start">
+                <HaneulWordmark />
+            </div>
         </footer>
     );
 }
