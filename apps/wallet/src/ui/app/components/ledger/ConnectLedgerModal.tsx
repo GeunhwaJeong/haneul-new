a1e0980bc1be72a2386010f4bac73bad2dbf396b
GeunhwaJeong <jeonggh892@gmail.com>
2023-05-01 20:10:46 -0400

    [wallet-ext] update Ledger tutorial link to point to our Github docs for the time being
    ## Description 
    Will update this to the Ledger help center article when it's ready.
    
    ## Test Plan 
    - Manual testing (I clicked the link lol)
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    N/A

--- apps/wallet/src/ui/app/components/ledger/ConnectLedgerModal.tsx
+++ apps/wallet/src/ui/app/components/ledger/ConnectLedgerModal.tsx
@@ -62,11 +62,11 @@ export function ConnectLedgerModal({
                             >
                                 Need more help?&nbsp;
                             </Text>
-                            {/* TODO: Replace this link with the correct URL once the tutorial is ready */}
+                            {/* TODO: Replace this link with the final URL once the tutorial is ready */}
                             <span>
                                 <Link
                                     underline="hover"
-                                    href="https://haneul-labs.com"
+                                    href="https://github.com/GeunhwaJeong/haneullabs-app-docs/blob/main/haneul-wallet-with-ledger.md"
                                     text="View tutorial."
                                     color="heroDark"
                                 />
