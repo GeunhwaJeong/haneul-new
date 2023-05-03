d10de59986890f4861003dad297ba868d2806c3f
GeunhwaJeong <jeonggh892@gmail.com>
2023-05-03 09:34:53 -0400

    [explorer] do some style tweaks / remove the percent change / etc.
    ## Description 
    Doing some last-minute tweaks before we turn on the Haneul token card :)
    
    <img width="1636" alt="image"
    src="https://user-images.githubusercontent.com/7453188/235929024-484f2612-75cc-4bf3-82f3-35056e0d67e4.png">
    
    <img width="1613" alt="image"
    src="https://user-images.githubusercontent.com/7453188/235931598-c2f4bfce-d606-4ed6-ba08-96219b115905.png">
    
    
    ## Test Plan 
    - Manual testing on all breakpoints
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [X] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    N/A

--- apps/explorer/src/hooks/useHaneulCoinData.ts
+++ apps/explorer/src/hooks/useHaneulCoinData.ts
@@ -21,8 +21,8 @@ export function useHaneulCoinData() {
         ['haneul-coin-data'],
         () => makeAppsBackendRequest<CoinData>('coins/haneul', {}),
         {
-            // Cache this forever because we have limited API bandwidth at the moment
-            cacheTime: Infinity,
+            cacheTime: 24 * 60 * 60 * 1000,
+            staleTime: Infinity,
         }
     );
 }
