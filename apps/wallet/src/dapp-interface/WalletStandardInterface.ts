3fdea39827b427abfd2f14f1cf57c4e4d811cd47
GeunhwaJeong <jeonggh892@gmail.com>
2023-05-29 22:45:16 +0100

    wallet-ext:wallet-kit: fix port disconnection when dapp preloads
    * this seems like a chrome issue, when dapp preloads and then user
    visits the page the port disconnects but content script never gets
    notified
    * we avoid creating a port until the document is visible and that seems
    to solve the issue
    * auto connect gets delayed and wait for the document to be visible
    * wallet interface delay loading accounts until a dapp connects
    
    fixes: 
    
    https://github.com/GeunhwaJeong/haneul/assets/10210143/4a354026-1682-4e7a-b2fb-5539af8e7247
    
    https://github.com/GeunhwaJeong/haneul/assets/10210143/cde9852f-b26f-46ff-a2f6-3dc2169002f6
    
    closes APPS-1036

--- apps/wallet/src/dapp-interface/WalletStandardInterface.ts
+++ apps/wallet/src/dapp-interface/WalletStandardInterface.ts
@@ -208,7 +208,6 @@ export class HaneulWallet implements Wallet {
                 this.#events.emit('change', { accounts: this.accounts });
             }
         });
-        this.#connected();
     }
 
     #on: StandardEventsOnMethod = (event, listener) => {
