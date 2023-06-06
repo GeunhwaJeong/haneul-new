034ed59f0a465144d460da6181a5c0c5e9acf9ce
GeunhwaJeong <jeonggh892@gmail.com>
2023-06-06 20:11:17 +0300

    [Kiosk] Demo dapp
    ## Description 
    
    A react dapp Demo for kiosk. It is a nice demonstration for the [Kiosk
    SDK](https://github.com/GeunhwaJeong/haneul/tree/main/sdk/kiosk).
    It allows a wide variety of functionality:
    
    - Fetching user's kiosk (and allowing to create one if the address
    doesn't have one).
    - Manage kiosk (withdraw profits, place items, list items for sale, take
    items from kiosk).
    - Visit a kiosk, visualises the kiosk's contents.
    - Allows to purchase an item that's listed in a kiosk. Resolves all
    transfer policy rules based on `haneullabs/kiosk` package.
    
    This is a demonstration app. It doesn't leverage caching (does many RPC
    calls), doesn't allow a selection of kiosk (if a user has > 1, defaults
    to first that's returned), doesn't have tests, and the UX could be
    improved in some cases.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ dapps/kiosk/src/components/Base/HaneulConnectButton.tsx
@@ -0,0 +1,14 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { ConnectButton } from '@haneullabs/wallet-kit';
+
+export function HaneulConnectButton() {
+  return (
+    <ConnectButton
+      style={{
+        backgroundColor: '#101827',
+      }}
+    />
+  );
+}
---
+++ dapps/kiosk/src/utils/utils.ts
@@ -0,0 +1,52 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { KioskListing } from '@haneullabs/kiosk';
+import {
+  GEUNHWA_PER_HANEUL,
+  ObjectId,
+  HaneulObjectResponse,
+  getObjectDisplay,
+  getObjectId,
+} from '@haneullabs/haneul.js';
+// Parse the display of a list of objects into a simple {object_id: display} map
+// to use throughout the app.
+export const parseObjectDisplays = (
+  data: HaneulObjectResponse[],
+): Record<ObjectId, Record<string, string> | undefined> => {
+  return data.reduce<Record<ObjectId, Record<string, string> | undefined>>(
+    (acc, item: HaneulObjectResponse) => {
+      const display = getObjectDisplay(item)?.data;
+      const id = getObjectId(item);
+      acc[id] = display || undefined;
+      return acc;
+    },
+    {},
+  );
+};
+
+export const processKioskListings = (
+  data: KioskListing[],
+): Record<ObjectId, KioskListing> => {
+  const results: Record<ObjectId, KioskListing> = {};
+
+  data
+    .filter((x) => !!x)
+    .map((x: KioskListing) => {
+      results[x.objectId || ''] = x;
+      return x;
+    });
+  return results;
+};
+
+export const geunhwaToHaneul = (geunhwa: bigint | string | undefined) => {
+  if (!geunhwa) return 0;
+  return Number(geunhwa || 0) / Number(GEUNHWA_PER_HANEUL);
+};
+
+export const formatHaneul = (amount: number) => {
+  return new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 5,
+  }).format(amount);
+};
