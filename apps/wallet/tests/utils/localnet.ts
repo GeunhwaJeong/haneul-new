0639612258f1fb8933d88e054a35c3266df8bb47
GeunhwaJeong <jeonggh892@gmail.com>
2023-06-08 14:39:50 -0700

    [Wallet]: add e2e test for balance changes
    ## Description 
    
    - Add tests for balance changes
    - Add `data-testid` for easier test selectors
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/tests/utils/localnet.ts
+++ apps/wallet/tests/utils/localnet.ts
@@ -3,12 +3,25 @@
 
 // eslint-disable-next-line import/order
 import 'tsconfig-paths/register';
-import { Ed25519Keypair } from '@haneullabs/haneul.js';
+import {
+    Ed25519Keypair,
+    JsonRpcProvider,
+    localnetConnection,
+} from '@haneullabs/haneul.js';
 import * as bip39 from '@scure/bip39';
 import { wordlist } from '@scure/bip39/wordlists/english';
 
+export async function generateKeypairFromMnemonic(mnemonic: string) {
+    return Ed25519Keypair.deriveKeypair(mnemonic);
+}
+
 export async function generateKeypair() {
     const mnemonic = bip39.generateMnemonic(wordlist);
-    const keypair = Ed25519Keypair.deriveKeypair(mnemonic);
+    const keypair = await generateKeypairFromMnemonic(mnemonic);
     return { mnemonic, keypair };
+}
+
+export async function requestingHaneulFromFaucet(address: string) {
+    const provider = new JsonRpcProvider(localnetConnection);
+    await provider.requestHaneulFromFaucet(address);
 }
