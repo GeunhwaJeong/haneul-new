aa2fa9371890b1719bdefa23cb292bc096f321db
GeunhwaJeong <jeonggh892@gmail.com>
2023-06-14 11:31:34 -0500

    [explorer] Add object video support
    ## Description 
    
    This adds video support to object displays in explorer. Some notes:
    - The object display must have `video_url` property, _in addition_ to an
    `image_url` field. The `image_url` is expected to be the thumbnail of
    the video, and the fallback display in the event that videos are not
    supported. This could change in the future, but for now seems like a
    sensible approach.
    - Only supported for recognized packages.
    
    ## Test Plan 
    
    Ran locally.
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ apps/explorer/src/hooks/useResolveVideo.ts
@@ -0,0 +1,23 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import {
+    type HaneulObjectResponse,
+    getObjectDisplay,
+    getObjectType,
+} from '@haneullabs/haneul.js';
+
+import { useRecognizedPackages } from './useRecognizedPackages';
+
+export function useResolveVideo(object: HaneulObjectResponse) {
+    const recognizedPackages = useRecognizedPackages();
+    const objectType = getObjectType(object);
+    const isRecognized =
+        objectType && recognizedPackages.includes(objectType.split('::')[0]);
+
+    if (!isRecognized) return null;
+
+    const display = getObjectDisplay(object)?.data;
+
+    return display?.video_url;
+}
