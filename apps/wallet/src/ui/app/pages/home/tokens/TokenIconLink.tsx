69d1131798ec3768b22a233dce21a70e05df16b7
GeunhwaJeong <jeonggh892@gmail.com>
2023-06-29 17:35:04 -0400

    [wallet-ext] remove Plausible now that we have data flowing into Amplitude
    ## Description 
    See title ^
    
    ## Test Plan 
    - Sanity checked wallet works and Growthbook attributes are updated
    correctly
    - Going to cancel our Plausible subscription tomorrow
    - CI
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/pages/home/tokens/TokenIconLink.tsx
+++ apps/wallet/src/ui/app/pages/home/tokens/TokenIconLink.tsx
@@ -10,7 +10,6 @@ import { LargeButton } from '_app/shared/LargeButton';
 import { DelegatedAPY } from '_app/shared/delegated-apy';
 import { useGetDelegatedStake } from '_app/staking/useGetDelegatedStake';
 import { ampli } from '_src/shared/analytics/ampli';
-import { trackEvent } from '_src/shared/plausible';
 
 export function TokenIconLink({ accountAddress }: { accountAddress: HaneulAddress }) {
 	const { data: delegatedStake, isLoading } = useGetDelegatedStake(accountAddress);
@@ -34,7 +33,6 @@ export function TokenIconLink({ accountAddress }: { accountAddress: HaneulAddres
 		<LargeButton
 			to="/stake"
 			onClick={() => {
-				trackEvent('StakingFromHome');
 				ampli.clickedStakeHaneul({
 					isCurrentlyStaking: totalActivePendingStake > 0,
 					sourceFlow: 'Home page',
