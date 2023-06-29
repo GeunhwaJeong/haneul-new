3304ff9f319b31d15dac3a674534537e29bb6546
GeunhwaJeong <jeonggh892@gmail.com>
2023-06-29 13:50:57 -0700

    [wallet]: update network order
    ## Description 
    
    - Update Icons for Network selection
    - Reorder Network selection items
    
    
    https://haneullabs.atlassian.net/browse/APPS-1360
    
    
    ![wallet-haneul-logo](https://github.com/GeunhwaJeong/haneul/assets/127577476/e2488da3-aba6-4d9c-93f4-63aa34e371fc)
    
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/components/logo/index.tsx
+++ apps/wallet/src/ui/app/components/logo/index.tsx
@@ -1,29 +1,26 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { Haneul, HaneulLogoTxt } from '@haneullabs/icons';
+import { HaneulMainnet, HaneulTestnet, HaneulDevnet, HaneulLocal, HaneulCustomRpc } from '@haneullabs/icons';
 
-import { Text } from '../../shared/text';
-import { type API_ENV, networkNames } from '_src/shared/api-env';
+import { API_ENV } from '_src/shared/api-env';
 
 type LogoProps = {
 	networkName?: API_ENV;
 };
 
+const networkLogos = {
+	[API_ENV.mainnet]: HaneulMainnet,
+	[API_ENV.devNet]: HaneulDevnet,
+	[API_ENV.testNet]: HaneulTestnet,
+	[API_ENV.local]: HaneulLocal,
+	[API_ENV.customRPC]: HaneulCustomRpc,
+};
+
 const Logo = ({ networkName }: LogoProps) => {
-	return (
-		<div className="inline-flex flex-nowrap items-center gap-0.5 text-gray-90">
-			<Haneul className="h-[26px] w-5" />
-			<div className="flex flex-col">
-				<HaneulLogoTxt className="w-5 h-[13px]" />
-				{networkName && (
-					<div className="whitespace-nowrap">
-						<Text variant="subtitleSmallExtra">{networkNames[networkName]}</Text>
-					</div>
-				)}
-			</div>
-		</div>
-	);
+	const LogoComponent = networkName ? networkLogos[networkName] : networkLogos[API_ENV.mainnet];
+
+	return <LogoComponent className="h-7 w-walletLogo" />;
 };
 
 export default Logo;
