755ce92aaed8654d63d4d7511ec36927daea6ae7
GeunhwaJeong <jeonggh892@gmail.com>
2023-07-14 10:59:34 -0400

    [wallet-ext] hide and unhide assets
    ## Description 
    
    <img width="431" alt="image"
    src="https://github.com/GeunhwaJeong/haneul/assets/128089541/3bd57fa2-2988-4112-921d-c4c6ea3e9909">
    <img width="366" alt="image"
    src="https://github.com/GeunhwaJeong/haneul/assets/128089541/99ae115e-2197-41fd-9ec6-f4d243158e0a">
    <img width="391" alt="image"
    src="https://github.com/GeunhwaJeong/haneul/assets/128089541/6f819047-321a-4b51-9d85-99ffa482f2fb">
    
    
    
    ![hide](https://github.com/GeunhwaJeong/haneul/assets/128089541/c11420c3-a7f9-41b9-97c0-79d2fb2ac94d)
    
    
    ![unhide](https://github.com/GeunhwaJeong/haneul/assets/128089541/1a185ae5-58e0-4f0d-b8a6-5224e21ca4b7)
    
    
    
    ## Test Plan 
    
    Locally
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/wallet/src/ui/app/shared/Link.tsx
+++ apps/wallet/src/ui/app/shared/Link.tsx
@@ -24,10 +24,14 @@ const styles = cva(
 				steelDark: [
 					'text-steel-dark hover:text-steel-darker focus:text-steel-darker disabled:text-steel-dark',
 				],
+				steelDarker: [
+					'text-steel-darker hover:text-steel-darker focus:text-steel-darker disabled:text-steel-darker',
+				],
 				heroDark: [
 					'text-hero-dark hover:text-hero-darkest focus:text-hero-darkest disabled:text-hero-dark',
 				],
 				haneulDark: ['text-haneul-dark'],
+				hero: ['text-hero hover:text-hero-dark focus:text-hero-dark disabled:text-hero-dark'],
 			},
 			weight: {
 				semibold: 'font-semibold',
@@ -53,11 +57,17 @@ const iconStyles = cva(['transition flex'], {
 			steelDark: [
 				'text-steel group-hover:text-steel-darker group-focus:text-steel-darker group-disabled:text-steel-dark',
 			],
+			steelDarker: [
+				'text-steel-darker group-hover:text-steel-darker group-focus:text-steel-darker group-disabled:text-steel-darker',
+			],
 			heroDark: [
 				'text-hero group-hover:text-hero-darkest group-focus:text-hero-darkest group-disabled:text-hero-dark',
 			],
 			haneulDark: [
 				'text-steel group-hover:text-haneul-dark group-focus:text-haneul-dark group-disabled:text-steel',
+			],
+			hero: [
+				'text-hero group-hover:text-hero-dark group-focus:text-hero-dark group-disabled:text-hero-dark',
 			],
 		},
 	},
