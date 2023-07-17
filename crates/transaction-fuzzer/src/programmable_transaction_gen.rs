8bb74213479f78224f9bdbbaefd7e7727ffc1a76
GeunhwaJeong <jeonggh892@gmail.com>
2023-07-17 12:42:24 -0700

    [Protocol Config] Mark get_for_max_version as unsafe
    ## Description 
    
    See main comment in `crates/haneul-protocol-config/src/lib.rs` for reasons.
    This is currently in use in places that it shouldn't be. Migration will
    take more work, but for now, this should stop new incorrect usage.
    
    ## Test Plan 
    
    `cargo build`
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/transaction-fuzzer/src/programmable_transaction_gen.rs
+++ crates/transaction-fuzzer/src/programmable_transaction_gen.rs
@@ -12,7 +12,8 @@ use haneul_types::base_types::{ObjectID, ObjectRef, HaneulAddress};
 use haneul_types::programmable_transaction_builder::ProgrammableTransactionBuilder;
 use haneul_types::transaction::{Argument, CallArg, Command, ProgrammableTransaction};
 
-static PROTOCOL_CONFIG: Lazy<ProtocolConfig> = Lazy::new(ProtocolConfig::get_for_max_version);
+static PROTOCOL_CONFIG: Lazy<ProtocolConfig> =
+    Lazy::new(ProtocolConfig::get_for_max_version_UNSAFE);
 
 prop_compose! {
     pub fn gen_transfer()
