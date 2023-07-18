7d6bf3340955f46e709a533089809c3f25c5caf8
GeunhwaJeong <jeonggh892@gmail.com>
2023-07-18 05:43:39 +0200

    UI Base package
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/ui/tailwind.config.ts
+++ apps/ui/tailwind.config.ts
@@ -5,6 +5,5 @@ import preset from '@haneullabs/core/tailwind.config';
 import { type Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/@haneullabs/ui/src/**/*.{js,jsx,ts,tsx}'],
 	presets: [preset],
 } satisfies Partial<Config>;
