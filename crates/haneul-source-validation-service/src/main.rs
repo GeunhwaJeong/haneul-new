28764df476de386e2bb99654a64fd22d6d9b097b
GeunhwaJeong <jeonggh892@gmail.com>
2023-07-18 14:26:49 -0600

    move: source service supports multiple networks
    ## Description 
    
    Adds support for multiple networks (`mainnet`, `testnet`, `devnet`,
    `localnet`) with a top-level lookup to case out on sources.
    
    ## Test Plan 
    
    Added / updated tests.
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-source-validation-service/src/main.rs
+++ crates/haneul-source-validation-service/src/main.rs
@@ -6,8 +6,6 @@ use tracing::info;
 
 use clap::Parser;
 
-use haneul_config::{haneul_config_dir, HANEUL_CLIENT_CONFIG};
-use haneul_sdk::wallet_context::WalletContext;
 use telemetry_subscribers::TelemetryConfig;
 
 use haneul_source_validation_service::{host_port, initialize, parse_config, serve, AppState};
@@ -22,10 +20,8 @@ pub async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let _logging_guard = TelemetryConfig::new().with_env().init();
     let package_config = parse_config(args.config_path)?;
-    let haneul_config = haneul_config_dir()?.join(HANEUL_CLIENT_CONFIG);
-    let context = WalletContext::new(&haneul_config, None, None).await?;
     let tmp_dir = tempfile::tempdir()?;
-    let sources = initialize(&context, &package_config, tmp_dir.path()).await?;
+    let sources = initialize(&package_config, tmp_dir.path()).await?;
     info!("verification complete");
     info!("serving on {}", host_port());
     serve(AppState { sources })?
