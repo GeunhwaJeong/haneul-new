4569a3951a601b067230670a8625902ba34c9fba
GeunhwaJeong <jeonggh892@gmail.com>
2023-07-20 22:09:22 +0200

    Make combobox shared
    ## Description 
    
    This migrates the combobox to the shared UI components, and updates our
    two usages of the old combobox to use the new one
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/explorer/src/ui/Search.tsx
+++ apps/explorer/src/ui/Search.tsx
@@ -1,9 +1,7 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 import { Search16 } from '@haneullabs/icons';
-import { Text } from '@haneullabs/ui';
-
-import { Combobox, ComboboxInput, ComboboxList } from './Combobox';
+import { Text, Combobox, ComboboxInput, ComboboxList } from '@haneullabs/ui';
 
 export type SearchResult = {
 	id: string;
