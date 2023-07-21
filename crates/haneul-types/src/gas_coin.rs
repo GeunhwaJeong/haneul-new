ebf8385a08759e02afb3d40599539e931443e52b
GeunhwaJeong <jeonggh892@gmail.com>
2023-07-21 15:29:48 -0700

    [macros] Update `with_checked_arithmetic` macro, and convert usage of `checked_arithmetic` macro to attribute macro
    ## Description 
    
    Usage of the `checked_arithmetic` macro was causing a number of
    quality-of-life issues for development around formatting and LSP
    support. However, converting to an attribute-based procedural macro for
    adding checked arithmetic operations solves all of these issues (LSPs
    and autoformatters should now work with it).
    
    Note that this only works on explicitly declared modules due to
    (current) Rust restrictions, and you cannot annotate a file-based module
    with this attribute macro.
    
    This also required expanding the applicability of the
    `with_checked_arithmetic` macro to work over more forms than it
    previously handled, however the change is pretty straightforward.
    
    The one downside to it is that this introduces some slight boilerplate
    of
    
    ```rust
    pub use checked::*;
    mod checked {
    // what was previously inside the `checked_aritmetic` macro + imports
    }
    ```
    
    ## Test Plan 
    
    Added an additional test to make sure the behavior of code within a
    module annotated with `with_checked_arithmetic` behaves in the same way
    as code within a `checked_arithmetic` block.
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-types/src/gas_coin.rs
+++ crates/haneul-types/src/gas_coin.rs
@@ -27,128 +27,134 @@ pub const GEUNHWA_PER_HANEUL: u64 = 1_000_000_000;
 /// Total supply denominated in Haneul
 pub const TOTAL_SUPPLY_HANEUL: u64 = 10_000_000_000;
 
+// Note: cannot use checked arithmetic here since `const unwrap` is still unstable.
 /// Total supply denominated in Geunhwa
 pub const TOTAL_SUPPLY_GEUNHWA: u64 = TOTAL_SUPPLY_HANEUL * GEUNHWA_PER_HANEUL;
 
 pub const GAS_MODULE_NAME: &IdentStr = ident_str!("haneul");
 pub const GAS_STRUCT_NAME: &IdentStr = ident_str!("HANEUL");
 
-haneul_macros::checked_arithmetic! {
+pub use checked::*;
 
-pub struct GAS {}
-impl GAS {
-    pub fn type_() -> StructTag {
-        StructTag {
-            address: HANEUL_FRAMEWORK_ADDRESS,
-            name: GAS_STRUCT_NAME.to_owned(),
-            module: GAS_MODULE_NAME.to_owned(),
-            type_params: Vec::new(),
+#[haneul_macros::with_checked_arithmetic]
+mod checked {
+    use super::*;
+
+    pub struct GAS {}
+    impl GAS {
+        pub fn type_() -> StructTag {
+            StructTag {
+                address: HANEUL_FRAMEWORK_ADDRESS,
+                name: GAS_STRUCT_NAME.to_owned(),
+                module: GAS_MODULE_NAME.to_owned(),
+                type_params: Vec::new(),
+            }
+        }
+
+        pub fn type_tag() -> TypeTag {
+            TypeTag::Struct(Box::new(Self::type_()))
+        }
+
+        pub fn is_gas(other: &StructTag) -> bool {
+            &Self::type_() == other
+        }
+
+        pub fn is_gas_type(other: &TypeTag) -> bool {
+            match other {
+                TypeTag::Struct(s) => Self::is_gas(s),
+                _ => false,
+            }
         }
     }
 
-    pub fn type_tag() -> TypeTag {
-        TypeTag::Struct(Box::new(Self::type_()))
-    }
+    /// Rust version of the Move haneul::coin::Coin<Haneul::haneul::HANEUL> type
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct GasCoin(pub Coin);
 
-    pub fn is_gas(other: &StructTag) -> bool {
-        &Self::type_() == other
-    }
+    impl GasCoin {
+        pub fn new(id: ObjectID, value: u64) -> Self {
+            Self(Coin::new(UID::new(id), value))
+        }
 
-    pub fn is_gas_type(other: &TypeTag) -> bool {
-        match other {
-            TypeTag::Struct(s) => Self::is_gas(s),
-            _ => false,
+        pub fn value(&self) -> u64 {
+            self.0.value()
+        }
+
+        pub fn type_() -> StructTag {
+            Coin::type_(TypeTag::Struct(Box::new(GAS::type_())))
+        }
+
+        /// Return `true` if `s` is the type of a gas coin (i.e., 0x2::coin::Coin<0x2::haneul::HANEUL>)
+        pub fn is_gas_coin(s: &StructTag) -> bool {
+            Coin::is_coin(s) && s.type_params.len() == 1 && GAS::is_gas_type(&s.type_params[0])
+        }
+
+        /// Return `true` if `s` is the type of a gas balance (i.e., 0x2::balance::Balance<0x2::haneul::HANEUL>)
+        pub fn is_gas_balance(s: &StructTag) -> bool {
+            Balance::is_balance(s)
+                && s.type_params.len() == 1
+                && GAS::is_gas_type(&s.type_params[0])
+        }
+
+        pub fn id(&self) -> &ObjectID {
+            self.0.id()
+        }
+
+        pub fn to_bcs_bytes(&self) -> Vec<u8> {
+            bcs::to_bytes(&self).unwrap()
+        }
+
+        pub fn to_object(&self, version: SequenceNumber) -> MoveObject {
+            MoveObject::new_gas_coin(version, *self.id(), self.value())
+        }
+
+        pub fn layout() -> MoveStructLayout {
+            Coin::layout(TypeTag::Struct(Box::new(GAS::type_())))
+        }
+
+        #[cfg(test)]
+        pub fn new_for_testing(value: u64) -> Self {
+            Self::new(ObjectID::random(), value)
         }
     }
-}
 
-/// Rust version of the Move haneul::coin::Coin<Haneul::haneul::HANEUL> type
-#[derive(Debug, Serialize, Deserialize)]
-pub struct GasCoin(pub Coin);
+    impl TryFrom<&MoveObject> for GasCoin {
+        type Error = ExecutionError;
 
-impl GasCoin {
-    pub fn new(id: ObjectID, value: u64) -> Self {
-        Self(Coin::new(UID::new(id), value))
-    }
-
-    pub fn value(&self) -> u64 {
-        self.0.value()
-    }
-
-    pub fn type_() -> StructTag {
-        Coin::type_(TypeTag::Struct(Box::new(GAS::type_())))
-    }
-
-    /// Return `true` if `s` is the type of a gas coin (i.e., 0x2::coin::Coin<0x2::haneul::HANEUL>)
-    pub fn is_gas_coin(s: &StructTag) -> bool {
-        Coin::is_coin(s) && s.type_params.len() == 1 && GAS::is_gas_type(&s.type_params[0])
-    }
-
-    /// Return `true` if `s` is the type of a gas balance (i.e., 0x2::balance::Balance<0x2::haneul::HANEUL>)
-    pub fn is_gas_balance(s: &StructTag) -> bool {
-        Balance::is_balance(s) && s.type_params.len() == 1 && GAS::is_gas_type(&s.type_params[0])
-    }
-
-    pub fn id(&self) -> &ObjectID {
-        self.0.id()
-    }
-
-    pub fn to_bcs_bytes(&self) -> Vec<u8> {
-        bcs::to_bytes(&self).unwrap()
-    }
-
-    pub fn to_object(&self, version: SequenceNumber) -> MoveObject {
-        MoveObject::new_gas_coin(version, *self.id(), self.value())
-    }
-
-    pub fn layout() -> MoveStructLayout {
-        Coin::layout(TypeTag::Struct(Box::new(GAS::type_())))
-    }
-
-    #[cfg(test)]
-    pub fn new_for_testing(value: u64) -> Self {
-        Self::new(ObjectID::random(), value)
-    }
-}
-
-impl TryFrom<&MoveObject> for GasCoin {
-    type Error = ExecutionError;
-
-    fn try_from(value: &MoveObject) -> Result<GasCoin, ExecutionError> {
-        if !value.type_().is_gas_coin() {
-            return Err(ExecutionError::new_with_source(
-                ExecutionErrorKind::InvalidGasObject,
-                format!("Gas object type is not a gas coin: {}", value.type_()),
-            ));
-        }
-        let gas_coin: GasCoin = bcs::from_bytes(value.contents()).map_err(|err| {
-            ExecutionError::new_with_source(
-                ExecutionErrorKind::InvalidGasObject,
-                format!("Unable to deserialize gas object: {:?}", err),
-            )
-        })?;
-        Ok(gas_coin)
-    }
-}
-
-impl TryFrom<&Object> for GasCoin {
-    type Error = ExecutionError;
-
-    fn try_from(value: &Object) -> Result<GasCoin, ExecutionError> {
-        match &value.data {
-            Data::Move(obj) => obj.try_into(),
-            Data::Package(_) => Err(ExecutionError::new_with_source(
-                ExecutionErrorKind::InvalidGasObject,
-                format!("Gas object type is not a gas coin: {:?}", value),
-            )),
+        fn try_from(value: &MoveObject) -> Result<GasCoin, ExecutionError> {
+            if !value.type_().is_gas_coin() {
+                return Err(ExecutionError::new_with_source(
+                    ExecutionErrorKind::InvalidGasObject,
+                    format!("Gas object type is not a gas coin: {}", value.type_()),
+                ));
+            }
+            let gas_coin: GasCoin = bcs::from_bytes(value.contents()).map_err(|err| {
+                ExecutionError::new_with_source(
+                    ExecutionErrorKind::InvalidGasObject,
+                    format!("Unable to deserialize gas object: {:?}", err),
+                )
+            })?;
+            Ok(gas_coin)
         }
     }
-}
 
-impl Display for GasCoin {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Coin {{ id: {}, value: {} }}", self.id(), self.value())
+    impl TryFrom<&Object> for GasCoin {
+        type Error = ExecutionError;
+
+        fn try_from(value: &Object) -> Result<GasCoin, ExecutionError> {
+            match &value.data {
+                Data::Move(obj) => obj.try_into(),
+                Data::Package(_) => Err(ExecutionError::new_with_source(
+                    ExecutionErrorKind::InvalidGasObject,
+                    format!("Gas object type is not a gas coin: {:?}", value),
+                )),
+            }
+        }
     }
-}
 
+    impl Display for GasCoin {
+        fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+            write!(f, "Coin {{ id: {}, value: {} }}", self.id(), self.value())
+        }
+    }
 }
