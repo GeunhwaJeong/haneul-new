07b1edbcf0561e510a62a86433711342f89aa972
GeunhwaJeong <jeonggh892@gmail.com>
2023-08-03 09:35:27 -0700

    Fix `haneul-node` binary placement in Docker
    ## Description 
    
    Fix `haneul-node` Dockerfile cleanup, by adding back `haneul-node` at
    `/usr/local/bin/haneul-node`.
    
    ## Test Plan 
    
    CI
    https://github.com/GeunhwaJeong/haneul-operations/actions/runs/5653096771
    `sudo docker run
    haneullabs/haneul-node:1112c07c573bb54b3a83a83b514d834451a73329
    /usr/local/bin/haneul-node --version`
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- docker/fullnode-x/fullnode/entry.sh
+++ docker/fullnode-x/fullnode/entry.sh
@@ -2,7 +2,4 @@
 # Copyright (c) Mysten Labs, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-unset LD_PRELOAD
-
-/usr/local/bin/haneul-node --config-path /opt/haneul/config/fullnode.yaml
-
+/opt/haneul/bin/haneul-node --config-path /opt/haneul/config/fullnode.yaml
