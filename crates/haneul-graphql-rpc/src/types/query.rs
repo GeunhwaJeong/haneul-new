773b3387dbe3e860f2f4f794cda659d85e6e23b7
GeunhwaJeong <jeonggh892@gmail.com>
2023-08-10 20:58:01 +0200

    [graphql/rpc] simple axum server
    ## Description 
    
    Sample Axum web server for testing and iterating on other features.
    Example below
    ```
    > ./target/debug/haneul-graphql-rpc start-server
    Start server
    Launch GraphiQL IDE at: http://127.0.0.1:8000
    ```
    
    <img width="845" alt="Screen Shot 2023-08-10 at 19 33 52"
    src="https://github.com/GeunhwaJeong/haneul/assets/93547199/4d1abee5-27b4-4800-b0c5-c0ea7e472bc5">
    
    
    ## Test Plan 
    
    Manual
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-graphql-rpc/src/types/query.rs
+++ crates/haneul-graphql-rpc/src/types/query.rs
@@ -7,23 +7,25 @@ use super::{address::Address, object::Object, owner::Owner, haneul_address::Hane
 
 pub(crate) struct Query;
 
+pub(crate) type HaneulGraphQLSchema = async_graphql::Schema<Query, EmptyMutation, EmptySubscription>;
+
 #[allow(unreachable_code)]
 #[allow(unused_variables)]
 #[Object]
 impl Query {
     async fn chain_identifier(&self) -> String {
-        unimplemented!()
+        "0000".to_string()
     }
 
     async fn owner(&self, address: HaneulAddress) -> Option<Owner> {
-        unimplemented!()
+        None
     }
 
     async fn object(&self, address: HaneulAddress, version: Option<u64>) -> Option<Object> {
-        unimplemented!()
+        None
     }
 
     async fn address(&self, address: HaneulAddress) -> Option<Address> {
-        unimplemented!()
+        None
     }
 }
