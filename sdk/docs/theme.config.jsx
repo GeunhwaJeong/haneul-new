a17cde9e4e4fd9d06c170d0654a767a6a8f684ee
GeunhwaJeong <jeonggh892@gmail.com>
2023-08-14 14:04:48 -0700

    Refactor Wallet SDK folders
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- sdk/docs/theme.config.jsx
+++ sdk/docs/theme.config.jsx
@@ -2,11 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const config = {
-	logo: <span>Haneul Typescript Docs</span>,
+	logo: <span>Haneul TypeScript Docs</span>,
 	project: {
-		link: 'https://github.com/GeunhwaJeong/haneul/tree/main/sdk/typescript',
+		link: 'https://github.com/GeunhwaJeong/haneul/tree/main/sdk/',
+	},
+	chat: {
+		link: 'https://discord.com/invite/Haneul',
 	},
 	docsRepositoryBase: 'https://github.com/GeunhwaJeong/haneul/tree/main/sdk/docs/pages',
+	footer: {
+		text: 'Copyright © 2023, Haneul Labs, Inc.',
+	},
 	useNextSeoProps() {
 		return {
 			titleTemplate: '%s',
--- sdk/wallet-adapter/example/src/README.md
+++
@@ -1,4 +0,0 @@
-#### Wallet Adapter Example
-
-This folder contains an example React frontend that utilizes the Haneul Wallet Adapter. You can view
-the Wallet Adapter source code in the /wallet-adapter/packages folder.
