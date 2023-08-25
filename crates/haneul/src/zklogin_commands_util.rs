12802ecd427d51e8f026f738e5cc012f0cbc6a13
GeunhwaJeong <jeonggh892@gmail.com>
2023-08-25 09:49:32 -1000

    fix: use another default address
    ## Description 
    
    Use a different demo address. 
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul/src/zklogin_commands_util.rs
+++ crates/haneul/src/zklogin_commands_util.rs
@@ -106,7 +106,7 @@ pub async fn perform_zk_login_test_tx(
             coin,
             None,
             5000000,
-            HaneulAddress::random_for_testing_only(),
+            HaneulAddress::ZERO, // as a demo, send to a dummy address
         )
         .await?;
     println!(
