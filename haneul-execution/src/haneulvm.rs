d9793021c1bf8fe5e21b044d216e54a9fe38400b
GeunhwaJeong <jeonggh892@gmail.com>
2023-08-24 21:36:29 -0500

    Execution version to rework integration with the MoveVM
    ## Description 
    
    This PR simply versions the execution layer via the `execution_layer.py`
    script.
    There is no new code here just a copy of the existing latest cut
    
    ## Test Plan 
    
    Nothing really
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ haneul-execution/src/haneulvm.rs
@@ -0,0 +1,191 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::{
+    collections::{BTreeSet, HashSet},
+    sync::Arc,
+};
+
+use move_binary_format::CompiledModule;
+use move_vm_config::verifier::VerifierConfig;
+use haneul_protocol_config::ProtocolConfig;
+use haneul_types::{
+    base_types::{ObjectRef, HaneulAddress, TxContext},
+    committee::EpochId,
+    digests::TransactionDigest,
+    effects::TransactionEffects,
+    error::{ExecutionError, HaneulError, HaneulResult},
+    execution::{ExecutionState, TypeLayoutStore},
+    execution_mode::{self, ExecutionResult},
+    gas::GasCharger,
+    metrics::{BytecodeVerifierMetrics, LimitsMetrics},
+    temporary_store::{InnerTemporaryStore, TemporaryStore},
+    transaction::{ProgrammableTransaction, TransactionKind},
+    type_resolver::LayoutResolver,
+};
+
+use move_vm_runtime_haneulvm::move_vm::MoveVM;
+use haneul_adapter_haneulvm::adapter::{
+    default_verifier_config, new_move_vm, run_metered_move_bytecode_verifier,
+};
+use haneul_adapter_haneulvm::execution_engine::execute_transaction_to_effects;
+use haneul_adapter_haneulvm::programmable_transactions;
+use haneul_adapter_haneulvm::type_layout_resolver::TypeLayoutResolver;
+use haneul_move_natives_haneulvm::all_natives;
+use haneul_verifier_haneulvm::meter::HaneulVerifierMeter;
+
+use crate::executor;
+use crate::verifier;
+
+pub(crate) struct Executor(Arc<MoveVM>);
+
+pub(crate) struct Verifier<'m> {
+    config: VerifierConfig,
+    metrics: &'m Arc<BytecodeVerifierMetrics>,
+    meter: HaneulVerifierMeter,
+}
+
+impl Executor {
+    pub(crate) fn new(
+        protocol_config: &ProtocolConfig,
+        paranoid_type_checks: bool,
+        silent: bool,
+    ) -> Result<Self, HaneulError> {
+        Ok(Executor(Arc::new(new_move_vm(
+            all_natives(silent),
+            protocol_config,
+            paranoid_type_checks,
+        )?)))
+    }
+}
+
+impl<'m> Verifier<'m> {
+    pub(crate) fn new(
+        protocol_config: &ProtocolConfig,
+        is_metered: bool,
+        metrics: &'m Arc<BytecodeVerifierMetrics>,
+    ) -> Self {
+        let config = default_verifier_config(protocol_config, is_metered);
+        let meter = HaneulVerifierMeter::new(&config);
+        Verifier {
+            config,
+            metrics,
+            meter,
+        }
+    }
+}
+
+impl executor::Executor for Executor {
+    fn execute_transaction_to_effects(
+        &self,
+        protocol_config: &ProtocolConfig,
+        metrics: Arc<LimitsMetrics>,
+        enable_expensive_checks: bool,
+        certificate_deny_set: &HashSet<TransactionDigest>,
+        epoch_id: &EpochId,
+        epoch_timestamp_ms: u64,
+        temporary_store: TemporaryStore,
+        shared_object_refs: Vec<ObjectRef>,
+        gas_charger: &mut GasCharger,
+        transaction_kind: TransactionKind,
+        transaction_signer: HaneulAddress,
+        transaction_digest: TransactionDigest,
+        transaction_dependencies: BTreeSet<TransactionDigest>,
+    ) -> (
+        InnerTemporaryStore,
+        TransactionEffects,
+        Result<(), ExecutionError>,
+    ) {
+        execute_transaction_to_effects::<execution_mode::Normal>(
+            shared_object_refs,
+            temporary_store,
+            transaction_kind,
+            transaction_signer,
+            gas_charger,
+            transaction_digest,
+            transaction_dependencies,
+            &self.0,
+            epoch_id,
+            epoch_timestamp_ms,
+            protocol_config,
+            metrics,
+            enable_expensive_checks,
+            certificate_deny_set,
+        )
+    }
+
+    fn dev_inspect_transaction(
+        &self,
+        protocol_config: &ProtocolConfig,
+        metrics: Arc<LimitsMetrics>,
+        enable_expensive_checks: bool,
+        certificate_deny_set: &HashSet<TransactionDigest>,
+        epoch_id: &EpochId,
+        epoch_timestamp_ms: u64,
+        temporary_store: TemporaryStore,
+        shared_object_refs: Vec<ObjectRef>,
+        gas_charger: &mut GasCharger,
+        transaction_kind: TransactionKind,
+        transaction_signer: HaneulAddress,
+        transaction_digest: TransactionDigest,
+        transaction_dependencies: BTreeSet<TransactionDigest>,
+    ) -> (
+        InnerTemporaryStore,
+        TransactionEffects,
+        Result<Vec<ExecutionResult>, ExecutionError>,
+    ) {
+        execute_transaction_to_effects::<execution_mode::DevInspect>(
+            shared_object_refs,
+            temporary_store,
+            transaction_kind,
+            transaction_signer,
+            gas_charger,
+            transaction_digest,
+            transaction_dependencies,
+            &self.0,
+            epoch_id,
+            epoch_timestamp_ms,
+            protocol_config,
+            metrics,
+            enable_expensive_checks,
+            certificate_deny_set,
+        )
+    }
+
+    fn update_genesis_state(
+        &self,
+        protocol_config: &ProtocolConfig,
+        metrics: Arc<LimitsMetrics>,
+        state_view: &mut dyn ExecutionState,
+        tx_context: &mut TxContext,
+        gas_charger: &mut GasCharger,
+        pt: ProgrammableTransaction,
+    ) -> Result<(), ExecutionError> {
+        programmable_transactions::execution::execute::<execution_mode::Genesis>(
+            protocol_config,
+            metrics,
+            &self.0,
+            state_view,
+            tx_context,
+            gas_charger,
+            pt,
+        )
+    }
+
+    fn type_layout_resolver<'r, 'vm: 'r, 'store: 'r>(
+        &'vm self,
+        store: Box<dyn TypeLayoutStore + 'store>,
+    ) -> Box<dyn LayoutResolver + 'r> {
+        Box::new(TypeLayoutResolver::new(&self.0, store))
+    }
+}
+
+impl<'m> verifier::Verifier for Verifier<'m> {
+    fn meter_compiled_modules(
+        &mut self,
+        _protocol_config: &ProtocolConfig,
+        modules: &[CompiledModule],
+    ) -> HaneulResult<()> {
+        run_metered_move_bytecode_verifier(modules, &self.config, &mut self.meter, self.metrics)
+    }
+}
