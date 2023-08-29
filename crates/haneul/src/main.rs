da620abbf99ad3f07e2ea4f12fb390dc8df1dcf8
GeunhwaJeong <jeonggh892@gmail.com>
2023-08-29 10:01:27 -0700

    [cli] change default logging level for Haneul CLI (Client, Move, Keytool)
    ## Description 
    
    This PR changes the default logging level for Haneul CLI (Client, Move,
    Keytool) from info to error. The logging level can be overwritten by
    setting the RUST_LOG env variable.
    
    ## Test Plan 
    
    All tests pass, manual visual test using the Haneul binary (no info logs
    are shown when issuing commands).
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [x] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes
    This PR changes the default logging level for Haneul CLI (Client, Move,
    Keytool) from info to error. The logging level can be overwritten by
    setting the RUST_LOG env variable.

--- crates/haneul/src/main.rs
+++ crates/haneul/src/main.rs
@@ -42,15 +42,15 @@ async fn main() {
     #[cfg(windows)]
     colored::control::set_virtual_terminal(true).unwrap();
 
-    let bin_name = env!("CARGO_BIN_NAME");
     let args = Args::parse();
     let _guard = match args.command {
-        HaneulCommand::Console { .. } | HaneulCommand::Client { .. } => {
-            telemetry_subscribers::TelemetryConfig::new()
-                .with_log_file(&format!("{bin_name}.log"))
-                .with_env()
-                .init()
-        }
+        HaneulCommand::Console { .. }
+        | HaneulCommand::Client { .. }
+        | HaneulCommand::KeyTool { .. }
+        | HaneulCommand::Move { .. } => telemetry_subscribers::TelemetryConfig::new()
+            .with_log_level("error")
+            .with_env()
+            .init(),
         _ => telemetry_subscribers::TelemetryConfig::new()
             .with_env()
             .init(),
