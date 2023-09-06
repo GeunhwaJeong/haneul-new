19824533ca9cfa059b619af896452a6190d4eff7
GeunhwaJeong <jeonggh892@gmail.com>
2023-09-06 02:31:39 +0200

    [graphql/rpc] allow short addresses on input
    ## Description 
    
    As title says
    
    ## Test Plan 
    
    Manual
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-graphql-rpc/src/types/haneul_address.rs
+++ crates/haneul-graphql-rpc/src/types/haneul_address.rs
@@ -20,15 +20,18 @@ impl ScalarType for HaneulAddress {
                         "Invalid HaneulAddress. Missing 0x prefix",
                     ));
                 }
-
-                let bytes = hex::decode(s)?;
-                if bytes.len() != HANEUL_ADDRESS_LENGTH {
+                if s.is_empty() || s.len() > HANEUL_ADDRESS_LENGTH * 2 {
                     return Err(InputValueError::custom(format!(
-                        "Expected HaneulAddress of length {}, received {}.",
+                        "Expected HaneulAddress string ranging from length 1 up to {} ({} bytes) or less, received {}.",
+                        HANEUL_ADDRESS_LENGTH * 2,
                         HANEUL_ADDRESS_LENGTH,
-                        bytes.len()
+                        s.len()
                     )));
                 }
+                // Pad to HANEUL_ADDRESS_LENGTH*2 width
+                s = format!("{:0>width$}", s, width = HANEUL_ADDRESS_LENGTH * 2);
+
+                let bytes = hex::decode(s)?;
                 let mut arr = [0u8; HANEUL_ADDRESS_LENGTH];
                 arr.copy_from_slice(&bytes);
                 Ok(HaneulAddress(arr))
@@ -177,7 +180,9 @@ mod tests {
 
     #[test]
     fn test_parse_invalid_length() {
-        let input = Value::String("0x0123456789abcdef".to_string());
+        let input = Value::String(
+            "0x0123456789abcdef0123456789abcdef01000023456789abcdef0123456789abcdef".to_string(),
+        );
         let parsed = <HaneulAddress as ScalarType>::parse(input);
         assert_input_value_error(parsed);
     }
