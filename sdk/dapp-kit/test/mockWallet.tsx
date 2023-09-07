964443aa4111d8108a4793eae8fd06ef65cb44c0
GeunhwaJeong <jeonggh892@gmail.com>
2023-09-07 15:54:45 -0400

    [dapp-kit] write tests for the new WalletProvider component / hook
    ## Description 
    
    I didn't write tests for https://github.com/GeunhwaJeong/haneul/pull/13651
    because I was lazy and trying to get this wrapped up 😆, but then I
    realized these are pretty important to have for all the various edge
    cases and pieces of functionality that come with managing wallet and
    account connections.
    
    ## Test Plan 
    - Automated tests pass
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ sdk/dapp-kit/test/mockWallet.tsx
@@ -0,0 +1,72 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { Ed25519Keypair } from '@haneullabs/haneul.js/keypairs/ed25519';
+import type { IdentifierRecord } from '@haneullabs/wallet-standard';
+import { ReadonlyWalletAccount, HANEUL_CHAINS } from '@haneullabs/wallet-standard';
+import type { Wallet } from '@haneullabs/wallet-standard';
+
+export class MockWallet implements Wallet {
+	version = '1.0.0' as const;
+	icon = `data:image/png;base64,` as const;
+	chains = HANEUL_CHAINS;
+	#walletName: string;
+	#additionalFeatures: IdentifierRecord<unknown>;
+
+	#connect = vi.fn().mockReturnValue({ accounts: this.accounts });
+	#disconnect = vi.fn();
+	#on = vi.fn();
+	#signPersonalMessage = vi.fn();
+	#signTransactionBlock = vi.fn();
+	#signAndExecuteTransactionBlock = vi.fn();
+
+	constructor(name: string, additionalFeatures: IdentifierRecord<unknown>) {
+		this.#walletName = name;
+		this.#additionalFeatures = additionalFeatures;
+	}
+
+	get name() {
+		return this.#walletName;
+	}
+
+	get accounts() {
+		const keypair = new Ed25519Keypair();
+		const account = new ReadonlyWalletAccount({
+			address: keypair.getPublicKey().toHaneulAddress(),
+			publicKey: keypair.getPublicKey().toHaneulBytes(),
+			chains: ['haneul:unknown'],
+			features: ['haneul:signAndExecuteTransactionBlock', 'haneul:signTransactionBlock'],
+		});
+		return [account];
+	}
+
+	get features(): IdentifierRecord<unknown> {
+		return {
+			'standard:connect': {
+				version: '1.0.0',
+				connect: this.#connect,
+			},
+			'standard:disconnect': {
+				version: '1.0.0',
+				disconnect: this.#disconnect,
+			},
+			'standard:events': {
+				version: '1.0.0',
+				on: this.#on,
+			},
+			'haneul:signPersonalMessage': {
+				version: '1.0.0',
+				signPersonalMessage: this.#signPersonalMessage,
+			},
+			'haneul:signTransactionBlock': {
+				version: '1.0.0',
+				signTransactionBlock: this.#signTransactionBlock,
+			},
+			'haneul:signAndExecuteTransactionBlock': {
+				version: '1.0.0',
+				signAndExecuteTransactionBlock: this.#signAndExecuteTransactionBlock,
+			},
+			...this.#additionalFeatures,
+		};
+	}
+}
--- sdk/dapp-kit/test/test-utils.tsx
+++ sdk/dapp-kit/test/test-utils.tsx
@@ -2,10 +2,30 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import type { HaneulClient } from '@haneullabs/haneul.js/client';
-import { HaneulClientProvider } from 'dapp-kit/src';
+import type { IdentifierRecord } from '@haneullabs/wallet-standard';
+import { getWallets } from '@haneullabs/wallet-standard';
+import { HaneulClientProvider, WalletProvider } from 'dapp-kit/src';
+import { MockWallet } from './mockWallet.js';
+import type { ComponentProps } from 'react';
 
 export function createHaneulClientContextWrapper(client: HaneulClient) {
 	return function HaneulClientContextWrapper({ children }: { children: React.ReactNode }) {
 		return <HaneulClientProvider networks={{ test: client }}>{children}</HaneulClientProvider>;
 	};
+}
+
+export function createWalletProviderContextWrapper(
+	providerProps: Omit<ComponentProps<typeof WalletProvider>, 'children'> = {},
+) {
+	return function WalletProviderContextWrapper({ children }: { children: React.ReactNode }) {
+		return <WalletProvider {...providerProps}>{children}</WalletProvider>;
+	};
+}
+
+export function registerMockWallet(
+	walletName: string,
+	additionalFeatures: IdentifierRecord<unknown> = {},
+) {
+	const walletsApi = getWallets();
+	return walletsApi.register(new MockWallet(walletName, additionalFeatures));
 }
