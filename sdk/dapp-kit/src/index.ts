ebadd5c4e52a081907f1ee8d7b28ec2f45e25a4f
GeunhwaJeong <jeonggh892@gmail.com>
2023-09-11 10:56:00 -0400

    [dapp-kit] add new useDisconnectWallet mutation hook
    ## Description 
    
    This PR adds a new `useDisconnectWallet` mutation hook with tests for
    disconnecting from an active wallet connection!
    
    Equivalent PR for connecting:
    https://github.com/GeunhwaJeong/haneul/pull/13661
    
    ## Test Plan 
    - Automated tests pass
    - Manual testing in an example dApp
    - CI
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- sdk/dapp-kit/src/index.ts
+++ sdk/dapp-kit/src/index.ts
@@ -7,6 +7,7 @@ export * from './components/WalletProvider.js';
 export * from './hooks/useRpcApiVersion.js';
 export * from './hooks/rpc/index.js';
 export * from './hooks/wallet/useConnectWallet.js';
+export * from './hooks/wallet/useDisconnectWallet.js';
 export * from './hooks/wallet/useWallet.js';
 export * from './hooks/useHaneulClientMutation.js';
 export * from './hooks/useHaneulClientQuery.js';
