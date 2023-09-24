ee677090f0154813615d07dda979b70bb4d1685e
GeunhwaJeong <jeonggh892@gmail.com>
2023-09-23 21:10:14 -0400

    [dapp-kit] add build pipeline for bundling UI code with vanilla-extract for styling
    ## Description 
    
    This PR sets up the build infrastructure required to package our UI
    components that'll leverage vanilla-extract for styling. We don't want
    external consumers to have to worry about what styling technologies
    we're using, so we're going to pre-bundle our CSS in `dapp-kit` at build
    time 🏗️ . For Haneul Explorer, we can still support source imports by
    bundling the CSS in dapp-kit from Haneul Explorer!
    
    ## Test Plan 
    - [ESM version] Tested in Haneul Explorer that importing the
    `haneullabs/dapp-kit/index.css` correctly imports the test CSS I added +
    `ConnectButton` renders successfully
    - [Source version] Tested in Haneul Explorer that we can forgo importing
    index.css and bundle the CSS ourselves using the vite vanilla-extract
    plugin + `ConnectButton` renders successfully
    - CI
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- sdk/dapp-kit/src/index.ts
+++ sdk/dapp-kit/src/index.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export * from './hooks/useHaneulClient.js';
+export * from './components/ConnectButton.js';
 export * from './components/HaneulClientProvider.js';
 export * from './components/WalletProvider.js';
 export * from './hooks/useRpcApiVersion.js';
