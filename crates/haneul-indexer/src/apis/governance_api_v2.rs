3237d3a909494d5503603e024988401dc7106f70
GeunhwaJeong <jeonggh892@gmail.com>
2023-10-04 10:41:29 -0500

    indexer-v2: support validator-apys

--- crates/haneul-indexer/src/apis/governance_api_v2.rs
+++ crates/haneul-indexer/src/apis/governance_api_v2.rs
@@ -5,21 +5,22 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-use crate::errors::IndexerError;
-use crate::indexer_reader::IndexerReader;
+use crate::{errors::IndexerError, indexer_reader::IndexerReader};
 use async_trait::async_trait;
-use jsonrpsee::core::RpcResult;
-use jsonrpsee::RpcModule;
+use jsonrpsee::{core::RpcResult, RpcModule};
 
-use haneul_json_rpc::api::GovernanceReadApiServer;
-use haneul_json_rpc::HaneulRpcModule;
-use haneul_json_rpc_types::{DelegatedStake, ValidatorApys};
-use haneul_json_rpc_types::{EpochInfo, HaneulCommittee};
+use cached::{proc_macro::cached, SizedCache};
+use haneul_json_rpc::{
+    api::GovernanceReadApiServer, governance_api::ValidatorExchangeRates, HaneulRpcModule,
+};
+use haneul_json_rpc_types::{DelegatedStake, EpochInfo, HaneulCommittee, ValidatorApys};
 use haneul_open_rpc::Module;
-use haneul_types::base_types::{ObjectID, HaneulAddress};
-use haneul_types::committee::EpochId;
-use haneul_types::haneul_serde::BigInt;
-use haneul_types::haneul_system_state::haneul_system_state_summary::HaneulSystemStateSummary;
+use haneul_types::{
+    base_types::{ObjectID, HaneulAddress},
+    committee::EpochId,
+    haneul_serde::BigInt,
+    haneul_system_state::haneul_system_state_summary::HaneulSystemStateSummary,
+};
 
 #[derive(Clone)]
 pub(crate) struct GovernanceReadApiV2 {
@@ -44,6 +45,117 @@ impl GovernanceReadApiV2 {
             Err(e) => Err(e),
         }
     }
+
+    async fn get_latest_haneul_system_state(&self) -> Result<HaneulSystemStateSummary, IndexerError> {
+        self.inner
+            .spawn_blocking(|this| this.get_latest_haneul_system_state())
+            .await
+    }
+}
+
+/// Cached exchange rates for validators for the given epoch, the cache size is 1, it will be cleared when the epoch changes.
+/// rates are in descending order by epoch.
+#[cached(
+    type = "SizedCache<EpochId, Vec<ValidatorExchangeRates>>",
+    create = "{ SizedCache::with_size(1) }",
+    convert = "{ system_state_summary.epoch }",
+    result = true
+)]
+async fn exchange_rates(
+    state: &GovernanceReadApiV2,
+    system_state_summary: HaneulSystemStateSummary,
+) -> Result<Vec<ValidatorExchangeRates>, IndexerError> {
+    // Get validator rate tables
+    let mut tables = vec![];
+
+    for validator in system_state_summary.active_validators {
+        tables.push((
+            validator.haneul_address,
+            validator.staking_pool_id,
+            validator.exchange_rates_id,
+            validator.exchange_rates_size,
+            true,
+        ));
+    }
+
+    // Get inactive validator rate tables
+    for df in state
+        .inner
+        .get_dynamic_fields_in_blocking_task(
+            system_state_summary.inactive_pools_id,
+            None,
+            system_state_summary.inactive_pools_size as usize,
+        )
+        .await?
+    {
+        let pool_id: haneul_types::id::ID = bcs::from_bytes(&df.bcs_name).map_err(|e| {
+            haneul_types::error::HaneulError::ObjectDeserializationError {
+                error: e.to_string(),
+            }
+        })?;
+        let inactive_pools_id = system_state_summary.inactive_pools_id;
+        let validator = state
+            .inner
+            .spawn_blocking(move |this| {
+                haneul_types::haneul_system_state::get_validator_from_table(
+                    &this,
+                    inactive_pools_id,
+                    &pool_id,
+                )
+            })
+            .await?;
+        tables.push((
+            validator.haneul_address,
+            validator.staking_pool_id,
+            validator.exchange_rates_id,
+            validator.exchange_rates_size,
+            false,
+        ));
+    }
+
+    let mut exchange_rates = vec![];
+    // Get exchange rates for each validator
+    for (address, pool_id, exchange_rates_id, exchange_rates_size, active) in tables {
+        let mut rates = vec![];
+        for df in state
+            .inner
+            .get_dynamic_fields_in_blocking_task(
+                exchange_rates_id,
+                None,
+                exchange_rates_size as usize,
+            )
+            .await?
+        {
+            let epoch: EpochId = bcs::from_bytes(&df.bcs_name).map_err(|e| {
+                haneul_types::error::HaneulError::ObjectDeserializationError {
+                    error: e.to_string(),
+                }
+            })?;
+
+            let exchange_rate: haneul_types::haneul_system_state::PoolTokenExchangeRate = state
+                .inner
+                .spawn_blocking(move |this| {
+                    haneul_types::dynamic_field::get_dynamic_field_from_store(
+                        &this,
+                        exchange_rates_id,
+                        &epoch,
+                    )
+                })
+                .await?;
+
+            rates.push((epoch, exchange_rate));
+        }
+
+        rates.sort_by(|(a, _), (b, _)| a.cmp(b).reverse());
+
+        exchange_rates.push(ValidatorExchangeRates {
+            address,
+            pool_id,
+            active,
+            rates,
+        });
+    }
+    Ok(exchange_rates)
 }
 
 #[async_trait]
@@ -67,10 +179,7 @@ impl GovernanceReadApiServer for GovernanceReadApiV2 {
     }
 
     async fn get_latest_haneul_system_state(&self) -> RpcResult<HaneulSystemStateSummary> {
-        self.inner
-            .spawn_blocking(|this| this.get_latest_haneul_system_state())
-            .await
-            .map_err(Into::into)
+        self.get_latest_haneul_system_state().await.map_err(Into::into)
     }
 
     async fn get_reference_gas_price(&self) -> RpcResult<BigInt<u64>> {
@@ -85,8 +194,19 @@ impl GovernanceReadApiServer for GovernanceReadApiV2 {
     }
 
     async fn get_validators_apy(&self) -> RpcResult<ValidatorApys> {
-        // Need Dynamic field queries
-        unimplemented!()
+        let system_state_summary: HaneulSystemStateSummary =
+            self.get_latest_haneul_system_state().await?;
+        let epoch = system_state_summary.epoch;
+        let stake_subsidy_start_epoch = system_state_summary.stake_subsidy_start_epoch;
+
+        let exchange_rate_table = exchange_rates(self, system_state_summary).await?;
+
+        let apys = haneul_json_rpc::governance_api::calculate_apys(
+            stake_subsidy_start_epoch,
+            exchange_rate_table,
+        );
+
+        Ok(ValidatorApys { apys, epoch })
     }
 }
 
