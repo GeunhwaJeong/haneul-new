ae2cbb0c650e0d4275018cd7cbb31cc8ba4783fc
GeunhwaJeong <jeonggh892@gmail.com>
2023-10-05 09:48:13 -0700

    [benchmark] Support benchmarking tx manager
    ## Description 
    
    This PR extends the single-node-benchmark to be able to benchmark the
    TransactionManager.
    Specifically it now supports 3 different components:
    1. Execution and storage layer only
    2. (1) + TxManager
    3. ValidatorService, which is basically (2) + Cert verification
    
    ## Test Plan 
    
    CI
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-single-node-benchmark/tests/smoke_tests.rs
+++ crates/haneul-single-node-benchmark/tests/smoke_tests.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use haneul_macros::sim_test;
+use haneul_single_node_benchmark::command::Component;
 use haneul_single_node_benchmark::execution::{
     benchmark_move_transactions, benchmark_simple_transfer,
 };
@@ -9,13 +10,15 @@ use haneul_single_node_benchmark::execution::{
 #[sim_test]
 async fn benchmark_simple_transfer_smoke_test() {
     // This test makes sure that the benchmark runs.
-    benchmark_simple_transfer(10, false).await;
-    benchmark_simple_transfer(10, true).await;
+    benchmark_simple_transfer(10, Component::Baseline).await;
+    benchmark_simple_transfer(10, Component::WithTxManager).await;
+    benchmark_simple_transfer(10, Component::ValidatorService).await;
 }
 
 #[sim_test]
 async fn benchmark_move_transactions_smoke_test() {
     // This test makes sure that the benchmark runs.
-    benchmark_move_transactions(10, false, 2, 1, 1).await;
-    benchmark_move_transactions(10, true, 2, 1, 1).await;
+    benchmark_move_transactions(10, Component::Baseline, 2, 1, 1).await;
+    benchmark_move_transactions(10, Component::WithTxManager, 2, 1, 1).await;
+    benchmark_move_transactions(10, Component::ValidatorService, 2, 1, 1).await;
 }
