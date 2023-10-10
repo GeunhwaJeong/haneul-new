4e6ca4af1ae6128fa23045a4a3c28b1dc427c52b
GeunhwaJeong <jeonggh892@gmail.com>
2023-10-10 13:38:30 -0700

    [Cleanup] remove remaining logic for input object fastpath
    ## Description 
    
    It seems this optimization is unlikely to come back, so removing the
    remaining scaffolding.
    
    ## Test Plan 
    
    CI
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-e2e-tests/tests/transfer_to_object_tests.rs
+++ crates/haneul-e2e-tests/tests/transfer_to_object_tests.rs
@@ -10,7 +10,7 @@ use haneul_types::base_types::{ObjectID, ObjectRef};
 use haneul_types::effects::TransactionEffectsAPI;
 use haneul_types::effects::{TransactionEffects, TransactionEvents};
 use haneul_types::error::HaneulError;
-use haneul_types::object::{Object, Owner};
+use haneul_types::object::Owner;
 use haneul_types::transaction::{CallArg, ObjectArg, Transaction};
 use test_cluster::{TestCluster, TestClusterBuilder};
 
@@ -175,11 +175,12 @@ impl TestEnvironment {
             .build();
         self.test_cluster.wallet.sign_transaction(&transaction)
     }
+
     async fn move_call(
         &self,
         function: &'static str,
         arguments: Vec<CallArg>,
-    ) -> anyhow::Result<(TransactionEffects, TransactionEvents, Vec<Object>)> {
+    ) -> anyhow::Result<(TransactionEffects, TransactionEvents)> {
         let transaction = self.create_move_call(function, arguments).await;
         self.test_cluster
             .execute_transaction_return_raw_effects(transaction)
@@ -187,7 +188,7 @@ impl TestEnvironment {
     }
 
     async fn start(&self) -> (ObjectRef, ObjectRef) {
-        let (fx, _, _) = self.move_call("start", vec![]).await.unwrap();
+        let (fx, _) = self.move_call("start", vec![]).await.unwrap();
         assert!(fx.status().is_ok());
 
         get_parent_and_child(fx.created())
