2965d4ef7fd03dafd910bab53d54819083fd55d7
GeunhwaJeong <jeonggh892@gmail.com>
2023-10-10 19:53:02 -0700

    sim data source 2/x: impl node state getter for simulacrum
    ## Description 
    
    Continuation of https://github.com/GeunhwaJeong/haneul/pull/14164
    This will allow the simulator feed data into indexer etc
    
    ## Test Plan 
    
    N/A
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-rest-api/src/node_state_getter.rs
+++ crates/haneul-rest-api/src/node_state_getter.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use haneul_core::authority::AuthorityState;
+use haneul_types::error::UserInputError;
 use haneul_types::{
     base_types::{ObjectID, VersionNumber},
     digests::{TransactionDigest, TransactionEventsDigest},
@@ -116,5 +117,94 @@ impl NodeStateGetter for AuthorityState {
 
     fn get_object(&self, object_id: &ObjectID) -> Result<Option<Object>, HaneulError> {
         self.database.get_object(object_id)
+    }
+}
+
+impl<T: Sync + Send> NodeStateGetter for simulacrum::Simulacrum<T> {
+    fn get_verified_checkpoint_by_sequence_number(
+        &self,
+        sequence_number: CheckpointSequenceNumber,
+    ) -> HaneulResult<VerifiedCheckpoint> {
+        self.store()
+            .get_checkpoint_by_sequence_number(sequence_number)
+            .cloned()
+            .ok_or(HaneulError::UserInputError {
+                error: UserInputError::VerifiedCheckpointNotFound(sequence_number),
+            })
+    }
+
+    fn get_latest_checkpoint_sequence_number(&self) -> HaneulResult<CheckpointSequenceNumber> {
+        Ok(self
+            .store()
+            .get_highest_checkpint()
+            .map(|checkpoint| *checkpoint.sequence_number())
+            .unwrap_or(0))
+    }
+
+    fn get_checkpoint_contents(
+        &self,
+        content_digest: CheckpointContentsDigest,
+    ) -> HaneulResult<CheckpointContents> {
+        self.store()
+            .get_checkpoint_contents(&content_digest)
+            .cloned()
+            .ok_or(HaneulError::UserInputError {
+                error: UserInputError::CheckpointContentsNotFound(content_digest),
+            })
+    }
+
+    fn multi_get_transaction_blocks(
+        &self,
+        tx_digests: &[TransactionDigest],
+    ) -> HaneulResult<Vec<Option<VerifiedTransaction>>> {
+        Ok(tx_digests
+            .iter()
+            .map(|digest| self.store().get_transaction(digest).cloned())
+            .collect())
+    }
+
+    fn multi_get_executed_effects(
+        &self,
+        digests: &[TransactionDigest],
+    ) -> HaneulResult<Vec<Option<TransactionEffects>>> {
+        Ok(digests
+            .iter()
+            .map(|digest| self.store().get_transaction_effects(digest).cloned())
+            .collect())
+    }
+
+    fn multi_get_events(
+        &self,
+        event_digests: &[TransactionEventsDigest],
+    ) -> HaneulResult<Vec<Option<TransactionEvents>>> {
+        Ok(event_digests
+            .iter()
+            .map(|digest| self.store().get_transaction_events(digest).cloned())
+            .collect())
+    }
+
+    fn multi_get_object_by_key(
+        &self,
+        object_keys: &[ObjectKey],
+    ) -> Result<Vec<Option<Object>>, HaneulError> {
+        object_keys
+            .iter()
+            .map(|key| ObjectStore::get_object_by_key(&self.store(), &key.0, key.1))
+            .collect::<Result<Vec<_>, HaneulError>>()
+    }
+
+    fn get_object_by_key(
+        &self,
+        object_id: &ObjectID,
+        version: VersionNumber,
+    ) -> Result<Option<Object>, HaneulError> {
+        Ok(self
+            .store()
+            .get_object_at_version(object_id, version)
+            .cloned())
+    }
+
+    fn get_object(&self, object_id: &ObjectID) -> Result<Option<Object>, HaneulError> {
+        ObjectStore::get_object(&self.store(), object_id)
     }
 }
