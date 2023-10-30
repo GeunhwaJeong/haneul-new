09b6ef8fd101a2ac1bf9de097bb8a52d9b9b0ccb
GeunhwaJeong <jeonggh892@gmail.com>
2023-10-30 15:38:38 -0700

    [graphql] accurate costing
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test Plan 
    
    How did you test the new or updated feature?
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-graphql-rpc/src/types/transaction_block.rs
+++ crates/haneul-graphql-rpc/src/types/transaction_block.rs
@@ -244,7 +244,7 @@ pub enum ExecutionStatus {
     Failure,
 }
 
-#[derive(InputObject, Debug, Default)]
+#[derive(InputObject, Debug, Default, Clone)]
 pub(crate) struct TransactionBlockFilter {
     pub package: Option<HaneulAddress>,
     pub module: Option<String>,
