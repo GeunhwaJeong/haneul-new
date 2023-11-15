f10e4cf4c42b02c7d62a02f6eab8c53ca7d6e286
GeunhwaJeong <jeonggh892@gmail.com>
2023-11-15 14:03:05 -0700

    [Docs] Updates to Rust SDK information
    ## Description 
    
    Pointing the nav directly to the GitHub repo.
    
    ## Test Plan 
    
    Local
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- docs/content/sidebars/references.js
+++ docs/content/sidebars/references.js
@@ -57,7 +57,11 @@ const references = [
 				label: 'Haneul TypeScript SDK Site',
 				href: 'https://haneul-typescript-docs.vercel.app/typescript',
 			},
-			'references/sdk/rust-sdk',
+			{
+				type: 'link',
+				label: 'Haneul Rust SDK GitHub',
+				href: 'https://github.com/GeunhwaJeong/haneul/tree/main/crates/haneul-sdk',
+			}
 		],
 	},
 	{
