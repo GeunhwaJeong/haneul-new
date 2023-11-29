290c119cd38efea24b49e1caad8d42716023861c
GeunhwaJeong <jeonggh892@gmail.com>
2023-11-29 11:29:27 -0800

    [graphql/rpc] split out graphiql server
    ## Description 
    
    As mentioned in
    [this](https://github.com/GeunhwaJeong/haneul/pull/14960#issuecomment-1820785396)
    comment:
    this is the first part of modularizing the server.
    The GraphiQL server with the IDE is decoupled from the core server
    Subseq PR will improve the initializers to make setting IDE title driven
    by caller at builder time.
    
    The changes here should be opaque to users. No need for release  notes.
    
    ## Test Plan 
    
    Existing
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-graphql-rpc/src/main.rs
+++ crates/haneul-graphql-rpc/src/main.rs
@@ -9,8 +9,9 @@ use haneul_graphql_rpc::commands::Command;
 use haneul_graphql_rpc::config::Ide;
 use haneul_graphql_rpc::config::{ConnectionConfig, ServerConfig, ServiceConfig};
 use haneul_graphql_rpc::schema_sdl_export;
-use haneul_graphql_rpc::server::builder::Server;
-use haneul_graphql_rpc::server::simple_server::start_example_server;
+use haneul_graphql_rpc::server::graphiql_server::{
+    start_graphiql_server, start_graphiql_server_from_cfg_path,
+};
 use tracing::error;
 
 #[tokio::main]
@@ -74,20 +75,16 @@ async fn main() {
                 ..ServerConfig::default()
             };
 
-            start_example_server(&server_config).await.unwrap();
+            start_graphiql_server(&server_config).await.unwrap();
         }
         Command::FromConfig { path } => {
-            let server = Server::from_yaml_config(path.to_str().unwrap());
             println!("Starting server...");
-            server
+            start_graphiql_server_from_cfg_path(path.to_str().unwrap())
                 .await
                 .map_err(|x| {
                     error!("Error: {:?}", x);
                     x
                 })
-                .unwrap()
-                .run()
-                .await
                 .unwrap();
         }
     }
