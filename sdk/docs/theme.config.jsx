5b514592c574f22e9482c5ed6916a35fd9f676bf
GeunhwaJeong <jeonggh892@gmail.com>
2023-12-04 08:09:56 -0800

    [sdk docs] Cleanup SEO

--- sdk/docs/theme.config.jsx
+++ sdk/docs/theme.config.jsx
@@ -1,6 +1,8 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { useRouter } from 'next/router';
+
 const config = {
 	logo: <span>Haneul TypeScript Docs</span>,
 	project: {
@@ -13,9 +15,30 @@ const config = {
 	footer: {
 		text: 'Copyright © 2023, Haneul Labs, Inc.',
 	},
+	head: (
+		<>
+			<meta name="google-site-verification" content="vRwZ3B3JxeegVwxa01XL4edDasP3pT8jPxEiBqqEaqw" />
+			<meta httpEquiv="Content-Language" content="en" />
+		</>
+	),
 	useNextSeoProps() {
+		const { asPath } = useRouter();
+
 		return {
-			titleTemplate: '%s',
+			titleTemplate: asPath !== '/' ? '%s | Haneul TypeScript Docs' : 'Haneul TypeScript Docs',
+			description:
+				'Haneul TypeScript Documentation. Discover the power of Haneul through examples, guides, and concepts.',
+			openGraph: {
+				title: 'Haneul TypeScript Docs',
+				description:
+					'Haneul TypeScript Documentation. Discover the power of Haneul through examples, guides, and concepts.',
+				site_name: 'Haneul TypeScript Docs',
+			},
+			additionalMetaTags: [{ content: 'Haneul TypeScript Docs', name: 'apple-mobile-web-app-title' }],
+			twitter: {
+				card: 'summary_large_image',
+				site: '@Haneul_Labs',
+			},
 		};
 	},
 };
