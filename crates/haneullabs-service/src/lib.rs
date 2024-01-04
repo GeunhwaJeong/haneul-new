f10cd2bba5484b45a5c38a205d682fa743676a1a
GeunhwaJeong <jeonggh892@gmail.com>
2024-01-04 11:40:37 -0700

    [haneullabs-service] 1/n initialize crate for service dependencies
    ## Description 
    
    Doing some work to bring this to the haneul repo – basic service utilities
    for new services.
    
    Full list of items to eventually include in [this notion
    doc](https://www.notion.so/haneullabs/code-bootstrapping-de278ec675c6473ba6004f22baf610eb?pvs=4)
    
    ## Test Plan 
    
    cargo test
    ```
    
    running 3 tests
    test health::tests::health_response_new_works ... ok
    test tests::package_name_works ... ok
    test tests::package_version_works ... ok
    
    test result: ok. 3 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
    
         Running tests/integration_test.rs (/Users/jeong-gh/haneullabs/haneul/target/debug/deps/integration_test-85ebcfb09adf53fc)
    
    running 1 test
    test test_add ... ok
    
    test result: ok. 1 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
    
       Doc-tests haneullabs-service
    
    running 0 tests
    
    test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
    ```
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ crates/haneullabs-service/src/lib.rs
@@ -0,0 +1,39 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+mod health;
+mod service;
+
+pub use service::get_haneullabs_service;
+pub use service::serve;
+
+pub const DEFAULT_PORT: u16 = 2024;
+
+#[macro_export]
+macro_rules! package_name {
+    () => {
+        env!("CARGO_PKG_NAME")
+    };
+}
+
+#[macro_export]
+macro_rules! package_version {
+    () => {
+        env!("CARGO_PKG_VERSION")
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn package_name_works() {
+        assert_eq!(package_name!(), "haneullabs-service",);
+    }
+
+    #[test]
+    fn package_version_works() {
+        assert_eq!(package_version!(), "0.0.1",);
+    }
+}
