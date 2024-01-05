7fe4f8968028fbc4bc02d8cebaf22120a54689e3
GeunhwaJeong <jeonggh892@gmail.com>
2024-01-05 18:59:01 -0500

    [dapp-kit] default storage to memory storage when local storage isn't available in an SSR context
    ## Description 
    
    dapp-kit breaks when used in a framework like Next since we default to
    using local storage for persisting connection data which doesn't exist
    in an SSR context. While we're working on better support for these
    frameworks, this PR fixes the immediate breakage issue by defaulting to
    memory storage when local storage isn't available. I also added the
    option to entirely disable data persistence by setting `storage: null`
    in the `WalletProvider`.
    
    Most of this was copy-pasta'd from Jordan-Haneullabs's Next PR, I did hoist
    the storage determination logic up to the `WalletProvider` since I think
    it might be more clear to consumers what storage ends up being used. I
    don't have a super strong opinion on where this lives
    (`WalletProvider.tsx` or `walletStore.ts`), so I'm glad to change this
    back if anyone has a preference.
    
    ## Test Plan 
    - Existing persistence tests pass
    - Tested manually
    - CI
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

---
+++ .changeset/smooth-carrots-jam.md
@@ -0,0 +1,5 @@
+---
+'@haneullabs/dapp-kit': patch
+---
+
+Default storage to memory storage when local storage isn't available during SSR
---
+++ sdk/dapp-kit/src/constants/walletDefaults.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import type { WalletWithRequiredFeatures } from '@haneullabs/wallet-standard';
+
+import { createInMemoryStore } from '../utils/stateStorage.js';
+
+export const HANEUL_WALLET_NAME = 'Haneul Wallet';
+
+export const DEFAULT_STORAGE =
+	typeof window !== 'undefined' && window.localStorage ? localStorage : createInMemoryStore();
+
+export const DEFAULT_STORAGE_KEY = 'haneul-dapp-kit:wallet-connection-info';
+
+export const DEFAULT_REQUIRED_FEATURES: (keyof WalletWithRequiredFeatures['features'])[] = [
+	'haneul:signTransactionBlock',
+];
