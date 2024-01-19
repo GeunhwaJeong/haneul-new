691db87e5c01932499802f580f47d82bc0c29a51
GeunhwaJeong <jeonggh892@gmail.com>
2024-01-19 14:53:07 +0900

    add the display tables

--- crates/haneul-replay/src/displays/gas_status_displays.rs
+++ crates/haneul-replay/src/displays/gas_status_displays.rs
@@ -5,7 +5,6 @@ use crate::displays::Pretty;
 use std::fmt::{Display, Formatter};
 use haneul_types::gas::HaneulGasStatus;
 use haneul_types::gas_model::gas_v2::HaneulGasStatus as GasStatusV2;
-#[allow(unused)]
 use tabled::{
     builder::Builder as TableBuilder,
     settings::{style::HorizontalLine, Style as TableStyle},
@@ -14,39 +13,65 @@ use tabled::{
 impl<'a> Display for Pretty<'a, HaneulGasStatus> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let Pretty(haneul_gas_status) = self;
-        Ok(match haneul_gas_status {
+        match haneul_gas_status {
             HaneulGasStatus::V2(s) => {
-                display_info(f, &s)?;
+                display_info(f, s)?;
+                per_object_storage_table(f, s)?;
             }
-        })
+        };
+        Ok(())
     }
 }
 
-#[allow(unused)]
-fn per_object_storage_table(f: &mut Formatter, _haneul_gas_status: &GasStatusV2) -> String {
-    todo!();
+fn per_object_storage_table(f: &mut Formatter, haneul_gas_status: &GasStatusV2) -> std::fmt::Result {
+    let mut builder = TableBuilder::default();
+    builder.push_record(vec!["Object ID", "Bytes", "Old Rebate", "New Rebate"]);
+    for (object_id, per_obj_storage) in haneul_gas_status.per_object_storage() {
+        builder.push_record(vec![
+            object_id.to_string(),
+            per_obj_storage.new_size.to_string(),
+            per_obj_storage.storage_rebate.to_string(),
+            per_obj_storage.storage_cost.to_string(),
+        ]);
+    }
+    let mut table = builder.build();
+
+    table.with(TableStyle::rounded().horizontals([HorizontalLine::new(
+        1,
+        TableStyle::modern().get_horizontal(),
+    )]));
+    write!(f, "\n{}\n", table)
 }
 
 fn display_info(f: &mut Formatter<'_>, haneul_gas_status: &GasStatusV2) -> std::fmt::Result {
-    write!(
-        f,
-        "\nReference Gas Price: {}\n",
+    let mut builder = TableBuilder::default();
+    builder.push_record(vec!["Gas Info".to_string()]);
+    builder.push_record(vec![format!(
+        "Reference Gas Price: {}",
         haneul_gas_status.reference_gas_price()
-    )?;
-    write!(f, "Gas Price: {}\n", haneul_gas_status.gas_status.gas_price())?;
-    write!(
-        f,
-        "Max Gas Stack Height: {}\n",
+    )]);
+    builder.push_record(vec![format!(
+        "Gas Price: {}",
+        haneul_gas_status.gas_status.gas_price()
+    )]);
+
+    builder.push_record(vec![format!(
+        "Max Gas Stack Height: {}",
         haneul_gas_status.gas_status.stack_height_high_water_mark()
-    )?;
-    write!(
-        f,
-        "Max Gas Stack Size: {}\n",
+    )]);
+
+    builder.push_record(vec![format!(
+        "Max Gas Stack Size: {}",
         haneul_gas_status.gas_status.stack_size_high_water_mark()
-    )?;
-    write!(
-        f,
+    )]);
+
+    builder.push_record(vec![format!(
         "Number of Bytecode Instructions Executed: {}",
         haneul_gas_status.gas_status.instructions_executed()
-    )
+    )]);
+
+    let mut table = builder.build();
+    table.with(TableStyle::rounded());
+
+    write!(f, "\n{}\n", table)
 }
