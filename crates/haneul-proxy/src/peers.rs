1b9035c9aea21e5ea77cf9125535b37247a41b88
GeunhwaJeong <jeonggh892@gmail.com>
2024-02-14 11:49:13 -0600

    [haneul-proxy/static keys] allow intake of static pub keys
    ## Description 
    
    we may want to provide static pub keys to accept when receiving metrics
    from haneul-node; this pr allows us to do that.
    this is also useful if we need to allow any general node to send us data
    and not be in the consensus committee.
    
    there is a breaking change for haneul-proxy, the config format have changed
    to support the idea of dynamic and static keys, but this is minor and
    will be adjusted along with the static key config.
    
    ## Test Plan 
    local testing
    
    ---
    If your changes are not user-facing and do not break anything, you can
    skip the following section. Otherwise, please briefly describe what has
    changed under the Release Notes section.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-proxy/src/peers.rs
+++ crates/haneul-proxy/src/peers.rs
@@ -56,21 +56,30 @@ pub struct HaneulPeer {
 #[derive(Debug, Clone)]
 pub struct HaneulNodeProvider {
     nodes: HaneulPeers,
+    static_nodes: HaneulPeers,
     rpc_url: String,
     rpc_poll_interval: Duration,
 }
 
 impl Allower for HaneulNodeProvider {
     fn allowed(&self, key: &Ed25519PublicKey) -> bool {
-        self.nodes.read().unwrap().contains_key(key)
+        self.static_nodes.read().unwrap().contains_key(key)
+            || self.nodes.read().unwrap().contains_key(key)
     }
 }
 
 impl HaneulNodeProvider {
-    pub fn new(rpc_url: String, rpc_poll_interval: Duration) -> Self {
+    pub fn new(rpc_url: String, rpc_poll_interval: Duration, static_peers: Vec<HaneulPeer>) -> Self {
+        // build our hashmap with the static pub keys. we only do this one time at binary startup.
+        let static_nodes: HashMap<Ed25519PublicKey, HaneulPeer> = static_peers
+            .into_iter()
+            .map(|v| (v.public_key.clone(), v))
+            .collect();
+        let static_nodes = Arc::new(RwLock::new(static_nodes));
         let nodes = Arc::new(RwLock::new(HashMap::new()));
         Self {
             nodes,
+            static_nodes,
             rpc_url,
             rpc_poll_interval,
         }
@@ -79,6 +88,15 @@ impl HaneulNodeProvider {
     /// get is used to retrieve peer info in our handlers
     pub fn get(&self, key: &Ed25519PublicKey) -> Option<HaneulPeer> {
         debug!("look for {:?}", key);
+        // check static nodes first
+        if let Some(v) = self.static_nodes.read().unwrap().get(key) {
+            return Some(HaneulPeer {
+                name: v.name.to_owned(),
+                p2p_address: v.p2p_address.to_owned(),
+                public_key: v.public_key.to_owned(),
+            });
+        }
+        // check dynamic nodes
         if let Some(v) = self.nodes.read().unwrap().get(key) {
             return Some(HaneulPeer {
                 name: v.name.to_owned(),
