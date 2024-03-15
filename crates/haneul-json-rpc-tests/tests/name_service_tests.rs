2da3b2990e8fbf9554f6f0f86e926eefba3ad385
GeunhwaJeong <jeonggh892@gmail.com>
2024-03-15 17:05:21 +0200

    [NameService] Adds support to more input formats
    ## Description 
    
    **Updated the description (16-01-2024) due to a change in our support
    (we won't omit TLDs and `@` is a special format after all, not another
    wildcard)**
    
    With this PR, we add support for wildcard `.` or `*` as the separator
    for a valid domain, and also support the new HaneulNS display format, based
    on `@`.
    
    Some examples:
    `test` -> `test.haneul`
    `sub@test` -> `sub.test.haneul`
    `inner.sub@test` -> `inner.sub.test.haneul`
    `more.inner.sub@test` -> `more.inner.sub.test.haneul`
    
    All of the above would work with `*` instead of `.` too.
    
    ## Test Plan 
    
    Added a new test file (We didn't have one for the name service). Not
    sure if this is the right way to test stuff on our crates, so please
    advise me here if it's not right.
    
    ---
    If your changes are not user-facing and not a breaking change, you can
    skip the following section. Otherwise, please indicate what changed, and
    then add to the Release Notes section as highlighted during the release
    process.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-json-rpc-tests/tests/name_service_tests.rs
+++ crates/haneul-json-rpc-tests/tests/name_service_tests.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use std::str::FromStr;
-use haneul_json_rpc::name_service;
+use haneul_json_rpc::name_service::{self, Domain};
 use haneul_types::{
     base_types::{ObjectID, HaneulAddress},
     collection_types::VecMap,
@@ -10,11 +10,11 @@ use haneul_types::{
 
 #[test]
 fn test_parent_extraction() {
-    let mut name = name_service::Domain::from_str("leaf.node.test.haneul").unwrap();
+    let mut name = Domain::from_str("leaf.node.test.haneul").unwrap();
 
     assert_eq!(name.parent().to_string(), "node.test.haneul");
 
-    name = name_service::Domain::from_str("node.test.haneul").unwrap();
+    name = Domain::from_str("node.test.haneul").unwrap();
 
     assert_eq!(name.parent().to_string(), "test.haneul");
 }
@@ -35,4 +35,60 @@ fn test_expirations() {
     name.expiration_timestamp_ms = system_time - 10;
 
     assert!(name.is_node_expired(system_time));
+}
+
+#[test]
+fn test_name_service_outputs() {
+    assert_eq!("@test".parse::<Domain>().unwrap().to_string(), "test.haneul");
+    assert_eq!(
+        "test.haneul".parse::<Domain>().unwrap().to_string(),
+        "test.haneul"
+    );
+    assert_eq!(
+        "test@sld".parse::<Domain>().unwrap().to_string(),
+        "test.sld.haneul"
+    );
+    assert_eq!(
+        "test.test@example".parse::<Domain>().unwrap().to_string(),
+        "test.test.example.haneul"
+    );
+    assert_eq!(
+        "haneul@haneul".parse::<Domain>().unwrap().to_string(),
+        "haneul.haneul.haneul"
+    );
+
+    assert_eq!("@haneul".parse::<Domain>().unwrap().to_string(), "haneul.haneul");
+
+    assert_eq!(
+        "test*test@test".parse::<Domain>().unwrap().to_string(),
+        "test.test.test.haneul"
+    );
+    assert_eq!(
+        "test.test.haneul".parse::<Domain>().unwrap().to_string(),
+        "test.test.haneul"
+    );
+    assert_eq!(
+        "test.test.test.haneul".parse::<Domain>().unwrap().to_string(),
+        "test.test.test.haneul"
+    );
+}
+
+#[test]
+fn test_different_wildcard() {
+    assert_eq!("test.haneul".parse::<Domain>(), "test*haneul".parse::<Domain>(),);
+
+    assert_eq!("@test".parse::<Domain>(), "test*haneul".parse::<Domain>(),);
+}
+
+#[test]
+fn test_invalid_inputs() {
+    assert!("*".parse::<Domain>().is_err());
+    assert!(".".parse::<Domain>().is_err());
+    assert!("@".parse::<Domain>().is_err());
+    assert!("@inner.haneul".parse::<Domain>().is_err());
+    assert!("@inner*haneul".parse::<Domain>().is_err());
+    assert!("test@".parse::<Domain>().is_err());
+    assert!("haneul".parse::<Domain>().is_err());
+    assert!("test.test@example.haneul".parse::<Domain>().is_err());
+    assert!("test@test@example".parse::<Domain>().is_err());
 }
