7e994b9ea1b7ae2bf0525da7b1f5f9bba73a4785
GeunhwaJeong <jeonggh892@gmail.com>
2024-03-19 19:24:30 +0000

    [GraphQL/E2E] Fix protocol version to 39
    ## Description 
    
    Make sure all existing E2E tests used a fixed protocol version (I picked
    39), so that when there are changes to a future protocol version, they
    won't cause all the digests/addresses etc in these tests to change.
    
    Also updated the `update_all_snapshots` script to not update these
    snapshots (should no longer be required) and therefore removed the
    dependency on `psql`.
    
    Added a section to the E2E test README to remind people to set a
    protocol version. I opted for an approach where we set a protocol
    version for each test rather than setting it once for all tests because:
    
    - The logic that controls which protocol version to use is embedded
    within the haneul-specific init logic, but the entrypoint we use in the
    test runner is from the Move transactional test codebase (which is not
    aware of protocol versions, etc).
    - We may want to set different protocol versions for different tests, in
    general, and so setting a blanket override or a hidden different default
    (other than the latest protocol version) seems liable to confuse people.
    
    ## Test Plan 
    
    Created a change to the framework (and a protocol version bump) on top
    of this change and ran the tests -- they still passed, with no need to
    change snapshots.

--- scripts/update_all_snapshots.sh
+++ scripts/update_all_snapshots.sh
@@ -7,11 +7,6 @@
 set -x
 set -e
 
-if ! command -v psql > /dev/null; then
-  echo "Error: PostgreSQL is not installed. Please follow crates/haneul-graphql-e2e-tests/README.md" >&2
-  exit 1
-fi
-
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 ROOT="$SCRIPT_DIR/.."
@@ -19,4 +14,3 @@ ROOT="$SCRIPT_DIR/.."
 cd "$ROOT/crates/haneul-protocol-config" && cargo insta test --review
 cd "$ROOT/crates/haneul-swarm-config" && cargo insta test --review
 cd "$ROOT/crates/haneul-open-rpc" && cargo run --example generate-json-rpc-spec -- record
-cd "$ROOT/crates/haneul-graphql-e2e-tests" && env UB=1 cargo nextest run --features pg_integration
