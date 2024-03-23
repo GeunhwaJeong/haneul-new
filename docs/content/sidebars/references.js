16256de76e2df0d0db6425492d5838af355b14e9
GeunhwaJeong <jeonggh892@gmail.com>
2024-03-22 17:20:51 -0700

    [docs] add go and python sdk links
    ## Description 
    
    adding links to go and python sdks
    
    ## Test Plan 
    
    visual inspection + manual test
    
    ---
    If your changes are not user-facing and do not break anything, you can
    skip the following section. Otherwise, please briefly describe what has
    changed under the Release Notes section.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- docs/content/sidebars/references.js
+++ docs/content/sidebars/references.js
@@ -71,15 +71,25 @@ const references = [
 		items: [
 			{
 				type: 'link',
-				label: 'Haneul TypeScript SDK',
-				href: 'https://sdk.haneul-labs.com/typescript',
-			},
-			{
-				type: 'link',
 				label: 'dApp Kit',
 				href: 'https://sdk.haneul-labs.com/dapp-kit',
 			},
+      {
+				type: 'link',
+				label: 'Haneul Go SDK',
+				href: 'https://github.com/block-vision/haneul-go-sdk',
+      },
+      {
+				type: 'link',
+				label: 'Haneul Python SDK',
+				href: 'https://github.com/FrankC01/pyhaneul',
+      },
 			'references/rust-sdk',
+			{
+				type: 'link',
+				label: 'Haneul TypeScript SDK',
+				href: 'https://sdk.haneul-labs.com/typescript',
+			},
 		],
 	},
 	{
