e3b28729f9c6a34b0ecf5106ba940a40362be4c8
GeunhwaJeong <jeonggh892@gmail.com>
2024-03-24 22:20:55 +0200

    Surfer: add a Move module for testing random
    ## Description 
    
    Surfer: add a Move module for testing random
    
    ## Test Plan 
    
    Manually with 
    `cargo run --release --bin haneul-surfer -- --packages
    crates/haneul-surfer/tests/random --run-duration 100 --epoch-duration 20`
    
    ---
    If your changes are not user-facing and do not break anything, you can
    skip the following section. Otherwise, please briefly describe what has
    changed under the Release Notes section.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- crates/haneul-surfer/tests/smoke_tests.rs
+++ crates/haneul-surfer/tests/smoke_tests.rs
@@ -9,10 +9,14 @@ use haneul_surfer::default_surf_strategy::DefaultSurfStrategy;
 #[sim_test]
 async fn smoke_test() {
     // This test makes sure that the haneul surfer runs.
-
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     path.extend(["tests", "move_building_blocks"]);
-    let results = haneul_surfer::run::<DefaultSurfStrategy>(Duration::from_secs(30), vec![path]).await;
+    let results = haneul_surfer::run::<DefaultSurfStrategy>(
+        Duration::from_secs(30),
+        Duration::from_secs(15),
+        vec![path],
+    )
+    .await;
     assert!(results.num_successful_transactions > 0);
     assert!(!results.unique_move_functions_called.is_empty());
 }
