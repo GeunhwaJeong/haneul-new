cca956727a5c641c64c936395e83c129e5454328
GeunhwaJeong <jeonggh892@gmail.com>
2024-04-04 10:43:43 -0700

    Remove haneul explorer
    ## Description 
    
    I'm proposing removing this from the Haneul repo, and moving this over here
    so that we don't have to keep this up-to-date, and we can clean up the
    repo a bit: https://github.com/GeunhwaJeong/haneul-explorer
    
    ---
    If your changes are not user-facing and do not break anything, you can
    skip the following section. Otherwise, please briefly describe what has
    changed under the Release Notes section.
    
    ### Type of Change (Check all that apply)
    
    - [ ] protocol change
    - [ ] user-visible impact
    - [ ] breaking change for a client SDKs
    - [ ] breaking change for FNs (FN binary must upgrade)
    - [ ] breaking change for validators or node operators (must upgrade
    binaries)
    - [ ] breaking change for on-chain data layout
    - [ ] necessitate either a data wipe or data migration
    
    ### Release notes

--- apps/ui/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-
-import preset from '@haneullabs/core/tailwind.config';
-import { type Config } from 'tailwindcss';
-
-export default {
-	presets: [preset],
-} satisfies Partial<Config>;
