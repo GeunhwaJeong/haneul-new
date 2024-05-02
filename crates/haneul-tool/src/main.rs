32b81198b62bd67da6e04b7e8b6a1405a015bce2
GeunhwaJeong <jeonggh892@gmail.com>
2024-05-02 12:22:28 -0500

    haneul-tool: use bin-version

--- crates/haneul-tool/src/main.rs
+++ crates/haneul-tool/src/main.rs
@@ -1,21 +1,36 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-extern crate core;
 
 use clap::*;
 use colored::Colorize;
 use haneul_tool::commands::ToolCommand;
 use haneul_types::exit_main;
 
+// Define the `GIT_REVISION` and `VERSION` consts
+bin_version::bin_version!();
+
+#[derive(Parser)]
+#[command(
+    name = "haneul-tool",
+    about = "Debugging utilities for haneul",
+    rename_all = "kebab-case",
+    author,
+    version = VERSION,
+)]
+struct App {
+    #[command(subcommand)]
+    command: ToolCommand,
+}
+
 #[tokio::main]
 async fn main() {
     #[cfg(windows)]
     colored::control::set_virtual_terminal(true).unwrap();
 
-    let cmd: ToolCommand = ToolCommand::parse();
+    let app = App::parse();
     let (_guards, handle) = telemetry_subscribers::TelemetryConfig::new()
         .with_env()
         .init();
 
-    exit_main!(cmd.execute(handle).await);
+    exit_main!(app.command.execute(handle).await);
 }
