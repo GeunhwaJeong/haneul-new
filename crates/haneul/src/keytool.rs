3680250bc01e7de9869ca0f8d10ca9f60d7aee60
GeunhwaJeong <jeonggh892@gmail.com>
2024-05-29 01:32:53 +0800

    [CLI] fix command `haneul keytool import` using `--alias` is useless when inputting mnemonic phrase
    ## Description 
    
    [CLI] fix command `haneul keytool import` using `--alias` is useless when
    inputting mnemonic phrase
    
    <img width="1067" alt="2x"
    src="https://github.com/GeunhwaJeong/haneul/assets/123343203/b3357aa8-d7de-4a4b-885c-944d16c215d8">
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [x] CLI: Fixed a bug where the provided alias in the `haneul keytool
    import` command was not kept when importing a key via mnemonic.
    - [ ] Rust SDK:
    
    ---------

--- crates/haneul/src/keytool.rs
+++ crates/haneul/src/keytool.rs
@@ -639,6 +639,7 @@ impl KeyToolCommand {
                             &input_string,
                             key_scheme,
                             derivation_path,
+                            alias,
                         )?;
                         let skp = keystore.get_key(&haneul_address)?;
                         let key = Key::from(skp);
