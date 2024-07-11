62549f031f54f2c908f0c3feea76dd71c53646af
GeunhwaJeong <jeonggh892@gmail.com>
2024-07-11 12:36:50 +0100

    [Examples/Front-end] Tic-tac-toe
    ## Description
    
    Build front-ends for shared and multi-sig tic-tac-toe, using
    `create-react-dapp`.
    
    ## Test plan
    
    Manually tested (screenshots below).
    
    #### New Shared game
    ![shared
    game](https://github.com/GeunhwaJeong/haneul/assets/332275/a7b5adcf-38b9-40b6-a01a-a0d0e1fe1836)
    
    #### New MultiSig game
    ![multi-sig
    game](https://github.com/GeunhwaJeong/haneul/assets/332275/6682c0e8-973a-408d-9696-1cc6fafcccbd)
    
    #### Game in-progress
    ![game
    in-progress](https://github.com/GeunhwaJeong/haneul/assets/332275/ee102324-ce07-4855-a52d-56376a812e0f)
    
    #### Game won, viewed by player
    ![game
    win](https://github.com/GeunhwaJeong/haneul/assets/332275/e1ede3f8-fe3c-4d3a-a189-6eacdb6967ae)
    
    #### Game won, viewed by spectator (no wallet connected)
    ![game win
    spectator](https://github.com/GeunhwaJeong/haneul/assets/332275/ae6f9eba-bb0b-4223-b08d-9435cce2938d)
    
    #### Delete game dialog
    ![delete
    game](https://github.com/GeunhwaJeong/haneul/assets/332275/29169cd7-544d-4591-ac68-6ddda6bb7ef7)
    
    ## Stack
    
    -

---
+++ examples/tic-tac-toe/ui/src/hooks/useObjectQuery.ts
@@ -0,0 +1,29 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { useHaneulClientContext, useHaneulClientQuery, UseHaneulClientQueryOptions } from '@haneullabs/dapp-kit';
+import { GetObjectParams, HaneulObjectResponse } from '@haneullabs/haneul/client';
+import { useQueryClient, UseQueryResult } from '@tanstack/react-query';
+
+export type UseObjectQueryOptions = UseHaneulClientQueryOptions<'getObject', HaneulObjectResponse>;
+export type UseObjectQueryResponse = UseQueryResult<HaneulObjectResponse, Error>;
+export type InvalidateUseObjectQuery = () => void;
+
+/**
+ * Fetches an object, returning the response from RPC and a callback
+ * to invalidate it.
+ */
+export function useObjectQuery(
+	params: GetObjectParams,
+	options?: UseObjectQueryOptions,
+): [UseObjectQueryResponse, InvalidateUseObjectQuery] {
+	const ctx = useHaneulClientContext();
+	const client = useQueryClient();
+	const response = useHaneulClientQuery('getObject', params, options);
+
+	const invalidate = async () => {
+		await client.invalidateQueries({ queryKey: [ctx.network, 'getObject', params] });
+	};
+
+	return [response, invalidate];
+}
---
+++ examples/tic-tac-toe/ui/src/main.tsx
@@ -0,0 +1,35 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import '@haneullabs/dapp-kit/dist/index.css';
+import '@radix-ui/themes/styles.css';
+
+import { HaneulClientProvider, WalletProvider } from '@haneullabs/dapp-kit';
+import { Theme } from '@radix-ui/themes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
+
+import App from './App.tsx';
+import { networkConfig } from './config';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<Theme appearance="light">
+			<QueryClientProvider client={queryClient}>
+				<HaneulClientProvider networks={networkConfig} defaultNetwork="testnet">
+					<WalletProvider autoConnect>
+						<>
+							<Toaster position="top-center" />
+							<App />
+						</>
+					</WalletProvider>
+				</HaneulClientProvider>
+			</QueryClientProvider>
+		</Theme>
+	</React.StrictMode>,
+);
