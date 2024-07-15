5c8a58dd56da840641549db43d6d683e47d9b1f8
GeunhwaJeong <jeonggh892@gmail.com>
2024-07-15 07:47:59 -0700

    [AuthAgg] Use epoch start state to construct AuthorityAggregator
    ## Description 
    
    This PR makes two changes:
    1. The primary change is to be able to construct AuthorityAggregator
    from epoch start state, instead of reading system state from the store.
    This is much safer and sync way to do it in prod.
    2. The secondary change is to extend AuthorityAggregatorBuilder to be
    able to build more aggregators, to simplify some of the code.
    
    ## Test plan 
    
    CI
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/haneul-benchmark/src/lib.rs
+++ crates/haneul-benchmark/src/lib.rs
@@ -17,9 +17,7 @@ use std::{
 use haneul_config::genesis::Genesis;
 use haneul_core::{
     authority_aggregator::{AuthorityAggregator, AuthorityAggregatorBuilder},
-    authority_client::{
-        make_authority_clients_with_timeout_config, AuthorityAPI, NetworkAuthorityClient,
-    },
+    authority_client::{AuthorityAPI, NetworkAuthorityClient},
     quorum_driver::{
         QuorumDriver, QuorumDriverHandler, QuorumDriverHandlerBuilder, QuorumDriverMetrics,
     },
@@ -28,7 +26,6 @@ use haneul_json_rpc_types::{
     HaneulObjectDataOptions, HaneulObjectResponse, HaneulObjectResponseQuery, HaneulTransactionBlockEffects,
     HaneulTransactionBlockEffectsAPI, HaneulTransactionBlockResponseOptions,
 };
-use haneul_network::{DEFAULT_CONNECT_TIMEOUT_SEC, DEFAULT_REQUEST_TIMEOUT_SEC};
 use haneul_sdk::{HaneulClient, HaneulClientBuilder};
 use haneul_types::base_types::ConciseableName;
 use haneul_types::committee::CommitteeTrait;
@@ -256,24 +253,17 @@ impl LocalValidatorAggregatorProxy {
         registry: &Registry,
         reconfig_fullnode_rpc_url: Option<&str>,
     ) -> Self {
-        let (aggregator, _) = AuthorityAggregatorBuilder::from_genesis(genesis)
+        let (aggregator, clients) = AuthorityAggregatorBuilder::from_genesis(genesis)
             .with_registry(registry)
-            .build()
-            .unwrap();
-
-        let committee = genesis.committee_with_network();
-        let clients = make_authority_clients_with_timeout_config(
-            &committee,
-            DEFAULT_CONNECT_TIMEOUT_SEC,
-            DEFAULT_REQUEST_TIMEOUT_SEC,
-        );
+            .build_network_clients();
+        let committee = genesis.committee().unwrap();
 
         Self::new_impl(
             aggregator,
             registry,
             reconfig_fullnode_rpc_url,
             clients,
-            committee.committee().clone(),
+            committee,
         )
         .await
     }
