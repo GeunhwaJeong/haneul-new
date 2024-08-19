d3976492d5e2fa5fb76348fe41741f6d29bb557c
GeunhwaJeong <jeonggh892@gmail.com>
2024-08-19 11:16:43 -0700

    [sdk e2e] fix some more flaky e2e tests
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- sdk/typescript/test/e2e/txn-builder.test.ts
+++ sdk/typescript/test/e2e/txn-builder.test.ts
@@ -220,6 +220,7 @@ async function validateTransaction(client: HaneulClient, signer: Keypair, tx: Tr
 			showEffects: true,
 		},
 	});
+	await client.waitForTransaction({ digest: result.digest });
 	expect(localDigest).toEqual(result.digest);
 	expect(result.effects?.status.status).toEqual('success');
 }
