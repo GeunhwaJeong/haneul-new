f8ce3f469fc68366960d2fa02588505aea2a6870
GeunhwaJeong <jeonggh892@gmail.com>
2024-08-19 09:35:30 -0500

    rest: introduce transaction simulation endpoint

--- crates/haneul-rest-api/src/client/sdk.rs
+++ crates/haneul-rest-api/src/client/sdk.rs
@@ -14,6 +14,7 @@ use haneul_sdk_types::types::ObjectId;
 use haneul_sdk_types::types::SignedCheckpointSummary;
 use haneul_sdk_types::types::SignedTransaction;
 use haneul_sdk_types::types::StructTag;
+use haneul_sdk_types::types::Transaction;
 use haneul_sdk_types::types::TransactionDigest;
 use haneul_sdk_types::types::ValidatorCommittee;
 use haneul_sdk_types::types::Version;
@@ -36,6 +37,7 @@ use crate::system::X_HANEUL_MIN_SUPPORTED_PROTOCOL_VERSION;
 use crate::transactions::ListTransactionsQueryParameters;
 use crate::transactions::TransactionExecutionResponse;
 use crate::transactions::TransactionResponse;
+use crate::transactions::TransactionSimulationResponse;
 use crate::types::X_HANEUL_CHAIN;
 use crate::types::X_HANEUL_CHAIN_ID;
 use crate::types::X_HANEUL_CHECKPOINT_HEIGHT;
@@ -397,6 +399,26 @@ impl Client {
             .inner
             .post(url)
             .query(parameters)
+            .header(reqwest::header::ACCEPT, crate::APPLICATION_BCS)
+            .header(reqwest::header::CONTENT_TYPE, crate::APPLICATION_BCS)
+            .body(body)
+            .send()
+            .await?;
+
+        self.bcs(response).await
+    }
+
+    pub async fn simulate_transaction(
+        &self,
+        transaction: &Transaction,
+    ) -> Result<Response<TransactionSimulationResponse>> {
+        let url = self.url().join("transactions/simulate")?;
+
+        let body = bcs::to_bytes(transaction)?;
+
+        let response = self
+            .inner
+            .post(url)
             .header(reqwest::header::ACCEPT, crate::APPLICATION_BCS)
             .header(reqwest::header::CONTENT_TYPE, crate::APPLICATION_BCS)
             .body(body)
