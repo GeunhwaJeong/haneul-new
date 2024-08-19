cf789f18c5208e858e5c1a20f38388ee057f2356
GeunhwaJeong <jeonggh892@gmail.com>
2024-08-19 11:53:22 +0100

    [haneul-tool] dump-packages uses GraphQL
    ## Description
    
    Replace the original implementation of the dump-packages command (which
    requires access to an indexer database) with an implementation that
    reads from a GraphQL service. The former is not readily accessible, but
    the latter should be.
    
    The new tool is also able to run incrementally: Fetching only packages
    created before a certain checkpoint, or pick up where a previous
    invocation took off to fetch new packages that were introduced since.
    
    ## Test plan
    
    Ran a test invocation, on our experimental read replica. With a max page
    size of 200, I was able to fetch 17000 packages (all the packages at the
    time the read replica was created) in 3 minutes.
    
    ## Stack
    
    - 
    -  
    -  
    -  
    -  
    -  
    - 
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

---
+++ crates/haneul-package-dump/build.rs
@@ -0,0 +1,10 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+fn main() {
+    cynic_codegen::register_schema("haneul")
+        .from_sdl_file("../haneul-graphql-rpc/schema.graphql")
+        .unwrap()
+        .as_default()
+        .unwrap();
+}
