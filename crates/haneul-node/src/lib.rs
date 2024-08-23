2c0620296b196fcd82877902fea3215cce96534c
GeunhwaJeong <jeonggh892@gmail.com>
2024-08-23 01:21:05 +0100

    Add ServerTiming headers to fullnode responses
    - Add facility for adding ServerTiming measurements from arbitrary
    points in the code.
    - Instrument wait_for_finality and local execution

--- crates/haneul-node/src/lib.rs
+++ crates/haneul-node/src/lib.rs
@@ -55,6 +55,7 @@ use fastcrypto_zkp::bn254::zk_login::JWK;
 pub use handle::HaneulNodeHandle;
 use haneullabs_metrics::{spawn_monitored_task, RegistryService};
 use haneullabs_network::server::ServerBuilder;
+use haneullabs_service::server_timing::server_timing_middleware;
 use narwhal_network::metrics::MetricsMakeCallbackHandler;
 use narwhal_network::metrics::{NetworkConnectionMetrics, NetworkMetrics};
 use haneul_archival::reader::ArchiveReaderBalancer;
@@ -2048,6 +2049,8 @@ pub async fn build_http_server(
         .await
         .unwrap();
     let addr = listener.local_addr().unwrap();
+
+    router = router.layer(axum::middleware::from_fn(server_timing_middleware));
 
     let handle = tokio::spawn(async move {
         axum::serve(
