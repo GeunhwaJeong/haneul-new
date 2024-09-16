49e1aeba5ab5538ec4be2f437a29681439d02a2b
GeunhwaJeong <jeonggh892@gmail.com>
2024-09-16 13:40:24 -0400

    indexer json rpc: batch object db reades
    ## Description 
    
    the original impl. fires a DB query for each object, which is wasteful
    
    ## Test plan 
    
    ci
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/haneul-indexer/src/apis/read_api.rs
+++ crates/haneul-indexer/src/apis/read_api.rs
@@ -60,40 +60,8 @@ impl ReadApiServer for ReadApi {
         object_id: ObjectID,
         options: Option<HaneulObjectDataOptions>,
     ) -> RpcResult<HaneulObjectResponse> {
-        let options = options.unwrap_or_default();
         let object_read = self.inner.get_object_read(object_id).await?;
-
-        match object_read {
-            ObjectRead::NotExists(id) => Ok(HaneulObjectResponse::new_with_error(
-                HaneulObjectResponseError::NotExists { object_id: id },
-            )),
-            ObjectRead::Exists(object_ref, o, layout) => {
-                let mut display_fields = None;
-                if options.show_display {
-                    match self.inner.get_display_fields(&o, &layout).await {
-                        Ok(rendered_fields) => display_fields = Some(rendered_fields),
-                        Err(e) => {
-                            return Ok(HaneulObjectResponse::new(
-                                Some((object_ref, o, layout, options, None).try_into()?),
-                                Some(HaneulObjectResponseError::DisplayError {
-                                    error: e.to_string(),
-                                }),
-                            ));
-                        }
-                    }
-                }
-                Ok(HaneulObjectResponse::new_with_data(
-                    (object_ref, o, layout, options, display_fields).try_into()?,
-                ))
-            }
-            ObjectRead::Deleted((object_id, version, digest)) => Ok(
-                HaneulObjectResponse::new_with_error(HaneulObjectResponseError::Deleted {
-                    object_id,
-                    version,
-                    digest,
-                }),
-            ),
-        }
+        object_read_to_object_response(&self.inner, object_read, options.unwrap_or_default()).await
     }
 
     // For ease of implementation we just forward to the single object query, although in the
@@ -109,16 +77,17 @@ impl ReadApiServer for ReadApi {
                 HaneulRpcInputError::SizeLimitExceeded(QUERY_MAX_RESULT_LIMIT.to_string()).into(),
             );
         }
+        let stored_objects = self.inner.multi_get_objects(object_ids).await?;
+        let options = options.unwrap_or_default();
 
-        let mut futures = vec![];
-        for object_id in object_ids {
-            futures.push(self.get_object(object_id, options.clone()));
-        }
+        let futures = stored_objects.into_iter().map(|stored_object| async {
+            let object_read = stored_object
+                .try_into_object_read(self.inner.package_resolver())
+                .await?;
+            object_read_to_object_response(&self.inner, object_read, options.clone()).await
+        });
 
-        futures::future::join_all(futures)
-            .await
-            .into_iter()
-            .collect::<Result<Vec<_>, _>>()
+        futures::future::try_join_all(futures).await
     }
 
     async fn get_total_transaction_blocks(&self) -> RpcResult<BigInt<u64>> {
@@ -290,5 +259,43 @@ impl HaneulRpcModule for ReadApi {
 
     fn rpc_doc_module() -> Module {
         haneul_json_rpc_api::ReadApiOpenRpc::module_doc()
+    }
+}
+
+async fn object_read_to_object_response(
+    indexer_reader: &IndexerReader,
+    object_read: ObjectRead,
+    options: HaneulObjectDataOptions,
+) -> RpcResult<HaneulObjectResponse> {
+    match object_read {
+        ObjectRead::NotExists(id) => Ok(HaneulObjectResponse::new_with_error(
+            HaneulObjectResponseError::NotExists { object_id: id },
+        )),
+        ObjectRead::Exists(object_ref, o, layout) => {
+            let mut display_fields = None;
+            if options.show_display {
+                match indexer_reader.get_display_fields(&o, &layout).await {
+                    Ok(rendered_fields) => display_fields = Some(rendered_fields),
+                    Err(e) => {
+                        return Ok(HaneulObjectResponse::new(
+                            Some((object_ref, o, layout, options, None).try_into()?),
+                            Some(HaneulObjectResponseError::DisplayError {
+                                error: e.to_string(),
+                            }),
+                        ));
+                    }
+                }
+            }
+            Ok(HaneulObjectResponse::new_with_data(
+                (object_ref, o, layout, options, display_fields).try_into()?,
+            ))
+        }
+        ObjectRead::Deleted((object_id, version, digest)) => Ok(HaneulObjectResponse::new_with_error(
+            HaneulObjectResponseError::Deleted {
+                object_id,
+                version,
+                digest,
+            },
+        )),
     }
 }
