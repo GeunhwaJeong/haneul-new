5c438f45bb163078f91ce065ae8a413ce5f2b101
GeunhwaJeong <jeonggh892@gmail.com>
2024-10-02 11:01:39 -0600

    [docs] Event updates
    ## Description 
    
    Updates guides/haneul-101/using-events to remove subscriptions
    Removes concepts/event because it was always a topic for enhancement and
    now the Move Book is a better resource for the info.
    Updates links to events.
    Adds a redirect from concepts/events to guides/events, which contains a
    link to the topic in the move book.
    Adds a deprecation note to the coin flip example for subscriptions.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- docs/site/src/components/BetaTag/index.tsx
+++ docs/site/src/components/BetaTag/index.tsx
@@ -12,17 +12,17 @@ export default function BetaTag(props) {
   }
 
   const beta = props.beta.toLowerCase();
-
+  // If `props.slim` is included, do not add spacing because the box is inline with
+  // content as opposed to at top of topic based on `beta` frontmatter.
   return (
     <Admonition
       title="Beta Feature"
       icon="⚙️"
-      className="!my-12 bg-haneul-blue-light border-haneul-blue-dark dark:bg-haneul-blue-dark dark:border-haneul-blue-light"
+      className={`${props.slim ? "" : "!my-12"} bg-haneul-blue-light border-haneul-blue-dark dark:bg-haneul-blue-dark dark:border-haneul-blue-light`}
     >
       <p className="pt-2">
-        The content in this topic describes a beta feature or service. Beta
-        features and services are in active development, so details are likely
-        to change.
+        This content describes a beta feature or service. Beta features and
+        services are in active development, so details are likely to change.
       </p>
       {(beta.includes("testnet") ||
         beta.includes("devnet") ||
