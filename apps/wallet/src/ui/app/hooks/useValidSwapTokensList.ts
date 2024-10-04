12b971778693d86546d17efbf928349cff300116
GeunhwaJeong <jeonggh892@gmail.com>
2024-10-04 15:22:37 -0700

    swap in extension
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

---
+++ apps/wallet/src/ui/app/hooks/useValidSwapTokensList.ts
@@ -0,0 +1,68 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+import { useActiveAddress } from '_app/hooks/useActiveAddress';
+import { useGetAllBalances } from '_app/hooks/useGetAllBalances';
+import { useRecognizedPackages } from '_app/hooks/useRecognizedPackages';
+import { useSupportedCoins } from '_app/hooks/useSupportedCoins';
+import { type CoinBalance } from '@haneullabs/haneul/client';
+import {
+	normalizeStructTag,
+	normalizeHaneulObjectId,
+	parseStructTag,
+	HANEUL_TYPE_ARG,
+} from '@haneullabs/haneul/utils';
+import { useMemo } from 'react';
+
+export function filterTokenBalances(tokens: CoinBalance[]) {
+	return tokens.filter(
+		(token) => Number(token.totalBalance) > 0 || token.coinType === HANEUL_TYPE_ARG,
+	);
+}
+
+export function useValidSwapTokensList() {
+	const address = useActiveAddress();
+	const { data, isLoading: isSupportedCoinsLoading } = useSupportedCoins();
+	const { data: rawCoinBalances, isLoading: isGetAllBalancesLoading } = useGetAllBalances(
+		address || '',
+	);
+	const packages = useRecognizedPackages();
+	const normalizedPackages = useMemo(
+		() => packages.map((id) => normalizeHaneulObjectId(id)),
+		[packages],
+	);
+
+	const supported = useMemo(
+		() =>
+			data?.supported.filter((type) => normalizedPackages.includes(parseStructTag(type).address)),
+		[data, normalizedPackages],
+	);
+
+	const coinBalances = useMemo(
+		() => (rawCoinBalances ? filterTokenBalances(rawCoinBalances) : null),
+		[rawCoinBalances],
+	);
+
+	const validSwaps = useMemo(
+		() =>
+			supported?.sort((a, b) => {
+				const haneulType = normalizeStructTag(HANEUL_TYPE_ARG);
+				const balanceA = BigInt(
+					coinBalances?.find(
+						(balance) => normalizeStructTag(balance.coinType) === normalizeStructTag(a),
+					)?.totalBalance ?? 0,
+				);
+				const balanceB = BigInt(
+					coinBalances?.find(
+						(balance) => normalizeStructTag(balance.coinType) === normalizeStructTag(b),
+					)?.totalBalance ?? 0,
+				);
+				return a === haneulType ? -1 : b === haneulType ? 1 : Number(balanceB - balanceA);
+			}) ?? [],
+		[supported, coinBalances],
+	);
+
+	return {
+		isLoading: isSupportedCoinsLoading || isGetAllBalancesLoading,
+		data: validSwaps,
+	};
+}
