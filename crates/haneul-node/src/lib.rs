f5a8f683df9258b22dcc9cd7ed2c65e670a76171
GeunhwaJeong <jeonggh892@gmail.com>
2024-10-11 14:07:40 -0500

    jsonrpc: initialize new coin_index_2
    Initialize the new coin_index_2 index and clear the old coin_index
    column family.

--- crates/haneul-node/src/lib.rs
+++ crates/haneul-node/src/lib.rs
@@ -573,6 +573,7 @@ impl HaneulNode {
                     .protocol_config()
                     .max_move_identifier_len_as_option(),
                 config.remove_deprecated_tables,
+                &store,
             )))
         } else {
             None
