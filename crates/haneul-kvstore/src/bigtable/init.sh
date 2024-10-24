f9d384fd7abd94e20b349922bd2693cf3dc86123
GeunhwaJeong <jeonggh892@gmail.com>
2024-10-24 16:08:25 -0400

    [kv store] basic bigtable client
    ## Description 
    Initial implementation of a KV store on top of BigTable.
    notes:
    * includes instructions for setting up a local BigTable instance via the
    emulator and a basic script for data ingestion(later will be added as an
    option to main ingestion binary)
    * generated proto files are included in the repo, so users won't have to
    compile the definitions themselves or install the `protoc` dependency.
    We can add an optional build step in a follow-up PR
    
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

---
+++ crates/haneul-kvstore/src/bigtable/init.sh
@@ -0,0 +1,20 @@
+# Copyright (c) Mysten Labs, Inc.
+# SPDX-License-Identifier: Apache-2.0
+INSTANCE_ID=${1:-haneul}
+command=(
+  cbt
+  -instance
+  "$INSTANCE_ID"
+)
+if [[ -n $BIGTABLE_EMULATOR_HOST ]]; then
+  command+=(-project emulator)
+fi
+
+for table in objects transactions checkpoints; do
+  (
+    set -x
+    "${command[@]}" createtable $table
+    "${command[@]}" createfamily $table haneul
+    "${command[@]}" setgcpolicy $table haneul maxversions=1
+  )
+done
---
+++ crates/haneul-kvstore/src/lib.rs
@@ -0,0 +1,52 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+mod bigtable;
+use anyhow::Result;
+use async_trait::async_trait;
+pub use bigtable::client::BigTableClient;
+use haneul_types::crypto::AuthorityStrongQuorumSignInfo;
+use haneul_types::digests::TransactionDigest;
+use haneul_types::effects::{TransactionEffects, TransactionEvents};
+use haneul_types::full_checkpoint_content::CheckpointData;
+use haneul_types::messages_checkpoint::{
+    CheckpointContents, CheckpointSequenceNumber, CheckpointSummary,
+};
+use haneul_types::object::Object;
+use haneul_types::storage::ObjectKey;
+use haneul_types::transaction::Transaction;
+
+#[async_trait]
+pub trait KeyValueStoreReader {
+    async fn get_objects(&mut self, objects: &[ObjectKey]) -> Result<Vec<Object>>;
+    async fn get_transactions(
+        &mut self,
+        transactions: &[TransactionDigest],
+    ) -> Result<Vec<TransactionData>>;
+    async fn get_checkpoints(
+        &mut self,
+        sequence_numbers: &[CheckpointSequenceNumber],
+    ) -> Result<Vec<Checkpoint>>;
+}
+
+#[async_trait]
+pub trait KeyValueStoreWriter {
+    async fn save_objects(&mut self, objects: &[&Object]) -> Result<()>;
+    async fn save_transactions(&mut self, transactions: &[TransactionData]) -> Result<()>;
+    async fn save_checkpoint(&mut self, checkpoint: &CheckpointData) -> Result<()>;
+}
+
+#[derive(Clone, Debug)]
+pub struct Checkpoint {
+    pub summary: CheckpointSummary,
+    pub contents: CheckpointContents,
+    pub signatures: AuthorityStrongQuorumSignInfo,
+}
+
+#[derive(Clone, Debug)]
+pub struct TransactionData {
+    pub transaction: Transaction,
+    pub effects: TransactionEffects,
+    pub events: Option<TransactionEvents>,
+    pub checkpoint_number: CheckpointSequenceNumber,
+    pub timestamp: u64,
+}
