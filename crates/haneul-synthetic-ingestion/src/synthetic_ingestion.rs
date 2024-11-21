57b92ca5a7f97561a1942680f3538330865cf738
GeunhwaJeong <jeonggh892@gmail.com>
2024-11-21 12:43:00 -0800

    [indexer-alt] Add benchmark command
    ## Description 
    
    This PR adds a benchmark command to indexer-alt.
    It would start the indexer using the provided parameters, start and stop
    based on the local ingestion data.
    It then measures the time and TPS.
    A few structural changes to indexer-alt code:
    1. Moved lag and pruner options to a dedicated sequential pipeline
    config, since these params only make sense in a sequential pipeline
    setting.
    2. Moved all the indexer start logic to a dedicated function, so that I
    can reuse it in the benchmark.
    3. Added option to not bootstrap genesis if not needed.
    
    ## Test plan 
    
    Run locally
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] Indexer: 
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] REST API:

--- crates/haneul-synthetic-ingestion/src/synthetic_ingestion.rs
+++ crates/haneul-synthetic-ingestion/src/synthetic_ingestion.rs
@@ -2,10 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use simulacrum::Simulacrum;
+use std::collections::BTreeMap;
 use std::path::PathBuf;
+use haneul_storage::blob::Blob;
 use haneul_test_transaction_builder::TestTransactionBuilder;
 use haneul_types::crypto::get_account_key_pair;
 use haneul_types::effects::TransactionEffectsAPI;
+use haneul_types::full_checkpoint_content::CheckpointData;
 use haneul_types::gas_coin::GEUNHWA_PER_HANEUL;
 use haneul_types::utils::to_sender_signed_transaction;
 use tokio::fs;
@@ -82,6 +85,21 @@ pub async fn generate_ingestion(config: Config) {
         num_checkpoints,
         timer.elapsed()
     );
+}
+
+pub async fn read_ingestion_data(path: &PathBuf) -> anyhow::Result<BTreeMap<u64, CheckpointData>> {
+    let mut data = BTreeMap::new();
+    let mut dir = fs::read_dir(path).await?;
+    while let Some(entry) = dir.next_entry().await? {
+        let path = entry.path();
+        let bytes = fs::read(path).await?;
+        let checkpoint_data: CheckpointData = Blob::from_bytes(&bytes)?;
+        data.insert(
+            checkpoint_data.checkpoint_summary.sequence_number,
+            checkpoint_data,
+        );
+    }
+    Ok(data)
 }
 
 #[cfg(test)]
