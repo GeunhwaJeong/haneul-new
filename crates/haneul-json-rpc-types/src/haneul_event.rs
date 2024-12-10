bebb84d7912afb3c97b8c5a0c327ef58fd001fb6
GeunhwaJeong <jeonggh892@gmail.com>
2024-12-10 09:16:55 -0600

    types: support decoding from both base64 as well as base58

--- crates/haneul-json-rpc-types/src/haneul_event.rs
+++ crates/haneul-json-rpc-types/src/haneul_event.rs
@@ -51,7 +51,7 @@ pub struct HaneulEvent {
     pub type_: StructTag,
     /// Parsed json value of the event
     pub parsed_json: Value,
-    #[serde_as(as = "Base64")]
+    #[serde_as(as = "haneul_types::haneul_serde::Base64orBase58")]
     #[schemars(with = "Base64")]
     /// Base64 encoded bcs bytes of the move event
     pub bcs: Vec<u8>,
