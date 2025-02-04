ae31cf9bde0e980e819c25ab7067e5efc847e799
GeunhwaJeong <jeonggh892@gmail.com>
2025-02-04 15:22:13 -0600

    rpc: add GetCoinInfo grpc endpoint

---
+++ crates/haneul-e2e-tests/tests/rpc/coin_info.rs
@@ -0,0 +1,40 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use haneul_macros::sim_test;
+use haneul_rpc_api::proto::node::v2::node_service_client::NodeServiceClient;
+use haneul_rpc_api::proto::node::v2::GetCoinInfoRequest;
+use haneul_rpc_api::proto::node::v2::GetCoinInfoResponse;
+use haneul_sdk_types::TypeTag;
+use test_cluster::TestClusterBuilder;
+
+#[sim_test]
+async fn get_coin_info() {
+    let test_cluster = TestClusterBuilder::new().build().await;
+
+    let mut grpc_client = NodeServiceClient::connect(test_cluster.rpc_url().to_owned())
+        .await
+        .unwrap();
+
+    let coin_type_sdk: TypeTag = "0x2::haneul::HANEUL".parse().unwrap();
+    let request = GetCoinInfoRequest {
+        coin_type: Some(coin_type_sdk.clone().into()),
+    };
+
+    let GetCoinInfoResponse {
+        coin_type,
+        metadata,
+        treasury,
+    } = grpc_client
+        .get_coin_info(request)
+        .await
+        .unwrap()
+        .into_inner();
+
+    assert_eq!(coin_type, Some(coin_type_sdk.into()));
+    assert_eq!(metadata.unwrap().symbol, Some("HANEUL".to_owned()));
+    assert_eq!(
+        treasury.unwrap().total_supply,
+        Some(haneul_types::gas_coin::TOTAL_SUPPLY_GEUNHWA)
+    );
+}
