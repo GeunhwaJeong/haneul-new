e5e42a71543c0ced2b02907ee73a6df0670c0f75
GeunhwaJeong <jeonggh892@gmail.com>
2025-03-10 09:04:04 -0600

    [typed store] iterators: migrate unbounded_iter callsites
    ## Description 
    
    * migrates unbounded_iter call sites to safe_iter
    * removes unused range_iter method
    * deprecates iterator_cf method
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/haneul-core/src/stake_aggregator.rs
+++ crates/haneul-core/src/stake_aggregator.rs
@@ -11,9 +11,10 @@ use haneul_types::base_types::AuthorityName;
 use haneul_types::base_types::ConciseableName;
 use haneul_types::committee::{Committee, CommitteeTrait, StakeUnit};
 use haneul_types::crypto::{AuthorityQuorumSignInfo, AuthoritySignInfo, AuthoritySignInfoTrait};
-use haneul_types::error::HaneulError;
+use haneul_types::error::{HaneulError, HaneulResult};
 use haneul_types::message_envelope::{Envelope, Message};
 use tracing::warn;
+use typed_store::TypedStoreError;
 
 /// StakeAggregator allows us to keep track of the total stake of a set of validators.
 /// STRENGTH indicates whether we want a strong quorum (2f+1) or a weak quorum (f+1).
@@ -38,15 +39,16 @@ impl<S: Clone + Eq, const STRENGTH: bool> StakeAggregator<S, STRENGTH> {
         }
     }
 
-    pub fn from_iter<I: Iterator<Item = (AuthorityName, S)>>(
+    pub fn from_iter<I: Iterator<Item = Result<(AuthorityName, S), TypedStoreError>>>(
         committee: Arc<Committee>,
         data: I,
-    ) -> Self {
+    ) -> HaneulResult<Self> {
         let mut this = Self::new(committee);
-        for (authority, s) in data {
+        for item in data {
+            let (authority, s) = item?;
             this.insert_generic(authority, s);
         }
-        this
+        Ok(this)
     }
 
     /// A generic version of inserting arbitrary type of V (e.g. void type).
