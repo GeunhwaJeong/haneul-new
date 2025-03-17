132e10cbf6bc93a69c02890a8db7794799640fb1
GeunhwaJeong <jeonggh892@gmail.com>
2025-03-17 19:28:02 +0000

    rpc-alt: resolveNameServiceNames
    ## Description
    
    Implement reverse resolution for HaneulNS.
    
    ## Test plan
    
    Add new assertions to existing name service tests:
    
    ```
    haneul$ cargo nextest run -p haneul-indexer-alt-e2e-tests \
      --test name_service_tests
    ```
    
    ## Stack
    
    -  
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/haneul-indexer-alt-jsonrpc/src/api/name_service/mod.rs
+++ crates/haneul-indexer-alt-jsonrpc/src/api/name_service/mod.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use jsonrpsee::{core::RpcResult, proc_macros::rpc};
+use haneul_json_rpc_types::Page as PageResponse;
 use haneul_open_rpc::Module;
 use haneul_open_rpc_macros::open_rpc;
 use haneul_types::base_types::HaneulAddress;
@@ -25,6 +26,20 @@ trait NameServiceApi {
         /// The name to resolve
         name: String,
     ) -> RpcResult<Option<HaneulAddress>>;
+
+    /// Find the HaneulNS name that points to this address.
+    ///
+    /// Although this method's response is paginated, it will only ever return at most one name.
+    #[method(name = "resolveNameServiceNames")]
+    async fn resolve_name_service_names(
+        &self,
+        /// The address to resolve
+        address: HaneulAddress,
+        /// Unused pagination cursor
+        cursor: Option<String>,
+        /// Unused pagination limit
+        limit: Option<usize>,
+    ) -> RpcResult<PageResponse<String, String>>;
 }
 
 pub(crate) struct NameService(pub Context);
@@ -36,6 +51,25 @@ impl NameServiceApiServer for NameService {
         Ok(response::resolved_address(ctx, &name)
             .await
             .with_internal_context(|| format!("Resolving HaneulNS name {name:?}"))?)
+    }
+
+    async fn resolve_name_service_names(
+        &self,
+        address: HaneulAddress,
+        _cursor: Option<String>,
+        _limit: Option<usize>,
+    ) -> RpcResult<PageResponse<String, String>> {
+        let Self(ctx) = self;
+
+        let mut page = PageResponse::empty();
+        if let Some(name) = response::resolved_name(ctx, address)
+            .await
+            .with_internal_context(|| format!("Resolving address {address}"))?
+        {
+            page.data.push(name);
+        }
+
+        Ok(page)
     }
 }
 
