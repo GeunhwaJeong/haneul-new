66ff4722ef1068a820456698276067b8942792de
GeunhwaJeong <jeonggh892@gmail.com>
2025-03-29 03:13:00 +0000

    graphql-alt: Stubs for fetching transactions, effects, objects
    ## Description
    Add stub types for Transactions, TransactionEffects, and Objects, and
    APIs to fetch one and multiple of each. The implementations are stubbed
    out to only return the IDs/keys that were passed in, until DB support
    can be added.
    
    ## Test plan
    Manual testing

---
+++ crates/haneul-indexer-alt-graphql/src/api/scalars/haneul_address.rs
@@ -0,0 +1,222 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+use std::{fmt, str::FromStr};
+
+use async_graphql::{InputValueError, InputValueResult, Scalar, ScalarType, Value};
+use move_core_types::account_address::AccountAddress;
+use haneul_types::base_types::HaneulAddress as NativeHaneulAddress;
+
+const HANEUL_ADDRESS_LENGTH: usize = 32;
+
+#[derive(Copy, Clone, Debug, Hash, Eq, PartialEq, Ord, PartialOrd)]
+pub(crate) struct HaneulAddress([u8; HANEUL_ADDRESS_LENGTH]);
+
+#[derive(thiserror::Error, Debug)]
+pub(crate) enum Error {
+    #[error("Invalid hexadecimal character at offset {0}")]
+    BadHex(usize),
+
+    #[error("Missing '0x' prefix while parsing HaneulAddress: {0:?}")]
+    NoPrefix(String),
+
+    #[error(
+        "Expected between 1 and {} hexadecimal digits ({} bytes), received {0}",
+        HANEUL_ADDRESS_LENGTH * 2,
+        HANEUL_ADDRESS_LENGTH,
+    )]
+    WrongLength(usize),
+}
+
+/// String containing 32 byte hex-encoded address, with a leading '0x'. Leading zeroes can be omitted on input but will always appear in outputs (HaneulAddress in output is guaranteed to be 66 characters long).
+#[Scalar]
+impl ScalarType for HaneulAddress {
+    fn parse(value: Value) -> InputValueResult<Self> {
+        let Value::String(s) = value else {
+            return Err(InputValueError::expected_type(value));
+        };
+
+        Ok(HaneulAddress::from_str(&s)?)
+    }
+
+    fn to_value(&self) -> Value {
+        Value::String(self.to_string())
+    }
+}
+
+impl FromStr for HaneulAddress {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Error> {
+        let Some(s) = s.strip_prefix("0x") else {
+            return Err(Error::NoPrefix(s.to_owned()));
+        };
+
+        if s.is_empty() || s.len() > HANEUL_ADDRESS_LENGTH * 2 {
+            return Err(Error::WrongLength(s.len()));
+        }
+
+        // Parse a single hexadecimal character from the string, or return an error pointing to the
+        // bad character in the source string.
+        let hex = |i: usize| -> Result<u8, Error> {
+            u8::from_str_radix(&s[i..=i], 16).map_err(|_| Error::BadHex(i + 2))
+        };
+
+        let mut arr = [0u8; HANEUL_ADDRESS_LENGTH];
+
+        let mut i = arr.len() - 1;
+        let mut j = s.len();
+
+        // Keep filling the array from the back until we have one hex digit left to process.
+        while j > 2 {
+            arr[i] = (hex(j - 2)? << 4) | hex(j - 1)?;
+            i -= 1;
+            j -= 2;
+        }
+
+        // Process the last hex digit, which will be compromised of either two or one character.
+        if j == 2 {
+            arr[i] = (hex(0)? << 4) | hex(1)?;
+        } else {
+            arr[i] = hex(0)?;
+        }
+
+        Ok(HaneulAddress(arr))
+    }
+}
+
+impl fmt::Display for HaneulAddress {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "0x")?;
+        for byte in &self.0 {
+            write!(f, "{:02x}", byte)?;
+        }
+        Ok(())
+    }
+}
+
+impl From<HaneulAddress> for AccountAddress {
+    fn from(value: HaneulAddress) -> Self {
+        AccountAddress::new(value.0)
+    }
+}
+
+impl From<HaneulAddress> for NativeHaneulAddress {
+    fn from(value: HaneulAddress) -> Self {
+        AccountAddress::from(value).into()
+    }
+}
+
+impl From<NativeHaneulAddress> for HaneulAddress {
+    fn from(value: NativeHaneulAddress) -> Self {
+        HaneulAddress(value.to_inner())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use async_graphql::Value;
+
+    const FULL_ADDRESS_STR: &str =
+        "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
+    const FULL_ADDRESS: HaneulAddress = HaneulAddress([
+        0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd,
+        0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef, 0x01, 0x23, 0x45, 0x67, 0x89, 0xab,
+        0xcd, 0xef,
+    ]);
+
+    const ODD_SHORT_ADDRESS_STR: &str = "0x123456789";
+    const EVEN_SHORT_ADDRESS_STR: &str = "0x0123456789";
+    const FULL_SHORT_ADDRESS_STR: &str =
+        "0x0000000000000000000000000000000000000000000000000000000123456789";
+    const SHORT_ADDRESS: HaneulAddress = HaneulAddress([
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x23, 0x45,
+        0x67, 0x89,
+    ]);
+
+    #[test]
+    fn test_parse_full() {
+        let parsed = HaneulAddress::from_str(FULL_ADDRESS_STR).unwrap();
+        assert_eq!(parsed, FULL_ADDRESS);
+    }
+
+    #[test]
+    fn test_parse_short_odd() {
+        let parsed = HaneulAddress::from_str(ODD_SHORT_ADDRESS_STR).unwrap();
+        assert_eq!(parsed, SHORT_ADDRESS);
+    }
+
+    #[test]
+    fn test_parse_short_even() {
+        let parsed = HaneulAddress::from_str(EVEN_SHORT_ADDRESS_STR).unwrap();
+        assert_eq!(parsed, SHORT_ADDRESS);
+    }
+
+    #[test]
+    fn test_full_to_value() {
+        let value = ScalarType::to_value(&FULL_ADDRESS);
+        assert_eq!(value, Value::String(FULL_ADDRESS_STR.to_string()));
+    }
+
+    #[test]
+    fn test_short_to_value() {
+        let value = ScalarType::to_value(&SHORT_ADDRESS);
+        assert_eq!(value, Value::String(FULL_SHORT_ADDRESS_STR.to_string()));
+    }
+
+    #[test]
+    fn test_round_trip_full() {
+        let value = ScalarType::to_value(&FULL_ADDRESS);
+        let parsed_back = ScalarType::parse(value).unwrap();
+        assert_eq!(FULL_ADDRESS, parsed_back);
+    }
+
+    #[test]
+    fn test_round_trip_short() {
+        let value = ScalarType::to_value(&SHORT_ADDRESS);
+        let parsed_back = ScalarType::parse(value).unwrap();
+        assert_eq!(SHORT_ADDRESS, parsed_back);
+    }
+
+    #[test]
+    fn test_parse_no_prefix() {
+        let err = HaneulAddress::from_str("123456789").unwrap_err();
+        assert!(matches!(err, Error::NoPrefix(_)), "{err:?}");
+    }
+
+    #[test]
+    fn test_parse_invalid_prefix() {
+        let err = HaneulAddress::from_str("1x123456789").unwrap_err();
+        assert!(matches!(err, Error::NoPrefix(_)), "{err:?}");
+    }
+
+    #[test]
+    fn test_parse_too_short() {
+        let err = HaneulAddress::from_str("0x").unwrap_err();
+        assert!(matches!(err, Error::WrongLength(0)), "{err:?}");
+    }
+
+    #[test]
+    fn test_parse_invalid_characters() {
+        let mut input = FULL_ADDRESS_STR.to_owned();
+        input.replace_range(20..=20, "g");
+
+        let err = HaneulAddress::from_str(&input).unwrap_err();
+        assert!(matches!(err, Error::BadHex(20)), "{err:?}");
+    }
+
+    #[test]
+    fn test_unicode_gibberish() {
+        let parsed = HaneulAddress::from_str("aAௗ0㌀0");
+        assert!(parsed.is_err());
+    }
+
+    #[test]
+    fn test_bad_scalar_type() {
+        let input = Value::Number(0x42.into());
+        let parsed = <HaneulAddress as ScalarType>::parse(input);
+        assert!(parsed.is_err());
+    }
+}
