477ad6320dcc5de3573a05115d1358b34c874c80
GeunhwaJeong <jeonggh892@gmail.com>
2025-04-14 15:06:34 -0700

    [cli] Remove cli tests that rely on devnet/testnet/mainnet
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/haneul/tests/shell_tests/with_network/move_build_bytecode_with_address_resolution/move_build_bytecode_with_address_resolution.sh
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Mysten Labs, Inc.
-# SPDX-License-Identifier: Apache-2.0
-
-haneul client --client.config $CONFIG \
-  publish simple --verify-deps \
-  --json | jq '.effects.status'
-
-haneul move --client.config $CONFIG \
-  build --path depends_on_simple
