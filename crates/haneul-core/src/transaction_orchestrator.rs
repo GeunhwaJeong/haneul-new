5f347e859dfad4b1a531df3e739ac90ffdfca6b0
GeunhwaJeong <jeonggh892@gmail.com>
2025-06-17 12:37:43 -0500

    grpc: merge simulate and resolve
    Merge SimulateTransaction and ResolveTransaction into a single api and
    add additional options to SimulateTransactionRequest to:
      - explicitly request gas selection
      - Request that Vm checks be disabled in order to support the usecases
        which rely on the JSONRPC devinspect api.

--- crates/haneul-core/src/transaction_orchestrator.rs
+++ crates/haneul-core/src/transaction_orchestrator.rs
@@ -39,7 +39,7 @@ use haneul_types::quorum_driver_types::{
 };
 use haneul_types::haneul_system_state::HaneulSystemState;
 use haneul_types::transaction::{TransactionData, VerifiedTransaction};
-use haneul_types::transaction_executor::SimulateTransactionResult;
+use haneul_types::transaction_executor::{SimulateTransactionResult, VmChecks};
 use tokio::sync::broadcast::error::RecvError;
 use tokio::sync::broadcast::Receiver;
 use tokio::task::JoinHandle;
@@ -699,7 +699,9 @@ where
     fn simulate_transaction(
         &self,
         transaction: TransactionData,
+        checks: VmChecks,
     ) -> Result<SimulateTransactionResult, HaneulError> {
-        self.validator_state.simulate_transaction(transaction)
+        self.validator_state
+            .simulate_transaction(transaction, checks)
     }
 }
