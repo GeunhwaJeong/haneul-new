1ddb0724f851a0464b3e619c6cce42e583c55659
GeunhwaJeong <jeonggh892@gmail.com>
2025-07-17 17:55:49 -0700

    Add workflows to test with mainnet protocol config
    ## Description 
    
    It is common to have a feature enabled in simtests and optionally
    testnet, but not mainnet. A feature can be in this state for months. But
    mainnet protocol config is only tested in a few upgrade related tests.
    It is important to make sure changes do not accidentally break the
    `mainnet` protocol while passing tests with the latest protocol config
    via features enabled in dev environments.
    
    ## Test plan 
    
    CI
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/haneul-e2e-tests/tests/multisig_tests.rs
+++ crates/haneul-e2e-tests/tests/multisig_tests.rs
@@ -426,6 +426,10 @@ async fn test_multisig_e2e() {
 
 #[sim_test]
 async fn test_multisig_with_zklogin_scenerios() {
+    if haneul_simulator::has_mainnet_protocol_config_override() {
+        return;
+    }
+
     let test_cluster = TestClusterBuilder::new()
         // Use a long epoch duration such that it won't change epoch on its own.
         .with_epoch_duration_ms(10000000)
@@ -959,6 +963,10 @@ async fn test_max_epoch_too_large_fail_zklogin_in_multisig() {
 
 #[sim_test]
 async fn test_random_zklogin_in_multisig() {
+    if haneul_simulator::has_mainnet_protocol_config_override() {
+        return;
+    }
+
     let test_vectors =
         &load_test_vectors("../haneul-types/src/unit_tests/zklogin_test_vectors.json")[1..11];
     let test_cluster = TestClusterBuilder::new()
