66ea6cb7de84fe7eda85ef51f527380b5f98d93c
GeunhwaJeong <jeonggh892@gmail.com>
2025-07-22 11:22:22 -0400

    fix(zklogin): skip invalid jwk not error
    ## Description 
    
    we used to error completely if a provider has a ES256 key in addition to
    a RSA key. it should instead skip it and parse the valid RSA key. added
    to protocol config flag:
    https://github.com/GeunhwaJeong/fastcrypto/pull/858
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/haneul-node/src/lib.rs
+++ crates/haneul-node/src/lib.rs
@@ -206,6 +206,7 @@ mod simulator {
         parse_jwks(
             haneul_types::zk_login_util::DEFAULT_JWK_BYTES,
             &OIDCProvider::Twitch,
+            true,
         )
         .map_err(|_| HaneulError::JWKRetrievalError)
     }
@@ -2104,7 +2105,7 @@ impl HaneulNode {
     ) -> HaneulResult<Vec<(JwkId, JWK)>> {
         use fastcrypto_zkp::bn254::zk_login::fetch_jwks;
         let client = reqwest::Client::new();
-        fetch_jwks(provider, &client)
+        fetch_jwks(provider, &client, true)
             .await
             .map_err(|_| HaneulError::JWKRetrievalError)
     }
