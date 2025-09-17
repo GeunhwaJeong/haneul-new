38fb092875de10a43f6f7c5e34170109b29a442b
GeunhwaJeong <jeonggh892@gmail.com>
2025-09-16 22:05:22 -0400

    graphql-alt: join bounds query with filter query
    ## Description 
    
    Reduces some of the duplicate code between event and transaction
    cursors.
    
    ## Test plan 
    
    Existing tests cover refactor.
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:

--- crates/haneul-indexer-alt-graphql/src/api/types/event/lookups.rs
+++ crates/haneul-indexer-alt-graphql/src/api/types/event/lookups.rs
@@ -1,14 +1,16 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-use std::collections::HashMap;
-use std::{iter::Rev, ops::Range, sync::Arc};
+use std::{collections::HashMap, iter::Rev, ops::Range, sync::Arc};
 
 use anyhow::Context as _;
 use async_graphql::{dataloader::DataLoader, Context};
 use itertools::Either;
-use haneul_indexer_alt_reader::kv_loader::TransactionEventsContents;
-use haneul_indexer_alt_reader::{kv_loader::KvLoader, pg_reader::PgReader, tx_digests::TxDigestKey};
+use haneul_indexer_alt_reader::{
+    kv_loader::{KvLoader, TransactionEventsContents},
+    pg_reader::PgReader,
+    tx_digests::TxDigestKey,
+};
 use haneul_indexer_alt_schema::transactions::StoredTxDigest;
 use haneul_types::{digests::TransactionDigest, event::Event as NativeEvent};
 
