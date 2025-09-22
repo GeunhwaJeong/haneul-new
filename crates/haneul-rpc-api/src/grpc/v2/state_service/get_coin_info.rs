9ae3a925debe748ad6aab2d46642ffce9a1bc9e9
GeunhwaJeong <jeonggh892@gmail.com>
2025-09-22 18:34:45 -0500

    Update regulated metadata logic to match graphql

--- crates/haneul-rpc-api/src/grpc/v2/state_service/get_coin_info.rs
+++ crates/haneul-rpc-api/src/grpc/v2/state_service/get_coin_info.rs
@@ -10,6 +10,7 @@ use haneul_rpc::proto::haneul::rpc::v2::GetCoinInfoRequest;
 use haneul_rpc::proto::haneul::rpc::v2::GetCoinInfoResponse;
 use haneul_sdk_types::{Address, StructTag};
 use haneul_types::base_types::{ObjectID as HaneulObjectID, HaneulAddress};
+use haneul_types::coin::RegulatedCoinMetadata;
 use haneul_types::coin_registry::{Currency, RegulatedState as CurrencyRegulatedState, SupplyState};
 use haneul_types::object::Owner::AddressOwner;
 use haneul_types::object::Owner::ConsensusAddressOwner;
@@ -143,16 +144,62 @@ fn get_coin_info_from_registry(
         }
     };
 
-    let regulated_metadata = Some((&currency.regulated).into());
+    // If registry has a definitive state, use it
+    let regulated_metadata = if !matches!(&currency.regulated, CurrencyRegulatedState::Unknown) {
+        Some((&currency.regulated).into())
+    } else {
+        // Registry has Unknown state, need to check for legacy RegulatedCoinMetadata
+        let indexes = match service.reader.inner().indexes() {
+            Some(indexes) => indexes,
+            None => {
+                // No indexes available, keep as Unknown
+                let mut response = GetCoinInfoResponse::default();
+                response.coin_type = Some(coin_type.to_string());
+                response.metadata = metadata;
+                response.treasury = treasury;
+                response.regulated_metadata = Some(CurrencyRegulatedState::Unknown.into());
+                return Ok(Some(response));
+            }
+        };
 
-    {
-        let mut response = GetCoinInfoResponse::default();
-        response.coin_type = Some(coin_type.to_string());
-        response.metadata = metadata;
-        response.treasury = treasury;
-        response.regulated_metadata = regulated_metadata;
-        Ok(Some(response))
-    }
+        let coin_info = indexes.get_coin_info(core_coin_type).map_err(|e| {
+            RpcError::new(
+                tonic::Code::Internal,
+                format!("Failed to get coin info for {}: {}", core_coin_type, e),
+            )
+        })?;
+
+        let regulated_id = coin_info.and_then(|info| info.regulated_coin_metadata_object_id);
+
+        match regulated_id {
+            None => {
+                // No RegulatedCoinMetadata exists, coin is unregulated
+                Some(CurrencyRegulatedState::Unregulated.into())
+            }
+            Some(id) => object_store
+                .get_object(&id)
+                .map(RegulatedCoinMetadata::try_from)
+                .transpose()
+                .map_err(|_| {
+                    RpcError::new(
+                        tonic::Code::Internal,
+                        format!(
+                            "Unable to read object {} for coin type {} as RegulatedCoinMetadata",
+                            id, core_coin_type
+                        ),
+                    )
+                })?
+                .map(Into::into)
+                .or(Some(CurrencyRegulatedState::Unregulated.into())),
+        }
+    };
+
+    let mut response = GetCoinInfoResponse::default();
+    response.coin_type = Some(coin_type.to_string());
+    response.metadata = metadata;
+    response.treasury = treasury;
+    response.regulated_metadata = regulated_metadata;
+    Ok(Some(response))
 }
 
 fn get_coin_info_from_index(
@@ -202,7 +249,7 @@ fn get_coin_info_from_index(
             .reader
             .inner()
             .get_object(&regulated_coin_metadata_object_id)
-            .map(haneul_types::coin::RegulatedCoinMetadata::try_from)
+            .map(RegulatedCoinMetadata::try_from)
             .transpose()
             .map_err(|_| {
                 RpcError::new(
@@ -215,7 +262,7 @@ fn get_coin_info_from_index(
             })?
             .map(Into::into)
     } else {
-        Some(CurrencyRegulatedState::Unknown.into())
+        Some(CurrencyRegulatedState::Unregulated.into())
     };
 
     {
