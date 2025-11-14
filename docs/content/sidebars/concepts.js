08052e956f4ada29bb3ae88cd95852647a190505
GeunhwaJeong <jeonggh892@gmail.com>
2025-11-14 12:48:41 -0600

    docs: Slightly restructure "Concepts" IA
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] Indexing Framework:
    
    ---------

--- docs/content/sidebars/concepts.js
+++ docs/content/sidebars/concepts.js
@@ -4,132 +4,51 @@
 const concepts = [
 	'concepts',
 	'concepts/haneul-for-ethereum',
-	'concepts/components',
 	{
 		type: 'category',
-		label: 'App Developers',
+		label: 'Haneul Architecture',
 		link: {
 			type: 'doc',
-			id: 'concepts/app-devs',
+			id: 'concepts/components',
 		},
 		items: [
-			{
-				type: 'category',
-				label: 'Object Model',
-				link: {
-					type: 'doc',
-					id: 'concepts/object-model',
-				},
-				items: [
-					{
-						type: 'category',
-						label: 'Object Ownership',
-						link: {
-							type: 'doc',
-							id: 'concepts/object-ownership',
-						},
-						items: [
-							'concepts/object-ownership/address-owned',
-							'concepts/object-ownership/immutable',
-							'concepts/object-ownership/party',
-							'concepts/object-ownership/shared',
-							'concepts/object-ownership/wrapped',
-						],
-					},
-					{
-						type: 'category',
-						label: 'Transfers',
-						link: {
-							type: 'doc',
-							id: 'concepts/transfers',
-						},
-						items: ['concepts/transfers/custom-rules', 
-								'concepts/transfers/transfer-policies',
-							    'concepts/transfers/transfer-to-object'],
-					},
-					'concepts/versioning',
-				],
-			},
-			{
-				type: 'category',
-				label: 'Move Overview',
-				link: {
-					type: 'doc',
-					id: 'concepts/haneul-move-concepts',
-				},
-				items: [
-					{
-						type: 'category',
-						label: 'Packages',
-						link: {
-							type: 'doc',
-							id: 'concepts/haneul-move-concepts/packages',
-						},
-						items: [
-							'concepts/haneul-move-concepts/packages/upgrade',
-							'concepts/haneul-move-concepts/packages/custom-policies',
-							'concepts/haneul-move-concepts/packages/automated-address-management',
-						],
-					},
-					{
-						type: 'category',
-						label: 'Dynamic Fields',
-						link: {
-							type: 'doc',
-							id: 'concepts/dynamic-fields',
-						},
-						items: ['concepts/dynamic-fields/tables-bags'],
-					},
-					'concepts/haneul-move-concepts/derived-objects',
-					'concepts/haneul-move-concepts/conventions'
-					
-				],
-			},
-			{
-				type: 'category',
-				label: 'Transactions',
-				link: {
-					type: 'doc',
-					id: 'concepts/transactions',
-				},
-				items: [
-					'concepts/transactions/prog-txn-blocks',
-					'concepts/transactions/sponsored-transactions',
-					'concepts/transactions/gas-smashing',
-				],
-			},
-			'concepts/gaming'
+			'concepts/haneul-architecture/networks',
+			'concepts/haneul-architecture/consensus',
+			'concepts/haneul-architecture/haneul-storage',
+			'concepts/haneul-architecture/haneul-security',
+			'concepts/haneul-architecture/epochs',
+			'concepts/haneul-architecture/indexer-functions',
+			'concepts/haneul-architecture/protocol-upgrades',
+			'concepts/haneul-architecture/data-management-things',
 		],
 	},
 	{
 		type: 'category',
-		label: 'Data Access',
+		label: 'Tokenomics',
+		link: {
+			type: 'doc',
+			id: 'concepts/tokenomics',
+		},
 		items: [
-			'concepts/grpc-overview',
-			{ 
-				type: 'category',
-				label: 'GraphQL and Indexer Framework',
-				link: {
-					type: 'doc',
-					id: 'concepts/graphql-indexer',
-				},
-				items: [
-					'concepts/graphql-rpc',
-					'concepts/custom-indexing-framework',
-					'concepts/custom-indexer/pipeline-architecture'
-				]
-			},
-			'concepts/archival-store'
-		]
+			'concepts/tokenomics/staking-unstaking',
+			'concepts/tokenomics/haneul-bridging',
+			'concepts/tokenomics/gas-in-haneul',
+			'concepts/tokenomics/vesting-strategies',
+			'concepts/haneul-architecture/staking-rewards',
+		],
 	},
 	{
 		type: 'category',
-		label: 'Cryptography',
+		label: 'Transactions',
 		link: {
 			type: 'doc',
-			id: 'concepts/cryptography',
+			id: 'concepts/transactions',
 		},
 		items: [
+			'concepts/haneul-architecture/transaction-lifecycle',
+			'concepts/transactions/prog-txn-blocks',
+			'concepts/transactions/sponsored-transactions',
+			'concepts/transactions/gas-smashing',
 			{
 				type: 'category',
 				label: 'Transaction Authentication',
@@ -145,6 +64,110 @@ const concepts = [
 					'concepts/cryptography/transaction-auth/intent-signing',
 				],
 			},
+		],
+	},
+	{
+		type: 'category',
+		label: 'Object Model',
+		link: {
+			type: 'doc',
+			id: 'concepts/object-model',
+		},
+		items: [
+			{
+				type: 'category',
+				label: 'Object Ownership',
+				link: {
+					type: 'doc',
+					id: 'concepts/object-ownership',
+				},
+				items: [
+					'concepts/object-ownership/address-owned',
+					'concepts/object-ownership/immutable',
+					'concepts/object-ownership/party',
+					'concepts/object-ownership/shared',
+					'concepts/object-ownership/wrapped',
+				],
+			},
+			{
+				type: 'category',
+				label: 'Transfers',
+				link: {
+					type: 'doc',
+					id: 'concepts/transfers',
+				},
+				items: [
+					'concepts/transfers/custom-rules',
+					'concepts/transfers/transfer-policies',
+					'concepts/transfers/transfer-to-object',
+				],
+			},
+			'concepts/versioning',
+		],
+	},
+	{
+		type: 'category',
+		label: 'Move',
+		link: {
+			type: 'doc',
+			id: 'concepts/haneul-move-concepts',
+		},
+		items: [
+			{
+				type: 'category',
+				label: 'Packages',
+				link: {
+					type: 'doc',
+					id: 'concepts/haneul-move-concepts/packages',
+				},
+				items: [
+					'concepts/haneul-move-concepts/packages/upgrade',
+					'concepts/haneul-move-concepts/packages/custom-policies',
+					'concepts/haneul-move-concepts/packages/automated-address-management',
+				],
+			},
+			{
+				type: 'category',
+				label: 'Dynamic Fields',
+				link: {
+					type: 'doc',
+					id: 'concepts/dynamic-fields',
+				},
+				items: ['concepts/dynamic-fields/tables-bags'],
+			},
+			'concepts/haneul-move-concepts/derived-objects',
+			'concepts/haneul-move-concepts/conventions',
+		],
+	},
+	{
+		type: 'category',
+		label: 'Data Access',
+		items: [
+			'concepts/grpc-overview',
+			{
+				type: 'category',
+				label: 'GraphQL and Indexer Framework',
+				link: {
+					type: 'doc',
+					id: 'concepts/graphql-indexer',
+				},
+				items: [
+					'concepts/graphql-rpc',
+					'concepts/custom-indexing-framework',
+					'concepts/custom-indexer/pipeline-architecture',
+				],
+			},
+			'concepts/archival-store',
+		],
+	},
+	{
+		type: 'category',
+		label: 'Cryptography',
+		link: {
+			type: 'doc',
+			id: 'concepts/cryptography',
+		},
+		items: [
 			'concepts/cryptography/zklogin',
 			'concepts/cryptography/passkeys',
 			{
@@ -175,41 +198,7 @@ const concepts = [
 			},*/
 		],
 	},
-	{
-		type: 'category',
-		label: 'Haneul Architecture',
-		link: {
-			type: 'doc',
-			id: 'concepts/haneul-architecture',
-		},
-		items: [
-			'concepts/haneul-architecture/high-level',
-			'concepts/haneul-architecture/haneul-storage',
-			'concepts/haneul-architecture/haneul-security',
-			'concepts/haneul-architecture/transaction-lifecycle',
-			'concepts/haneul-architecture/consensus',
-			'concepts/haneul-architecture/indexer-functions',
-			'concepts/haneul-architecture/epochs',
-			'concepts/haneul-architecture/protocol-upgrades',
-			'concepts/haneul-architecture/data-management-things',
-			'concepts/haneul-architecture/staking-rewards',
-		],
-	},
-	{
-		type: 'category',
-		label: 'Tokenomics',
-		link: {
-			type: 'doc',
-			id: 'concepts/tokenomics',
-		},
-		items: [
-			'concepts/tokenomics/staking-unstaking',
-			'concepts/tokenomics/haneul-bridging',
-			'concepts/tokenomics/gas-pricing',
-			'concepts/tokenomics/gas-in-haneul',
-			'concepts/tokenomics/vesting-strategies'
-		],
-	},
+	'concepts/gaming',
 	'concepts/research-papers',
 ];
 export default concepts;
