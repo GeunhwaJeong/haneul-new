41aeb38ce1de866fdf7c4ff0d1d8434a447c773e
GeunhwaJeong <jeonggh892@gmail.com>
2026-01-01 10:51:57 +0000

    graphql(node): Epoch implements Node
    ## Description
    
    As in title.
    
    ## Test plan
    
    ```
    $ cargo nextest run -p haneul-indexer-alt-e2e-tests -- graphql/node
    ```

--- crates/haneul-indexer-alt-graphql/src/api/scalars/id.rs
+++ crates/haneul-indexer-alt-graphql/src/api/scalars/id.rs
@@ -12,6 +12,7 @@ use haneul_types::{
 #[derive(Serialize, Deserialize)]
 pub(crate) enum Id {
     Address(NativeHaneulAddress),
+    Epoch(u64),
     MovePackage(NativeHaneulAddress),
     ObjectByAddress(NativeHaneulAddress),
     ObjectByRef(NativeHaneulAddress, SequenceNumber, ObjectDigest),
