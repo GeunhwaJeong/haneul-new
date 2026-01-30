954c53fad9a3189d975a32e55a2d0e66c0ee6cfa
GeunhwaJeong <jeonggh892@gmail.com>
2026-01-30 11:27:10 -0500

    Allow multiple address groups of different priority in EndpointManager
    ## Description 
    
    Addresses from different sources are all saved, and the highest-priority
    source is used. This allows us to e.g. retain address overrides from
    admin server, even after epoch change when new committee is updated.
    
    ## Test plan 
    
    Added unit tests
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] Indexing Framework:

--- crates/haneul-node/src/lib.rs
+++ crates/haneul-node/src/lib.rs
@@ -38,7 +38,7 @@ use haneul_core::consensus_adapter::ConsensusClient;
 use haneul_core::consensus_manager::UpdatableConsensusClient;
 use haneul_core::epoch::randomness::RandomnessManager;
 use haneul_core::execution_cache::build_execution_cache;
-use haneul_network::endpoint_manager::EndpointId;
+use haneul_network::endpoint_manager::{AddressSource, EndpointId};
 use haneul_network::validator::server::HANEUL_TLS_SERVER_NAME;
 use haneul_types::full_checkpoint_content::Checkpoint;
 
@@ -2426,7 +2426,11 @@ fn update_peer_addresses(
     for (peer_id, address) in
         epoch_start_state.get_validator_as_p2p_peers(config.protocol_public_key())
     {
-        endpoint_manager.update_endpoint(EndpointId::P2p(peer_id), vec![address]);
+        endpoint_manager.update_endpoint(
+            EndpointId::P2p(peer_id),
+            AddressSource::Committee,
+            vec![address],
+        );
     }
 }
 
