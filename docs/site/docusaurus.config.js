8505a4b53846ba392b454c4632b0ba960de56dbc
GeunhwaJeong <jeonggh892@gmail.com>
2026-02-18 08:21:38 -0600

    docs: Revise/Reorganize Concepts and Guides 'Accessing Data' sections
    ## Description 
    
    Describe the changes or additions included in this PR.
    
    ## Test plan 
    
    How did you test the new or updated feature?
    
    ---
    
    ## Release notes
    
    Check each box that your changes affect. If none of the boxes relate to
    your changes, release notes aren't required.
    
    For each box you select, include information after the relevant heading
    that describes the impact of your changes that a user might notice and
    any actions they must take to implement updates.
    
    - [ ] Protocol: 
    - [ ] Nodes (Validators and Full nodes): 
    - [ ] gRPC:
    - [ ] JSON-RPC: 
    - [ ] GraphQL: 
    - [ ] CLI: 
    - [ ] Rust SDK:
    - [ ] Indexing Framework:
    
    ---------

--- docs/site/docusaurus.config.js
+++ docs/site/docusaurus.config.js
@@ -160,8 +160,7 @@ const config = {
             "**/snippets/**",
             "**/standards/deepbook-ref/**",
             "**/app-examples/ts-sdk-ref/**",
-            "**/haneul-graphql/**/generated.md",
-            "**/haneul-graphql/**/generated.mdx", 
+            "**/app-examples/ts-sdk-ref/**",
           ],
           admonitions: {
             keywords: ["checkpoint"],
